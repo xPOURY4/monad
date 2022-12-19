e63c6603ffb47617ba74db8a705c78c438b75e58
James Hunsaker <hunsaker@spacetime.org>
2022-12-19 09:41:12 -0500

    do not inherit from

--- include/monad/core/huge_mem.hpp
+++ include/monad/core/huge_mem.hpp
@@ -6,15 +6,18 @@
 
 MONAD_NAMESPACE_BEGIN
 
-class HugeMem
+class HugeMem final
 {
-protected:
     size_t const size_;
     void *const mem_;
 
 public:
     HugeMem(size_t);
     ~HugeMem();
+
+    [[gnu::always_inline]] size_t getSize() const { return size_; }
+
+    [[gnu::always_inline]] void *getMem() const { return mem_; }
 };
 
 MONAD_NAMESPACE_END
