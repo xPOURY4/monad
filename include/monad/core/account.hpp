0b3276fdd82f62944f47d16d597484cd5e76d80c
Shea Williams <sheapwilliams@gmail.com>
2023-01-06 13:04:06 -0500

    Add Account structure
    - Ensure cache line alignment

---
+++ include/monad/core/account.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <monad/config.hpp>
+#include <monad/core/bytes.hpp>
+#include <monad/core/int.hpp>
+
+MONAD_NAMESPACE_BEGIN
+
+struct Account
+{
+    uint256_t balance{0};
+    bytes32_t code_hash{NULL_HASH};
+    uint64_t nonce{0};
+    uint64_t incarnation{0};
+};
+
+static_assert(sizeof(Account) == 80);
+static_assert(alignof(Account) == 8);
+
+MONAD_NAMESPACE_END
--- include/monad/core/bytes.hpp
+++ include/monad/core/bytes.hpp
@@ -11,4 +11,7 @@ using bytes32_t = ::evmc::bytes32;
 static_assert(sizeof(bytes32_t) == 32);
 static_assert(alignof(bytes32_t) == 1);
 
+using namespace evmc::literals;
+inline constexpr bytes32_t NULL_HASH{0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470_bytes32};
+
 MONAD_NAMESPACE_END
