cb083f6294214c6d215e829ba8b77f2164762553
Shea Williams <sheapwilliams@gmail.com>
2023-03-08 13:35:08 -0700

    Add tl-expected, which is compatible with C++23 std::expected

---
+++ include/monad/execution/config.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <monad/config.hpp>
+
+#define MONAD_EXECUTION_NAMESPACE_BEGIN MONAD_NAMESPACE_BEGIN namespace execution {
+
+#define MONAD_EXECUTION_NAMESPACE_END } MONAD_NAMESPACE_END
