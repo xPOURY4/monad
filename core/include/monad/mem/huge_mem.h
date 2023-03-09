95502cecab8a466ab8288d56bb8ae70960f8b61e
James Hunsaker <hunsaker@spacetime.org>
2023-03-08 21:35:21 -0500

    copy huge mem impl

--- core/include/monad/mem/huge_mem.h
+++ core/include/monad/mem/huge_mem.h
@@ -12,3 +12,6 @@ typedef struct huge_mem_t
 
 static_assert(sizeof(huge_mem_t) == 16);
 static_assert(alignof(huge_mem_t) == 8);
+
+void huge_mem_alloc(huge_mem_t *, size_t);
+void huge_mem_free(huge_mem_t *);
