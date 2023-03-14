adeb316cb3794002d8c62d5894340af61a3f22d0
James Hunsaker <hunsaker@spacetime.org>
2023-03-14 12:37:02 -0400

    c std bit

--- core/include/monad/core/bit.h
+++ core/include/monad/core/bit.h
@@ -1,6 +1,49 @@
 #pragma once
 
+#include <limits.h>
+#include <stdbool.h>
 #include <stddef.h>
+
+#ifndef SIZE_WIDTH
+    #define SIZE_WIDTH (sizeof(size_t) * CHAR_BIT) /* TODO c23 */
+#endif
+
+static inline bool has_single_bit(size_t const x)
+{
+    return popcount(x) == 1;
+}
+
+/**
+ * undefined if x <= 1
+ */
+static inline size_t bit_ceil_undef(size_t const x)
+{
+    return 1UL << bit_width_undef(x - 1);
+}
+
+/**
+ * undefined if x = 0
+ */
+static inline size_t bit_width_undef(size_t const x)
+{
+    return SIZE_WIDTH - countl_zero_undef(x);
+}
+
+/**
+ * undefined if x = 0
+ */
+static inline size_t countl_zero_undef(size_t const x)
+{
+    return __builtin_clzl(x);
+}
+
+/**
+ * undefined if x = 0
+ */
+static inline size_t countr_zero_undef(size_t const x)
+{
+    return __builtin_ctzl(x);
+}
 
 static inline size_t popcount(size_t const x)
 {
--- core/include/monad/core/math.h
+++ core/include/monad/core/math.h
@@ -1,24 +1,10 @@
 #pragma once
 
-#include <limits.h>
 #include <stddef.h>
-
-#ifndef SIZE_WIDTH
-    #define SIZE_WIDTH (sizeof(size_t) * CHAR_BIT) /* TODO c23 */
-#endif
 
 static inline size_t max(size_t const x, size_t const y)
 {
     return x > y ? x : y;
-}
-
-/**
- * returns smallest m such that (1 << m) >= n
- * undefined if n is 0 or 1
- */
-static inline size_t log2_up(size_t const n)
-{
-    return SIZE_WIDTH - __builtin_clzl(n - 1);
 }
 
 /**
