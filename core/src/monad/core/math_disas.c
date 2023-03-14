8cac782226cfae3218947caa1f6239c997eae817
James Hunsaker <hunsaker@spacetime.org>
2023-03-14 13:27:17 -0400

    disas lib

---
+++ core/src/monad/core/math_disas.c
@@ -0,0 +1,11 @@
+#include <monad/core/math.h>
+
+size_t max_disas(size_t const x, size_t const y)
+{
+    return max(x, y);
+}
+
+size_t round_up_disas(size_t const x, size_t const y)
+{
+    return round_up(x, y);
+}
