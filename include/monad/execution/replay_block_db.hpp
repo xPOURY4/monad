60b5046759a239831fe12e61784c92e6aabc2ce6
Tong Zhi <tzhi@monad.xyz>
2023-04-20 23:27:59 -0400

    :new: :white_check_mark: Add ReplayFromBlockDb
    Problem:
    - Need a class to recursively replay all the blocks
    
    Solution:
    - Add ReplayFromBlockDb class

---
+++ include/monad/execution/replay_block_db.hpp
@@ -0,0 +1,168 @@
+#pragma once
+
+#include <monad/db/block_db.hpp>
+#include <monad/db/state_db.hpp>
+#include <monad/execution/config.hpp>
+#include <monad/execution/ethereum/fork_traits.hpp>
+#include <monad/execution/evm.hpp>
+#include <monad/execution/execution_model.hpp>
+#include <monad/execution/transaction_processor_data.hpp>
+
+#include <optional>
+#include <ostream>
+
+MONAD_EXECUTION_NAMESPACE_BEGIN
+
+template <
+    class TState, class TBlockDb, class TExecution,
+    template <typename> class TAllTxnBlockProcessor,
+    template <typename> class TStateTrie, class TTransactionTrie,
+    class TReceiptTrie, class TReceiptCollector>
+class ReplayFromBlockDb
+{
+public:
+    enum class Status
+    {
+        SUCCESS_END_OF_DB,
+        SUCCESS,
+        INVALID_END_BLOCK_NUMBER,
+        START_BLOCK_NUMBER_OUTSIDE_DB,
+        DECOMPRESS_BLOCK_ERROR,
+        DECODE_BLOCK_ERROR
+    };
+
+    struct Result
+    {
+        Status status;
+        block_num_t block_number;
+    };
+
+    template <concepts::fork_traits<TState> TTraits>
+    [[nodiscard]] constexpr block_num_t
+    loop_until(std::optional<block_num_t> until_block_number)
+    {
+        if (!until_block_number.has_value()) {
+            return TTraits::last_block_number;
+        }
+        else {
+            return std::min(
+                until_block_number.value() - 1u,
+                static_cast<uint64_t>(TTraits::last_block_number));
+        }
+    }
+
+    template <
+        concepts::fork_traits<TState> TTraits,
+        template <typename, typename> class TTxnProcessor,
+        template <typename, typename> class TEvm,
+        template <typename, typename, typename, typename, typename>
+        class TFiberData>
+    [[nodiscard]] Result run_fork(
+        TState &state, TStateTrie<TState> &state_trie, TBlockDb const &block_db,
+        TReceiptCollector &receipt_collector, std::ostream &output,
+        block_num_t current_block_number,
+        std::optional<block_num_t> until_block_number = std::nullopt)
+    {
+        for (; current_block_number <= loop_until<TTraits>(until_block_number);
+             ++current_block_number) {
+            Block block{};
+            auto const block_read_status =
+                block_db.get(current_block_number, block);
+
+            switch (block_read_status) {
+            case TBlockDb::Status::NO_BLOCK_FOUND:
+                return Result{
+                    Status::SUCCESS_END_OF_DB, current_block_number - 1u};
+            case TBlockDb::Status::DECOMPRESS_ERROR:
+                return Result{
+                    Status::DECOMPRESS_BLOCK_ERROR, current_block_number};
+            case TBlockDb::Status::DECODE_ERROR:
+                return Result{Status::DECODE_BLOCK_ERROR, current_block_number};
+            case TBlockDb::Status::SUCCESS: {
+                TAllTxnBlockProcessor<TExecution> block_processor{};
+                auto const receipts = block_processor.template execute<
+                    TState,
+                    TFiberData<
+                        TState,
+                        TTraits,
+                        TTxnProcessor<TState, TTraits>,
+                        TEvm<TState, TTraits>,
+                        TExecution>>(state, block);
+
+                TTransactionTrie transaction_trie(block.transactions);
+                TReceiptTrie receipt_trie(receipts);
+
+                auto const transaction_root = transaction_trie.root_hash();
+                auto const receipt_root = receipt_trie.root_hash();
+                auto const state_root = state_trie.incremental_update(state);
+
+                output << "Transaction Root: " << transaction_root.bytes
+                       << "\n";
+                output << "Receipt Root: " << receipt_root.bytes << "\n";
+                output << "State Root: " << state_root.bytes << "\n";
+
+                receipt_collector.emplace_back(receipts);
+            }
+            default:
+                break;
+            }
+        }
+
+        if (until_block_number.has_value() &&
+            until_block_number.value() <= current_block_number) {
+            return Result{Status::SUCCESS, current_block_number - 1u};
+        }
+
+        else {
+            return run_fork<
+                typename TTraits::next_fork_t,
+                TTxnProcessor,
+                TEvm,
+                TFiberData>(
+                state,
+                state_trie,
+                block_db,
+                receipt_collector,
+                output,
+                current_block_number,
+                until_block_number);
+        }
+    }
+
+    template <
+        concepts::fork_traits<TState> TTraits,
+        template <typename, typename> class TTxnProcessor,
+        template <typename, typename> class TEvm,
+        template <typename, typename, typename, typename, typename>
+        class TFiberData>
+    [[nodiscard]] inline Result
+    run(TState &state, TStateTrie<TState> &state_trie, TBlockDb const &block_db,
+        TReceiptCollector &receipt_collector, std::ostream &output,
+        block_num_t start_block_number,
+        std::optional<block_num_t> until_block_number = std::nullopt)
+    {
+        Block block{};
+
+        if (until_block_number.has_value() &&
+            (until_block_number <= start_block_number)) {
+            return Result{Status::INVALID_END_BLOCK_NUMBER, start_block_number};
+        }
+
+        if (block_db.get(start_block_number, block) ==
+            TBlockDb::Status::NO_BLOCK_FOUND) {
+            return Result{
+                Status::START_BLOCK_NUMBER_OUTSIDE_DB, start_block_number};
+        }
+
+        return run_fork<TTraits, TTxnProcessor, TEvm, TFiberData>(
+            state,
+            state_trie,
+            block_db,
+            receipt_collector,
+            output,
+            start_block_number,
+            until_block_number);
+    }
+};
+
+MONAD_EXECUTION_NAMESPACE_END
