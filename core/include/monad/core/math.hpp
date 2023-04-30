10c3deb12984143458ed826dfda33a38a6ec961e
James Hunsaker <hunsaker@spacetime.org>
2023-04-29 20:02:23 -0400

    convert math to c++

---
+++ core/include/monad/core/math.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <monad/config.hpp>
+
+MONAD_NAMESPACE_BEGIN
+
+/**
+ * returns smallest z such that z % y == 0 and z >= x
+ */
+template <class T>
+constexpr T round_up(T const x, T const y)
+{
+    T z = x + (y - 1);
+    z /= y;
+    z *= y;
+    return z;
+    /*
+        TODO alt impl
+        T z = x;
+        T const r = x % y;
+        if (r) {
+            z += y - r;
+        }
+    */
+    return z;
+}
+
+MONAD_NAMESPACE_END
