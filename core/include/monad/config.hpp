0c4b2ef16d5116e1c7b1c9f516161aef8599ef7e
James Hunsaker <hunsaker@spacetime.org>
2023-04-30 22:44:09 -0400

    add basic assumptions

--- core/include/monad/config.hpp
+++ core/include/monad/config.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <bit>
+#include <climits>
+
 #define MONAD_NAMESPACE_BEGIN                                                  \
     namespace monad                                                            \
     {
 
 #define MONAD_NAMESPACE_END }
+
+static_assert(CHAR_BIT == 8);
+
+static_assert(
+    std::endian::native == std::endian::big ||
+    std::endian::native == std::endian::little);
