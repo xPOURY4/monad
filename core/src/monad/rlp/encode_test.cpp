cf578e13a84900b360cdbaf2da1c145f7aba3773
James Hunsaker <hunsaker@spacetime.org>
2023-05-02 17:26:23 -0400

    encode string tests

--- core/src/monad/rlp/encode_test.cpp
+++ core/src/monad/rlp/encode_test.cpp
@@ -1,9 +1,14 @@
 #include <monad/rlp/encode.hpp>
 
+#include <monad/core/byte_string.hpp>
+
 #include <gtest/gtest.h>
 
 #include <algorithm>
 #include <cstddef>
+
+using monad::byte_string;
+using monad::byte_string_view;
 
 TEST(rlp, impl_length_length)
 {
@@ -67,4 +72,64 @@ TEST(rlp, impl_encode_length)
         buf,
         result,
         (unsigned char[]){0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}));
+}
+
+TEST(rlp, string_length)
+{
+    size_t result;
+
+    result = monad::rlp::string_length(
+        monad::to_byte_string_view((unsigned char[]){1}));
+    EXPECT_EQ(result, 1);
+
+    result = monad::rlp::string_length(
+        monad::to_byte_string_view((unsigned char[]){128}));
+    EXPECT_EQ(result, 2);
+
+    result = monad::rlp::string_length({});
+    EXPECT_EQ(result, 1);
+
+    result = monad::rlp::string_length(
+        monad::to_byte_string_view((unsigned char[]){1, 2}));
+    EXPECT_EQ(result, 3);
+
+    result = monad::rlp::string_length(byte_string(55, 1));
+    EXPECT_EQ(result, 56);
+
+    result = monad::rlp::string_length(byte_string(56, 1));
+    EXPECT_EQ(result, 58);
+}
+
+TEST(rlp, encode_string)
+{
+    unsigned char buf[256];
+    unsigned char *result;
+
+    result = monad::rlp::encode_string(buf, byte_string({1}));
+    EXPECT_EQ(result - buf, 1);
+    EXPECT_TRUE(byte_string_view(buf, result) == byte_string({1}));
+
+    result = monad::rlp::encode_string(buf, byte_string({128}));
+    EXPECT_EQ(result - buf, 2);
+    EXPECT_TRUE(byte_string_view(buf, result) == byte_string({129, 128}));
+
+    result = monad::rlp::encode_string(buf, byte_string{});
+    EXPECT_EQ(result - buf, 1);
+    EXPECT_TRUE(byte_string_view(buf, result) == byte_string({128}));
+
+    result = monad::rlp::encode_string(buf, byte_string({1, 2}));
+    EXPECT_EQ(result - buf, 3);
+    EXPECT_TRUE(byte_string_view(buf, result) == byte_string({130, 1, 2}));
+
+    result = monad::rlp::encode_string(buf, byte_string(55, 1));
+    EXPECT_EQ(result - buf, 56);
+    EXPECT_TRUE(
+        byte_string_view(buf, result) ==
+        byte_string({183}) + byte_string(55, 1));
+
+    result = monad::rlp::encode_string(buf, byte_string(56, 1));
+    EXPECT_EQ(result - buf, 58);
+    EXPECT_TRUE(
+        byte_string_view(buf, result) ==
+        byte_string({184, 56}) + byte_string(56, 1));
 }
