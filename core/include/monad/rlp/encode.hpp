8ee8d9f20547b21edeb076ecad7e3ddeec1d530f
James Hunsaker <hunsaker@spacetime.org>
2023-05-01 23:15:32 -0400

    rlp encode list, sort files

--- core/include/monad/rlp/encode.hpp
+++ core/include/monad/rlp/encode.hpp
@@ -14,11 +14,6 @@ namespace impl
 {
     constexpr size_t length_length(size_t const n)
     {
-        /*
-        size_t const len_bits = std::bit_width(n);
-        size_t const len_bytes = (len_bits + 7) / 8;
-        return len_bytes;
-        */
         size_t const lz_bits = std::countl_zero(n);
         size_t const lz_bytes = lz_bits / 8;
         return sizeof(size_t) - lz_bytes;
@@ -43,6 +38,9 @@ namespace impl
     }
 }
 
+/**
+ * max return value is 1 + sizeof(size_t) + s.size()
+ */
 constexpr size_t string_length(byte_string_view const s)
 {
     if (s.size() == 1 and s[0] <= 0x7F) {
@@ -69,6 +67,35 @@ encode_string(unsigned char *d, byte_string_view const s)
     }
     else {
         *d++ = 0xB7 + impl::length_length(s.size());
+        d = impl::encode_length(d, s.size());
+        std::memcpy(d, s.data(), s.size());
+        d += s.size();
+    }
+    return d;
+}
+
+/**
+ * max return value is 1 + sizeof(size_t) + s.size()
+ */
+constexpr size_t list_length(byte_string_view const s)
+{
+    if (s.size() <= 55) {
+        return 1 + s.size();
+    }
+    else {
+        return 1 + impl::length_length(s.size()) + s.size();
+    }
+}
+
+constexpr unsigned char *encode_list(unsigned char *d, byte_string_view const s)
+{
+    if (s.size() <= 55) {
+        *d++ = 0xC0 + s.size();
+        std::memcpy(d, s.data(), s.size());
+        d += s.size();
+    }
+    else {
+        *d++ = 0xF7 + impl::length_length(s.size());
         d = impl::encode_length(d, s.size());
         std::memcpy(d, s.data(), s.size());
         d += s.size();
--- core/src/monad/rlp/encode_disas.cpp
+++ core/src/monad/rlp/encode_disas.cpp
@@ -26,4 +26,15 @@ encode_string_disas(unsigned char *const d, byte_string_view const s)
     return encode_string(d, s);
 }
 
+size_t list_length_disas(byte_string_view const s)
+{
+    return list_length(s);
+}
+
+unsigned char *
+encode_list_disas(unsigned char *const d, byte_string_view const s)
+{
+    return encode_list(d, s);
+}
+
 MONAD_RLP_NAMESPACE_END
