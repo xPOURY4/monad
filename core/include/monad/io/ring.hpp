ba796f2a5d13561f6a0991a4b4f48782fff9c15a
James Hunsaker <hunsaker@spacetime.org>
2023-05-19 23:55:43 -0400

    sq poll thread

--- core/include/monad/io/ring.hpp
+++ core/include/monad/io/ring.hpp
@@ -9,13 +9,26 @@ MONAD_IO_NAMESPACE_BEGIN
 class Ring final
 {
     io_uring ring_;
+    io_uring_params params_;
 
 public:
-    Ring(unsigned entries = 128);
+    Ring(unsigned entries, unsigned sq_thread_cpu);
     ~Ring();
+
+    Ring(Ring &&) = default;
+    Ring &operator=(Ring &&) = default;
+
+    [[gnu::always_inline]] unsigned get_sq_entries() const
+    {
+        return params_.sq_entries;
+    }
+    [[gnu::always_inline]] unsigned get_cq_entries() const
+    {
+        return params_.cq_entries;
+    }
 };
 
-static_assert(sizeof(Ring) == 216);
+static_assert(sizeof(Ring) == 336);
 static_assert(alignof(Ring) == 8);
 
 MONAD_IO_NAMESPACE_END
--- core/src/monad/io/ring.cpp
+++ core/src/monad/io/ring.cpp
@@ -4,10 +4,14 @@
 
 MONAD_IO_NAMESPACE_BEGIN
 
-Ring::Ring(unsigned const entries)
+Ring::Ring(unsigned const entries, unsigned const sq_thread_cpu)
     : ring_{}
+    , params_{
+          .flags = IORING_SETUP_SQPOLL | IORING_SETUP_SQ_AFF,
+          .sq_thread_cpu = sq_thread_cpu,
+          .sq_thread_idle = 60 * 1000}
 {
-    int const result = io_uring_queue_init(entries, &ring_, 0);
+    int const result = io_uring_queue_init_params(entries, &ring_, &params_);
     MONAD_ASSERT(!result);
 }
 
