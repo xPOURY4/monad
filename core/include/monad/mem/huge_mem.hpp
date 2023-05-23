27186046711110d91b4effdf9ee7fbc6a947ea1b
James Hunsaker <hunsaker@spacetime.org>
2023-05-23 16:11:32 -0400

    format, size_t in global

--- core/include/monad/mem/huge_mem.hpp
+++ core/include/monad/mem/huge_mem.hpp
@@ -8,18 +8,24 @@ MONAD_NAMESPACE_BEGIN
 
 class HugeMem final
 {
-    std::size_t const size_;
+    size_t const size_;
     unsigned char *const data_;
 
 public:
-    HugeMem(std::size_t size);
+    HugeMem(size_t size);
     ~HugeMem();
 
     HugeMem(HugeMem &&) = default;
 
-    [[gnu::always_inline]] std::size_t get_size() const { return size_; }
+    [[gnu::always_inline]] size_t get_size() const
+    {
+        return size_;
+    }
 
-    [[gnu::always_inline]] unsigned char *get_data() const { return data_; }
+    [[gnu::always_inline]] unsigned char *get_data() const
+    {
+        return data_;
+    }
 };
 
 static_assert(sizeof(HugeMem) == 16);
