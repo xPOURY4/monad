a820d70bdfc9b59d6418799347c56ed841fe5625
James Hunsaker <hunsaker@spacetime.org>
2023-06-05 15:25:33 -0400

    copy disas tests from old repo

---
+++ core/monad/tests/test_disas.py
@@ -0,0 +1,141 @@
+import os
+import re
+from glob import glob
+from subprocess import check_output
+
+__this_file = os.path.realpath(__file__)
+__this_dir = os.path.dirname(__this_file)
+__test_dir = os.path.join(__this_dir, "disas")
+
+TARGET_REGEX = "((?:call|ja|jae|jb|jbe|je|jmp|jne|js)[ ]+)0x[0-9a-f]+[ ]+"
+TARGET_PATTERN = re.compile(TARGET_REGEX)
+
+
+def _get_test_ids():
+
+    test_ids = []
+    files = glob(os.path.join(__test_dir, "*.py"))
+    for file in files:
+        file = os.path.basename(file)
+        if file.startswith("_"):
+            continue
+        test_id = file[0:-3]
+        test_ids.append(test_id)
+    return test_ids
+
+
+def _create_test_class():
+    class TestDisas:
+        @staticmethod
+        def _load_test(test_id):
+
+            test_file = os.path.join(TestDisas._test_dir, "%s.py" % (test_id,))
+            ns = {}
+            with open(test_file, "r") as f:
+                code = compile(f.read(), test_file, "exec")
+                exec(code, ns, ns)
+            obj = ns.get("obj")
+            syms = ns.get("syms")
+            assert isinstance(obj, (str,))
+            assert isinstance(syms, (list,))
+            for sym in syms:
+                assert isinstance(sym, (str,))
+            return obj, syms
+
+        @staticmethod
+        def _load_result(test_id):
+
+            result_file = os.path.join(TestDisas._test_dir, "%s.dis" % (test_id,))
+            with open(result_file, "r") as f:
+                result = f.read()
+            return result
+
+        @staticmethod
+        def _gen_result(test_id):
+
+            obj, syms = TestDisas._load_test(test_id)
+            cmd = [
+                "gdb",
+                "-batch",
+                "-ex",
+                "file %s" % (obj,),
+            ]
+            for sym in syms:
+                cmd += [
+                    "-ex",
+                    "disas '%s'" % (sym,),
+                ]
+            result = check_output(cmd)
+            result = result.decode("ascii")
+            result = result.splitlines()
+
+            def remove_address(line: str):
+                line = line.expandtabs()
+                if not line.startswith("   0x"):
+                    return line
+                line = line[22:]
+                assert line[0] == "<"
+                return line
+
+            result = [remove_address(line) for line in result]
+
+            def remove_target(line: str):
+                return TestDisas._target_pattern.sub("\\1", line, count=1)
+
+            result = [remove_target(line) for line in result]
+
+            def gdb_10_to_12(line: str):  # TODO
+                return line.replace("nopw   %cs:0", "cs nopw 0")
+
+            result = [gdb_10_to_12(line) for line in result]
+            result = "\n".join(result)
+            return result
+
+        @staticmethod
+        def _save_result(test_id):
+
+            result = TestDisas._gen_result(test_id)
+            result_file = os.path.join(TestDisas._test_dir, "%s.dis" % (test_id,))
+            with open(result_file, "w") as f:
+                f.write(result)
+
+        @staticmethod
+        def _run_test(test_id):
+
+            expected_result = TestDisas._load_result(test_id)
+            current_result = TestDisas._gen_result(test_id)
+            assert expected_result == current_result
+
+    setattr(TestDisas, "_test_dir", __test_dir)
+    setattr(TestDisas, "_target_pattern", TARGET_PATTERN)
+
+    test_ids = _get_test_ids()
+    for test_id in test_ids:
+
+        def test_func(self):
+
+            TestDisas._run_test(test_id)
+
+        setattr(TestDisas, "test_%s" % (test_id,), test_func)
+
+    return TestDisas
+
+
+TestDisas = _create_test_class()
+
+
+def main():
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", choices=("generate",))
+    args = parser.parse_args()
+    if args.cmd == "generate":
+        test_ids = _get_test_ids()
+        for test_id in test_ids:
+            TestDisas._save_result(test_id)
+
+
+if __name__ == "__main__":
+    main()
