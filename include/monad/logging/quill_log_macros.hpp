956f36b1caf99f1b680e054335c58ad9a8fbed6c
Tong Zhi <tzhi@monad.xyz>
2023-06-09 19:38:42 -0400

    :green_heart: Fix gcc build issue
    Problem:
    - Gcc build fails due to the clang pragma & macro definition
    
    Solution:
    - Add preprocessor logic to determine if clang is the compiler
    - Add newline at the end of macro defs

--- include/monad/logging/quill_log_macros.hpp
+++ include/monad/logging/quill_log_macros.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
-
 #define MONAD_LOG_TRACE_L3(logger, fmt, ...) QUILL_LOG_TRACE_L3(logger, fmt, ##__VA_ARGS__)
 #define MONAD_LOG_TRACE_L2(logger, fmt, ...) QUILL_LOG_TRACE_L2(logger, fmt, ##__VA_ARGS__)
 #define MONAD_LOG_TRACE_L1(logger, fmt, ...) QUILL_LOG_TRACE_L1(logger, fmt, ##__VA_ARGS__)
@@ -11,5 +8,3 @@
 #define MONAD_LOG_WARNING(logger, fmt, ...) QUILL_LOG_WARNING(logger, fmt, ##__VA_ARGS__)
 #define MONAD_LOG_ERROR(logger, fmt, ...) QUILL_LOG_ERROR(logger, fmt, ##__VA_ARGS__)
 #define MONAD_LOG_CRITICAL(logger, fmt, ...) QUILL_LOG_CRITICAL(logger, fmt, ##__VA_ARGS__)
-
-#pragma clang diagnostic pop
