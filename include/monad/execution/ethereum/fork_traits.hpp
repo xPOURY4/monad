131aa51a8e2e540a006ac073282419bda43d55ad
Rene Garcia <rgarc@alum.mit.edu>
2023-06-13 19:28:58 -0400

    :art: Refactor Byzantium precompiles list
    Problem: The previous definition had the same first four precompiles as previous forks.
    Solution: Express the list of Byzantium precompiles using type list operations from boost::mp11.

--- include/monad/execution/ethereum/fork_traits.hpp
+++ include/monad/execution/ethereum/fork_traits.hpp
@@ -256,11 +256,16 @@ namespace fork_traits
         static constexpr evmc_revision rev = EVMC_BYZANTIUM;
         static constexpr auto last_block_number = 9'068'999u;
 
-        using static_precompiles_t = type_list_t<
-            byzantium, contracts::EllipticCurveRecover, contracts::Sha256Hash,
-            contracts::Ripemd160Hash, contracts::Identity,
-            contracts::ModularExponentiation, contracts::BNAdd,
-            contracts::BNMultiply, contracts::BNPairing>;
+        template <typename TList>
+        using switch_fork_t = boost::mp11::mp_replace_front<TList, byzantium>;
+
+        using static_precompiles_t = boost::mp11::mp_append<
+            boost::mp11::mp_transform<
+                switch_fork_t, homestead::static_precompiles_t>,
+            type_list_t<
+                byzantium, contracts::ModularExponentiation, contracts::BNAdd,
+                contracts::BNMultiply, contracts::BNPairing>>;
+
         static_assert(boost::mp11::mp_size<static_precompiles_t>() == 8);
 
         template <class TState>
