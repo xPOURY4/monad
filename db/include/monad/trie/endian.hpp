f9f6852576e2eb9e222804ab99cc3f8d89157eac
James Hunsaker <hunsaker@spacetime.org>
2023-06-13 14:12:15 -0400

    remove unused core files

--- db/include/monad/trie/endian.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <bit>
-
-static_assert(
-    std::endian::native == std::endian::little ||
-    std::endian::native == std::endian::big);
--- db/include/monad/trie/likely.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define MONAD_TRIE_LIKELY(x) __builtin_expect(!!(x), 1)
-#define MONAD_TRIE_UNLIKELY(x) __builtin_expect(!!(x), 0)
