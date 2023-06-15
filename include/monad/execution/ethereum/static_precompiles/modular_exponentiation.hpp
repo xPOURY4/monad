87eb1eb3ae20e5ebdcf33eeaecbfed696edeabda
Rene Garcia <rgarc@alum.mit.edu>
2023-06-15 11:39:45 -0400

    :white_check_mark: Add test cases and implement modular exponentiation.
    Problem:
    - We don't have a basic test suite for the modular exponentiation precompile.
    - In the previous commit, we refactored the Byzantium precompile list to use type list operations.
    
    Solution:
    - Port a test suite for the modular exponentiation precompile from go-ethereum into our repo.
    - Add a compile time check to assert that the type list we ended up with in the previous commit is the same as if we had just written all the precompiles in a type list.
    - Implement the modular exponentiation precompile by thinly wrapping silkpre.

--- include/monad/execution/ethereum/static_precompiles/modular_exponentiation.hpp
+++ include/monad/execution/ethereum/static_precompiles/modular_exponentiation.hpp
@@ -2,6 +2,7 @@
 
 #include <monad/core/concepts.hpp>
 #include <monad/execution/ethereum/config.hpp>
+#include <silkpre/precompile.h>
 
 MONAD_EXECUTION_ETHEREUM_NAMESPACE_BEGIN
 
@@ -10,7 +11,42 @@ namespace static_precompiles
     template <class TFork>
     struct ModularExponentiation
     {
-        static evmc::Result execute(const evmc_message &m) noexcept;
+        static evmc::Result execute(const evmc_message &message) noexcept
+        {
+            // TODO: In the future, we can template parameterize the call to
+            // `silkpre_expmod_gas` over the revision instead of passing it in
+            // as an argument, but that would involve physically forking the
+            // implementation.
+            auto const unsigned_required_gas = silkpre_expmod_gas(
+                message.input_data, message.input_size, TFork::rev);
+
+            // `silkpre_expmod_gas` uses UINT64_MAX as a sentinel for out of gas
+            if (unsigned_required_gas > std::numeric_limits<int64_t>::max()) {
+                return evmc::Result{evmc_result{
+                    .status_code = evmc_status_code::EVMC_OUT_OF_GAS}};
+            }
+
+            auto const signed_required_gas =
+                static_cast<int64_t>(unsigned_required_gas);
+
+            if (message.gas < signed_required_gas) {
+                return evmc::Result{evmc_result{
+                    .status_code = evmc_status_code::EVMC_OUT_OF_GAS}};
+            }
+
+            auto const result =
+                silkpre_expmod_run(message.input_data, message.input_size);
+
+            return evmc::Result{evmc_result{
+                .status_code = evmc_status_code::EVMC_SUCCESS,
+                .gas_left = message.gas - signed_required_gas,
+                .output_data = result.data,
+                .output_size = result.size,
+                // `silkpre_expmod_run` allocates a buffer with malloc to
+                // contain the result, so it is crucial to set the release
+                // member variable
+                .release = evmc_free_result_memory}};
+        }
     };
 }
 
