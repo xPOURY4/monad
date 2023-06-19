6cc51752d766bab580337bca89701f7852b68bc8
Rene Garcia <rgarc@alum.mit.edu>
2023-06-19 12:20:10 -0400

    :white_check_mark: Add tests and implement blake2f.
    Problem:
    - We need a test suite for the blake2f precompile contract.
    - The gas checking code was incorrectly testing gas_left if the precompile call ended with EVMC_PRECOMPILE_FAILURE.
    
    Solution:
    - Port the respective test suite from the geth codebase.
    - Implement the blake2f precompile by thinly wrapping silkpre.
    - Fix the gas checking code to check for gas_left == 0 if the precompile ends with status code EVMC_PRECOMPILE_FAILURE.

--- include/monad/execution/ethereum/static_precompiles/blake2f.hpp
+++ include/monad/execution/ethereum/static_precompiles/blake2f.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <monad/execution/ethereum/config.hpp>
-#include <monad/core/concepts.hpp>
+#include <silkpre/precompile.h>
 
 MONAD_EXECUTION_ETHEREUM_NAMESPACE_BEGIN
 
@@ -10,7 +10,41 @@ namespace static_precompiles
     template <class TFork>
     struct Blake2F
     {
-        static evmc::Result execute(const evmc_message &m) noexcept;
+        static evmc::Result execute(const evmc_message &message) noexcept
+        {
+            auto const unsigned_cost = silkpre_blake2_f_gas(
+                message.input_data, message.input_size, TFork::rev);
+
+            if (unsigned_cost > std::numeric_limits<int64_t>::max()) {
+                return evmc::Result{evmc_result{
+                    .status_code = evmc_status_code::EVMC_PRECOMPILE_FAILURE}};
+            }
+
+            auto const signed_cost = static_cast<int64_t>(unsigned_cost);
+
+            if (message.gas < signed_cost) {
+                return evmc::Result{evmc_result{
+                    .status_code = evmc_status_code::EVMC_OUT_OF_GAS}};
+            }
+
+            auto const result =
+                silkpre_blake2_f_run(message.input_data, message.input_size);
+
+            if (!result.data) {
+                return evmc::Result{evmc_result{
+                    .status_code = evmc_status_code::EVMC_PRECOMPILE_FAILURE}};
+            }
+
+            return evmc::Result{evmc_result{
+                .status_code = evmc_status_code::EVMC_SUCCESS,
+                .gas_left = message.gas - signed_cost,
+                .output_data = result.data,
+                .output_size = result.size,
+                // In the success case, `silkpre_blake2_f_run` allocates a
+                // buffer with malloc to contain the result, so it is crucial to
+                // set the release member variable
+                .release = evmc_free_result_memory}};
+        }
     };
 }
 
