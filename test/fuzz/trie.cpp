f7c334ac01e1c2dfc287f11a211863519f02885e
Alexander Lee <alex@monad.xyz>
2023-06-23 11:05:43 -0400

    :heavy_plus_sign: Add google fuzztest
    Problem:
    - We would like a way to battle test various components in isolation
      from others
    
    Solution:
    - Introduce the fuzztest dependency to produce random inputs into
      these components

---
+++ test/fuzz/trie.cpp
@@ -0,0 +1,13 @@
+#include <fuzztest/fuzztest.h>
+#include <gtest/gtest.h>
+
+TEST(MyTestSuite, OnePlustTwoIsTwoPlusOne)
+{
+    EXPECT_EQ(1 + 2, 2 + 1);
+}
+
+void IntegerAdditionCommutes(int a, int b)
+{
+    EXPECT_EQ(a + b, b + a + 1);
+}
+FUZZ_TEST(MyTestSuite, IntegerAdditionCommutes);
