a1c312e5c11b7d18808fe9a05ecedce49797ba8d
Tong Zhi <tzhi@monad.xyz>
2023-06-30 15:27:22 -0400

    :new: :white_check_mark: Genesis tool

---
+++ include/monad/execution/ethereum/genesis.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <monad/execution/ethereum/config.hpp>
+
+#include <monad/core/account.hpp>
+#include <monad/core/address.hpp>
+#include <monad/core/assert.h>
+#include <monad/core/block.hpp>
+#include <monad/core/byte_string.hpp>
+#include <monad/core/bytes.hpp>
+
+#include <evmc/hex.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <intx/intx.hpp>
+
+#include <algorithm>
+#include <fstream>
+#include <string>
+
+MONAD_EXECUTION_ETHEREUM_NAMESPACE_BEGIN
+
+// TODO: Different chain_id has different genesis json file (with some of them
+// not having certain field)
+// Issue #131
+inline BlockHeader
+read_genesis_blockheader(nlohmann::json const &genesis_json)
+{
+    BlockHeader block_header{};
+
+    block_header.difficulty = intx::from_string<uint256_t>(
+        genesis_json["difficulty"].get<std::string>());
+
+    auto const extra_data =
+        evmc::from_hex(genesis_json["extraData"].get<std::string>());
+    MONAD_ASSERT(extra_data.has_value());
+    block_header.extra_data = extra_data.value();
+
+    block_header.gas_limit =
+        std::stoull(genesis_json["gasLimit"].get<std::string>(), nullptr, 0);
+
+    auto const mix_hash_byte_string =
+        evmc::from_hex(genesis_json["mixHash"].get<std::string>());
+    MONAD_ASSERT(mix_hash_byte_string.has_value());
+    std::copy_n(
+        mix_hash_byte_string.value().begin(),
+        mix_hash_byte_string.value().length(),
+        block_header.mix_hash.bytes);
+
+    uint64_t const nonce{
+        std::stoull(genesis_json["nonce"].get<std::string>(), nullptr, 0)};
+    intx::be::unsafe::store<uint64_t>(block_header.nonce.data(), nonce);
+
+    auto const parent_hash_byte_string =
+        evmc::from_hex(genesis_json["parentHash"].get<std::string>());
+    MONAD_ASSERT(parent_hash_byte_string.has_value());
+    std::copy_n(
+        parent_hash_byte_string.value().begin(),
+        parent_hash_byte_string.value().length(),
+        block_header.parent_hash.bytes);
+
+    block_header.timestamp =
+        std::stoull(genesis_json["timestamp"].get<std::string>(), nullptr, 0);
+
+    return block_header;
+}
+
+template <typename TStateDB>
+inline void read_genesis_state(nlohmann::json const &genesis_json, TStateDB &db)
+{
+    for (auto const &account_info : genesis_json["alloc"].items()) {
+        address_t address{};
+        auto const address_byte_string =
+            evmc::from_hex("0x" + account_info.key());
+        MONAD_ASSERT(address_byte_string.has_value());
+        std::copy_n(
+            address_byte_string.value().begin(),
+            address_byte_string.value().length(),
+            address.bytes);
+
+        Account account{};
+        auto const balance_byte_string =
+            account_info.value()["wei_balance"].get<std::string>();
+        account.balance = intx::from_string<uint256_t>(balance_byte_string);
+        account.nonce = 0u;
+
+        db.create(address, account);
+    }
+    db.commit();
+}
+
+template <typename TStateDB>
+inline BlockHeader
+read_genesis(std::filesystem::path const &genesis_file, TStateDB &db)
+{
+    std::ifstream ifile(genesis_file.c_str());
+    auto const genesis_json = nlohmann::json::parse(ifile);
+    auto block_header = read_genesis_blockheader(genesis_json);
+
+    block_header.ommers_hash = NULL_LIST_HASH;
+    block_header.transactions_root = NULL_ROOT;
+    block_header.receipts_root = NULL_ROOT;
+
+    read_genesis_state(genesis_json, db);
+
+    block_header.state_root = db.root_hash();
+
+    return block_header;
+}
+
+MONAD_EXECUTION_ETHEREUM_NAMESPACE_END
---
+++ src/monad/execution/ethereum/test/genesis.cpp
@@ -0,0 +1,102 @@
+#include <monad/core/block.hpp>
+
+#include <monad/db/trie_db.hpp>
+
+#include <monad/execution/ethereum/genesis.hpp>
+
+#include <gtest/gtest.h>
+
+#include <nlohmann/json.hpp>
+
+#include <test_resource_data.h>
+
+#include <fstream>
+
+using namespace monad;
+using namespace monad::execution;
+using namespace monad::execution::ethereum;
+
+template <typename TDB>
+struct GenesisStateTest : public testing::Test
+{
+};
+
+template <typename TDB>
+struct GenesisStateRootTest : public testing::Test
+{
+};
+
+using NoTrieDBTypes = ::testing::Types<db::InMemoryDB, db::RocksDB>;
+using TrieDBTypes = ::testing::Types<db::InMemoryTrieDB, db::RocksTrieDB>;
+
+TYPED_TEST_SUITE(GenesisStateTest, NoTrieDBTypes);
+TYPED_TEST_SUITE(GenesisStateRootTest, TrieDBTypes);
+
+TEST(Genesis, read_ethereum_mainnet_genesis_header)
+{
+    auto const genesis_file_path =
+        test_resource::ethereum_genesis_dir / "mainnet.json";
+
+    std::ifstream ifile(genesis_file_path.c_str());
+    auto const genesis_json = nlohmann::json::parse(ifile);
+    auto const block_header = read_genesis_blockheader(genesis_json);
+
+    EXPECT_EQ(block_header.difficulty, 17179869184);
+    auto const extra_data = byte_string(
+        {0x11, 0xbb, 0xe8, 0xdb, 0x4e, 0x34, 0x7b, 0x4e, 0x8c, 0x93, 0x7c,
+         0x1c, 0x83, 0x70, 0xe4, 0xb5, 0xed, 0x33, 0xad, 0xb3, 0xdb, 0x69,
+         0xcb, 0xdb, 0x7a, 0x38, 0xe1, 0xe5, 0x0b, 0x1b, 0x82, 0xfa});
+
+    EXPECT_EQ(block_header.extra_data, extra_data);
+    EXPECT_EQ(block_header.gas_limit, 5000);
+    EXPECT_EQ(
+        block_header.mix_hash,
+        0x0000000000000000000000000000000000000000000000000000000000000000_bytes32);
+    EXPECT_EQ(
+        block_header.nonce,
+        byte_string_fixed<8UL>(
+            {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42}));
+    EXPECT_EQ(
+        block_header.parent_hash,
+        0x0000000000000000000000000000000000000000000000000000000000000000_bytes32);
+    EXPECT_EQ(block_header.timestamp, 0);
+}
+
+TYPED_TEST(GenesisStateTest, read_ethereum_mainnet_genesis_state)
+{
+    using namespace intx;
+
+    auto const genesis_file_path =
+        test_resource::ethereum_genesis_dir / "mainnet.json";
+    TypeParam db;
+
+    std::ifstream ifile(genesis_file_path.c_str());
+    auto const genesis_json = nlohmann::json::parse(ifile);
+    read_genesis_state(genesis_json, db);
+
+    address_t a1 = 0x3282791d6fd713f1e94f4bfd565eaa78b3a0599d_address;
+    Account acct1{.balance = 0x487A9A304539440000_u256, .nonce = 0u};
+
+    address_t a2 = 0x08411652c871713609af0062a8a1281bf1bbcfd9_address;
+    Account acct2{.balance = 0x4BE4E7267B6AE00000_u256, .nonce = 0u};
+
+    EXPECT_TRUE(db.contains(a1));
+    EXPECT_EQ(db.at(a1), acct1);
+
+    EXPECT_TRUE(db.contains(a2));
+    EXPECT_EQ(db.at(a2), acct2);
+}
+
+TYPED_TEST(GenesisStateRootTest, ethereum_mainnet_genesis_state_root)
+{
+    auto const genesis_file_path =
+        test_resource::ethereum_genesis_dir / "mainnet.json";
+    TypeParam db;
+
+    auto const block_header = read_genesis(genesis_file_path, db);
+
+    // https://etherscan.io/block/0
+    auto expected_state_root{
+        0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544_bytes32};
+    EXPECT_EQ(block_header.state_root, expected_state_root);
+}
