483ec0d5015d5a7b1b95f9ac2b1f04553ace09d4
Tong Zhi <tzhi@monad.xyz>
2023-07-05 16:52:17 -0400

    :new: Add Null List Hash value

--- include/monad/core/bytes.hpp
+++ include/monad/core/bytes.hpp
@@ -12,9 +12,14 @@ static_assert(sizeof(bytes32_t) == 32);
 static_assert(alignof(bytes32_t) == 1);
 
 using namespace evmc::literals;
-inline constexpr bytes32_t NULL_HASH{0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470_bytes32};
+inline constexpr bytes32_t NULL_HASH{
+    0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470_bytes32};
+
+inline constexpr bytes32_t NULL_LIST_HASH{
+    0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347_bytes32};
 
 // Root hash of an empty trie
-inline constexpr bytes32_t NULL_ROOT{0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421_bytes32};
+inline constexpr bytes32_t NULL_ROOT{
+    0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421_bytes32};
 
 MONAD_NAMESPACE_END
