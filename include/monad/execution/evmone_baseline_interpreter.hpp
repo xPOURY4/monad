bea9a5aae14abff495cb12020cd9ac2f105cd278
Rene Garcia <rgarc@alum.mit.edu>
2023-08-01 00:10:02 -0400

    :loud_sound: Enable VM tracing on debug mode.
    Problem:
    - The evmone interpreter ships with instruction-level tracing that is
      disabled by default (presumably for performance reasons).
    - Having this tracing facilitates debugging the execution of
      transactions.
    
    Solution:
    - Enable VM tracing by calling `add_tracing` with an instruction tracer
      conditioned on whether or not the program was compiled with
      `EVMONE_TRACING` enabled.

--- include/monad/execution/evmone_baseline_interpreter.hpp
+++ include/monad/execution/evmone_baseline_interpreter.hpp
@@ -4,6 +4,7 @@
 #include <monad/core/concepts.hpp>
 
 #include <monad/execution/config.hpp>
+#include <monad/logging/monad_log.hpp>
 
 #include <evmone/baseline.hpp>
 
@@ -18,6 +19,11 @@
 
 #include <evmone/vm.hpp>
 
+#ifdef EVMONE_TRACING
+    #include <evmone/tracing.hpp>
+    #include <sstream>
+#endif
+
 MONAD_EXECUTION_NAMESPACE_BEGIN
 
 template <class TState, concepts::fork_traits<TState> TTraits>
@@ -27,23 +33,32 @@ struct EVMOneBaselineInterpreter
     static evmc::Result
     execute(TEvmHost *h, TState const &s, evmc_message const &m)
     {
-        evmc::Result result{evmc_result{.status_code = EVMC_SUCCESS, .gas_left = m.gas}};
+        [[maybe_unused]] decltype(monad::log::logger_t::get_logger()) logger =
+            monad::log::logger_t::get_logger(
+                "evmone_baseline_interpreter_logger");
+        evmc::Result result{
+            evmc_result{.status_code = EVMC_SUCCESS, .gas_left = m.gas}};
         auto const code = s.get_code(m.code_address);
         if (code.empty()) {
             return result;
         }
 
         evmone::VM v{};
+#ifdef EVMONE_TRACING
+        std::ostringstream instruction_trace_string_stream;
+        v.add_tracer(
+            evmone::create_instruction_tracer(instruction_trace_string_stream));
+#endif
+
         evmone::ExecutionState es{
-            m,
-            TTraits::rev,
-            h->get_interface(),
-            h->to_context(),
-            code,
-            {}};
+            m, TTraits::rev, h->get_interface(), h->to_context(), code, {}};
         evmone::baseline::CodeAnalysis ca{
             evmone::baseline::analyze(TTraits::rev, code)};
         result = evmc::Result{evmone::baseline::execute(v, m.gas, es, ca)};
+
+#ifdef EVMONE_TRACING
+        MONAD_LOG_DEBUG(logger, "{}", instruction_trace_string_stream.str());
+#endif
         return result;
     }
 };
