348588ad9022f2ea4d74e32078f86dd674ba070b
Niall Douglas <niall@monad.xyz>
2023-08-03 18:05:04 +0100

    Thanks to suggestion by @Chen-Yifan actually test assumptions behind metaprogrammed limits on dense and flat map value sizes, and tweak to match empirical results.
    Also add Bob Jenkins' small PRNG as it is useful for writing
    test code.

--- core/test/unordered_map.cpp
+++ core/test/unordered_map.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
+#include <monad/core/small_prng.hpp>
 #include <monad/core/unordered_map.hpp>
+
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 
 TEST(UnorderedNodeMap, works)
 {
@@ -48,4 +55,84 @@ TEST(UnorderedFlatSet, works)
     unordered_flat_set<int> set;
     set.insert(5);
     EXPECT_TRUE(set.contains(5));
+}
+
+template <size_t count>
+struct bytes
+{
+    char v[count]{};
+    bytes(uint32_t x)
+    {
+        memcpy(v, &x, sizeof(x));
+    }
+    bool operator==(const bytes &o) const noexcept
+    {
+        return 0 == memcmp(v, o.v, count);
+    }
+};
+struct hasher
+{
+    template <class T>
+    size_t operator()(const T &v) const noexcept
+    {
+        return MONAD_NAMESPACE::hash_bytes(v.v, sizeof(v.v));
+    }
+};
+template <class T>
+struct tag
+{
+    using type = T;
+};
+
+TEST(UnorderedSets, quick_comparative_benchmark)
+{
+    if (getenv("CI") != nullptr) {
+        return;
+    }
+    using namespace MONAD_NAMESPACE;
+    std::vector<uint32_t> values(5000000);
+    small_prng rand;
+    for (auto &i : values) {
+        i = rand();
+    }
+    auto do_test = [&](auto tag1, const char *desc) {
+        using T = typename decltype(tag1)::type;
+        auto do_cont = [&](auto tag2, const char *desc2) {
+            using cont = typename decltype(tag2)::type;
+            std::cout << "   Testing " << desc2 << " with " << desc << " ... "
+                      << std::flush;
+            auto begin = std::chrono::steady_clock::now();
+            {
+                cont c;
+                for (auto i : values) {
+                    c.insert(i);
+                }
+                for (size_t n = 0; n < 10; n++) {
+                    for (auto i : values) {
+                        volatile auto it = c.find(i);
+                        (void)it;
+                    }
+                }
+            }
+            auto end = std::chrono::steady_clock::now();
+            auto diff = std::chrono::duration_cast<std::chrono::microseconds>(
+                            end - begin)
+                            .count() /
+                        1000000.0;
+            std::cout << diff << std::endl;
+        };
+        do_cont(tag<std::unordered_set<T, hasher>>{}, "std::unordered_set");
+        do_cont(tag<unordered_node_set<T, hasher>>{}, "unordered_node_set");
+        if constexpr (sizeof(T) <= 384) {
+            do_cont(
+                tag<unordered_dense_set<T, hasher>>{}, "unordered_dense_set");
+        }
+        if constexpr (sizeof(T) <= 48) {
+            do_cont(tag<unordered_flat_set<T, hasher>>{}, "unordered_flat_set");
+        }
+    };
+    do_test(tag<bytes<16>>{}, "16 byte values");
+    do_test(tag<bytes<64>>{}, "64 byte values");
+    do_test(tag<bytes<256>>{}, "256 byte values");
+    do_test(tag<bytes<512>>{}, "512 byte values");
 }
