0aff1ee832a81c89e3ebb8932f28f7b6e8ea7134
James Hunsaker <hunsaker@spacetime.org>
2023-08-21 11:53:27 -0400

    db reader interface

---
+++ include/monad/db/db.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <monad/config.hpp>
+#include <monad/core/account.hpp>
+#include <monad/core/address.hpp>
+#include <monad/core/byte_string.hpp>
+#include <monad/core/bytes.hpp>
+
+#include <cstdint>
+#include <optional>
+
+MONAD_NAMESPACE_BEGIN
+
+struct Db
+{
+    virtual std::optional<Account> read_account(address_t const &) = 0;
+
+    virtual bytes32_t read_storage(
+        address_t const &, uint64_t incarnation, bytes32_t const &location) = 0;
+
+    virtual byte_string read_code(bytes32_t const &) = 0;
+};
+
+MONAD_NAMESPACE_END
