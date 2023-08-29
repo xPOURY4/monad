98ab556d4a0039344c4a12b079874ca81e2896c4
Rene Garcia <rgarc@alum.mit.edu>
2023-08-29 13:50:25 -0400

    :new: Serialize state from database to JSON.

---
+++ test/unit/common/include/monad/test/dump_state_from_db.hpp
@@ -0,0 +1,266 @@
+#include <monad/db/in_memory_db.hpp>
+#include <monad/db/in_memory_trie_db.hpp>
+#include <monad/db/rocks_db.hpp>
+#include <monad/db/rocks_trie_db.hpp>
+#include <monad/logging/formatter.hpp>
+#include <monad/test/config.hpp>
+
+MONAD_TEST_NAMESPACE_BEGIN
+
+namespace detail
+{
+    template <typename THasBytes>
+    [[nodiscard]] inline bytes32_t hash(THasBytes const &hashable)
+    {
+        return std::bit_cast<bytes32_t>(
+            ethash::keccak256(hashable.bytes, sizeof(hashable.bytes)));
+    }
+
+    template <typename TDatabase, typename TCursor>
+    void dump_accounts_from_trie(
+        nlohmann::json &state, TDatabase const &db,
+        trie::Nibbles const &hashed_account_address, TCursor &leaf_cursor,
+        TCursor &trie_cursor)
+    {
+        auto const maybe_account = monad::trie_db_read_account(
+            hashed_account_address, leaf_cursor, trie_cursor);
+
+        MONAD_ASSERT(maybe_account.has_value());
+        auto const &account = maybe_account.value();
+
+        auto const keccaked_account_hex =
+            fmt::format("{}", hashed_account_address);
+
+        state[keccaked_account_hex]["balance"] =
+            "0x" + intx::to_string(account.balance, 16);
+
+        state[keccaked_account_hex]["nonce"] =
+            fmt::format("0x{:x}", account.nonce);
+
+        auto const code = db.read_code(account.code_hash);
+        state[keccaked_account_hex]["code"] = "0x" + evmc::hex(code);
+    }
+
+    template <typename TCursor>
+    void dump_storage_from_trie(
+        nlohmann::json &state, byte_string_view key_slice, TCursor &leaf_cursor,
+        TCursor &trie_cursor)
+    {
+        monad::address_t account_address;
+        std::memcpy(
+            &account_address, key_slice.data(), sizeof(monad::address_t));
+        key_slice.remove_prefix(sizeof(monad::address_t));
+
+        auto const [keccaked_storage_key_nibbles, num_bytes] =
+            monad::trie::deserialize_nibbles(key_slice);
+        MONAD_ASSERT(num_bytes == key_slice.size());
+
+        bytes32_t const storage_value =
+            monad::trie_db_read_storage_with_hashed_key(
+                account_address,
+                0u,
+                keccaked_storage_key_nibbles,
+                leaf_cursor,
+                trie_cursor);
+        auto const keccaked_account_address =
+            fmt::format("{}", hash(account_address));
+        state[keccaked_account_address]["storage"]["original_account_address"] =
+            fmt::format("{}", account_address);
+
+        auto const storage_key =
+            fmt::format("{}", keccaked_storage_key_nibbles);
+        state[keccaked_account_address]["storage"][storage_key] =
+            fmt::format("{}", storage_value);
+    }
+
+    template <typename TDatabase>
+    void dump_accounts_from_db(
+        TDatabase &db, nlohmann::json &state, address_t address,
+        Account const &account)
+    {
+        auto const keccaked_address_hex = fmt::format("{}", hash(address));
+
+        state[keccaked_address_hex]["balance"] =
+            "0x" + intx::to_string(account.balance, 16);
+
+        state[keccaked_address_hex]["nonce"] =
+            fmt::format("0x{:x}", account.nonce);
+
+        auto const code = db.read_code(account.code_hash);
+        state[keccaked_address_hex]["code"] = "0x" + evmc::hex(code);
+    }
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_accounts_from_db(monad::db::RocksTrieDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+
+    auto leaf_cursor = db.accounts_trie.make_leaf_cursor();
+    auto trie_cursor = db.accounts_trie.make_trie_cursor();
+
+    std::unique_ptr<rocksdb::Iterator> it{
+        db.db->NewIterator({}, db.accounts_trie.leaves_cursor.cf_)};
+
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        auto const hashed_account_address =
+            monad::trie::deserialize_nibbles(it->key());
+        detail::dump_accounts_from_trie(
+            state, db, hashed_account_address, leaf_cursor, trie_cursor);
+    }
+
+    return state;
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_storage_from_db(monad::db::RocksTrieDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    auto leaf_cursor = db.storage_trie.make_leaf_cursor();
+    auto trie_cursor = db.storage_trie.make_trie_cursor();
+
+    std::unique_ptr<rocksdb::Iterator> it{
+        db.db->NewIterator({}, db.storage_trie.leaves_cursor.cf_)};
+
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        auto key_slice = monad::db::from_slice(it->key());
+        detail::dump_storage_from_trie(
+            state, key_slice, leaf_cursor, trie_cursor);
+    }
+    return state;
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_accounts_from_db(monad::db::InMemoryDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    for (auto const &[address, account] : db.accounts) {
+        detail::dump_accounts_from_db(db, state, address, account);
+    }
+    return state;
+}
+[[nodiscard]] inline nlohmann::json
+dump_storage_from_db(monad::db::InMemoryDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    for (auto const &[address, storage] : db.storage) {
+        auto const keccaked_address_hex =
+            fmt::format("{}", detail::hash(address));
+        state[keccaked_address_hex]["storage"]["original_account_address"] =
+            fmt::format("{}", address);
+        for (auto const &[key, value] : storage) {
+            auto const keccaked_storage_address =
+                fmt::format("{}", detail::hash(key));
+            state[keccaked_address_hex]["storage"][keccaked_storage_address] =
+                fmt::format("{}", value);
+        }
+    }
+    return state;
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_accounts_from_db(monad::db::RocksDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    std::unique_ptr<rocksdb::Iterator> it{
+        db.db->NewIterator({}, db.accounts_cf())};
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        auto const key_view = monad::db::from_slice(it->key());
+
+        address_t address;
+        std::memcpy(&address, key_view.data(), sizeof(address_t));
+
+        auto const value = monad::db::from_slice(it->value());
+
+        Account account;
+        bytes32_t storage_root;
+        rlp::decode_account(account, storage_root, value);
+
+        detail::dump_accounts_from_db(db, state, address, account);
+    }
+    return state;
+}
+[[nodiscard]] inline nlohmann::json dump_storage_from_db(monad::db::RocksDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    std::unique_ptr<rocksdb::Iterator> it{
+        db.db->NewIterator({}, db.storage_cf())};
+
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        auto const key_slice = db::from_slice(it->key());
+        byte_string_view key_view{key_slice};
+        MONAD_ASSERT(key_view.size() == sizeof(address_t) + sizeof(bytes32_t));
+
+        monad::address_t address;
+        std::copy_n(key_view.data(), sizeof(address_t), address.bytes);
+        auto const keccaked_account_hex =
+            fmt::format("{}", detail::hash(address));
+        key_view.remove_prefix(sizeof(address_t));
+
+        bytes32_t storage_address;
+        std::copy_n(key_view.data(), sizeof(bytes32_t), storage_address.bytes);
+
+        bytes32_t keccaked_storage_key = detail::hash(storage_address);
+
+        auto const value_slice = db::from_slice(it->value());
+
+        bytes32_t storage_value;
+        std::copy_n(value_slice.data(), sizeof(bytes32_t), storage_value.bytes);
+
+        state[keccaked_account_hex]["storage"]["original_account_address"] =
+            fmt::format("{}", address);
+        state[keccaked_account_hex]["storage"]
+             [fmt::format("{}", keccaked_storage_key)] =
+                 fmt::format("{}", storage_value);
+    }
+
+    return state;
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_accounts_from_db(monad::db::InMemoryTrieDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+
+    auto leaf_cursor = db.accounts_trie.make_leaf_cursor();
+    auto trie_cursor = db.accounts_trie.make_trie_cursor();
+
+    for (auto const &[address, _] : db.accounts_trie.leaves_storage) {
+        auto const [hashed_account_address, size] =
+            monad::trie::deserialize_nibbles(address);
+        detail::dump_accounts_from_trie(
+            state, db, hashed_account_address, leaf_cursor, trie_cursor);
+    }
+
+    return state;
+}
+
+[[nodiscard]] inline nlohmann::json
+dump_storage_from_db(monad::db::InMemoryTrieDB &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    auto leaf_cursor = db.storage_trie.make_leaf_cursor();
+    auto trie_cursor = db.storage_trie.make_trie_cursor();
+
+    for (auto const &[key, value] : db.storage_trie.leaves_storage) {
+        monad::byte_string_view key_slice{key};
+        detail::dump_storage_from_trie(
+            state, key_slice, leaf_cursor, trie_cursor);
+    }
+    return state;
+}
+
+template <typename TDatabase>
+nlohmann::json dump_state_from_db(TDatabase &db)
+{
+    nlohmann::json state = nlohmann::json::object();
+    auto const accounts = dump_accounts_from_db(db);
+    auto const storage = dump_storage_from_db(db);
+
+    state.update(accounts, /*merge_objects=*/true);
+    state.update(storage, /*merge_objects=*/true);
+
+    return state;
+}
+
+MONAD_TEST_NAMESPACE_END
---
+++ test/unit/common/src/test/dump_state_from_db.cpp
@@ -0,0 +1,173 @@
+#include <gtest/gtest.h>
+
+#include <ethash/keccak.hpp>
+
+#include <monad/db/in_memory_db.hpp>
+#include <monad/db/in_memory_trie_db.hpp>
+#include <monad/db/rocks_db.hpp>
+#include <monad/db/rocks_trie_db.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <monad/test/dump_state_from_db.hpp>
+#include <monad/test/make_db.hpp>
+
+#include <monad/core/transaction.hpp>
+#include <monad/execution/test/fakes.hpp>
+#include <monad/state/code_state.hpp>
+using namespace monad;
+
+template <typename>
+struct StateSerialization : public testing::Test
+{
+};
+
+using DBTypes = ::testing::Types<
+    db::InMemoryDB, db::RocksDB, db::InMemoryTrieDB, db::RocksTrieDB>;
+
+TYPED_TEST_SUITE(StateSerialization, DBTypes);
+
+TYPED_TEST(StateSerialization, serialize_add)
+{
+    /* The following command was used as a post state to test against.
+    ./cmake-build-debug/test/ethereum_test/monad-ethereum-test \
+     --gtest_catch_exceptions=0 \
+     --gtest_filter="VMTests/vmArithmeticTest.add" \
+     --dump_state \
+     --fork Berlin --txn 0  \
+     log_levels \
+     --state debug \
+     --change_set debug \
+     --trie_db debug \
+     --evmone debug \
+     --ethereum_test debug
+     */
+    auto db = test::make_db<TypeParam>();
+    auto const expected_payload = nlohmann::json::parse(R"(
+{
+  "0x03601462093b5945d1676df093446790fd31b20e7b12a2e8e5e09d068109616b": {
+    "balance": "0xba1a9ce0b9aa781",
+    "code": "0x",
+    "nonce": "0x1"
+  },
+  "0x227a737497210f7cc2f464e3bfffadefa9806193ccdf873203cd91c8d3eab518": {
+    "balance": "0xba1a9ce0ba1a9ce",
+    "code": "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "nonce": "0x0",
+    "storage": {
+      "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563": "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe",
+      "original_account_address": "0x0000000000000000000000000000000000000100"
+    }
+  },
+  "0x4599828688a5c37132b6fc04e35760b4753ce68708a7b7d4d97b940047557fdb": {
+    "balance": "0xba1a9ce0ba1a9ce",
+    "code": "0x60047fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "nonce": "0x0"
+  },
+  "0x4c933a84259efbd4fb5d1522b5255e6118da186a2c71ec5efaa5c203067690b7": {
+    "balance": "0xba1a9ce0ba1a9ce",
+    "code": "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60010160005500",
+    "nonce": "0x0"
+  },
+  "0x9d860e7bb7e6b09b87ab7406933ef2980c19d7d0192d8939cf6dc6908a03305f": {
+    "balance": "0x7024c",
+    "code": "0x",
+    "nonce": "0x0"
+  },
+  "0xa17eacbc25cda025e81db9c5c62868822c73ce097cee2a63e33a2e41268358a1": {
+    "balance": "0xba1a9ce0ba1a9ce",
+    "code": "0x60017fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "nonce": "0x0"
+  },
+  "0xa5cc446814c4e9060f2ecb3be03085683a83230981ca8f19d35a4438f8c2d277": {
+    "balance": "0xba1a9ce0ba1a9ce",
+    "code": "0x600060000160005500",
+    "nonce": "0x0"
+  },
+  "0xf057b39b049c7df5dfa86c4b0869abe798cef059571a5a1e5bbf5168cf6c097b": {
+    "balance": "0xba1a9ce0ba1a9cf",
+    "code": "0x600060006000600060006004356101000162fffffff100",
+    "nonce": "0x0"
+  }
+})");
+    auto const a = 0x0000000000000000000000000000000000000100_address;
+    auto const a_code =
+        evmc::from_hex("7ffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+                       "fffffffffff7fffffffffffffffffffffffffffffffffffffffffff"
+                       "ffffffffffffffffffffff0160005500")
+            .value();
+    auto const a_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(a_code.data(), a_code.size()));
+    auto const b = 0x0000000000000000000000000000000000000101_address;
+    auto const b_code =
+        evmc::from_hex("60047ffffffffffffffffffffffffffffffffffffffffffffffffff"
+                       "fffffffffffffff0160005500")
+            .value();
+    auto const b_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(b_code.data(), b_code.size()));
+    auto const c = 0x0000000000000000000000000000000000000102_address;
+    auto const c_code =
+        evmc::from_hex("60017ffffffffffffffffffffffffffffffffffffffffffffffffff"
+                       "fffffffffffffff0160005500")
+            .value();
+    auto const c_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(c_code.data(), c_code.size()));
+    auto const d = 0x0000000000000000000000000000000000000103_address;
+    auto const d_code = evmc::from_hex("600060000160005500").value();
+    auto const d_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(d_code.data(), d_code.size()));
+    auto const e = 0x0000000000000000000000000000000000000104_address;
+    auto const e_code =
+        evmc::from_hex("7ffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+                       "fffffffffff60010160005500")
+            .value();
+    auto const e_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(e_code.data(), e_code.size()));
+    auto const f = 0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba_address;
+    auto const g = 0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b_address;
+    auto const h = 0xcccccccccccccccccccccccccccccccccccccccc_address;
+    auto const h_code =
+        evmc::from_hex("600060006000600060006004356101000162fffffff100")
+            .value();
+    auto const h_code_hash = std::bit_cast<bytes32_t>(
+        ethash::keccak256(h_code.data(), h_code.size()));
+
+    db.commit(monad::state::StateChanges{
+        .account_changes =
+            {{a,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9ce, .code_hash = a_code_hash}},
+             {b,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9ce, .code_hash = b_code_hash}},
+             {c,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9ce, .code_hash = c_code_hash}},
+             {d,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9ce, .code_hash = d_code_hash}},
+             {e,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9ce, .code_hash = e_code_hash}},
+             {f, monad::Account{.balance = 0x7024c}},
+             {g, monad::Account{.balance = 0xba1a9ce0b9aa781, .nonce = 1}},
+             {h,
+              monad::Account{
+                  .balance = 0xba1a9ce0ba1a9cf, .code_hash = h_code_hash}}},
+        .storage_changes =
+            {{a,
+              {{0x0000000000000000000000000000000000000000000000000000000000000000_bytes32,
+                0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe_bytes32}}}},
+        .code_changes = {
+            {a_code_hash, a_code},
+            {b_code_hash, b_code},
+            {c_code_hash, c_code},
+            {d_code_hash, d_code},
+            {e_code_hash, e_code},
+            {h_code_hash, h_code}}});
+
+    auto const actual_payload = monad::test::dump_state_from_db(db);
+    // performs a deep comparison:
+    // https://github.com/nlohmann/json/blob/6cc0eaf88f14493719c440dc5c343ee70aa5551f/include/nlohmann/json.hpp#L3687-L3698
+    EXPECT_EQ(expected_payload, actual_payload);
+}
