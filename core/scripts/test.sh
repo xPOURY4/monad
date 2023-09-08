76053d5a34ecd56dbe7cd2d32c7229794dfec51a
James Hunsaker <hunsaker@spacetime.org>
2023-09-08 14:05:57 -0400

    parallel ctest

--- core/scripts/test.sh
+++ core/scripts/test.sh
@@ -2,7 +2,8 @@
 
 set -Eeuo pipefail
 
-ctest --output-on-failure --test-dir build
+CTEST_PARALLEL_LEVEL=${CTEST_PARALLEL_LEVEL:-$(nproc)} \
+  ctest --output-on-failure --test-dir build
 
 if readelf -p .comment build/*.so | grep clang; then
   pytest --pyargs monad
