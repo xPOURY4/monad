b7220521e457d5c76b1decbfab0784ad8a7f7c6b
Rene Garcia <rgarc@alum.mit.edu>
2023-09-29 12:34:51 -0400

    :new: Add free function to break bytecode into list of instructions.
    :new: Parse contract into a control flow graph.
    
    :art: Remove unnecessary `monad::`.
    
    :art: Remove unnecessary compiler directives.
    
    :bug: Ignore maybe-uninitialized warning on GCC.

---
+++ include/monad/analysis/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <monad/config.hpp>
+
+#define MONAD_ANALYSIS_NAMESPACE_BEGIN                                         \
+    MONAD_NAMESPACE_BEGIN namespace analysis                                   \
+    {
+
+#define MONAD_ANALYSIS_NAMESPACE_END                                           \
+    }                                                                          \
+    MONAD_NAMESPACE_END
