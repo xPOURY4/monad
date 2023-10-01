4f4a6c0b9d8d77775f6e679b7091b6680c7eb431
James Hunsaker <hunsaker@spacetime.org>
2023-10-01 18:02:45 -0400

    list of packages as array, explicit deps with debuginfo

--- core/scripts/fedora/install_deps.sh
+++ core/scripts/fedora/install_deps.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
-yum -y install \
-  abseil-cpp \
-  abseil-cpp-debuginfo \
-  gmock \
-  gmock-debuginfo \
-  google-benchmark \
-  google-benchmark-debuginfo \
-  gtest \
-  gtest-debuginfo \
-  liburing \
-  liburing-debuginfo \
-  mimalloc \
+packages=(
+  abseil-cpp
+  abseil-cpp-debuginfo
+
+  boost-fiber
+  boost-fiber-debuginfo
+
+  glibc
+  glibc-debuginfo
+
+  glibc-common
+  glibc-common-debuginfo
+
+  gmock
+  gmock-debuginfo
+
+  google-benchmark
+  google-benchmark-debuginfo
+
+  gtest
+  gtest-debuginfo
+
+  libgcc
+  libgcc-debuginfo
+
+  libstdc++
+  libstdc++-debuginfo
+
+  liburing
+  liburing-debuginfo
+
+  mimalloc
   mimalloc-debuginfo
+)
+
+yum -y install "${packages[@]}"
