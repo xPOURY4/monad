63b069610ed4f355a6150dacabc8b9e7d8f8766a
James Hunsaker <hunsaker@spacetime.org>
2023-10-19 18:12:02 -0400

    remove hash cache from block db

--- include/monad/db/block_db.hpp
+++ include/monad/db/block_db.hpp
@@ -2,11 +2,9 @@
 
 #include <monad/config.hpp>
 #include <monad/core/block.hpp>
-#include <monad/core/bytes.hpp>
 
 #include <monad/db/file_db.hpp>
 
-#include <array>
 #include <filesystem>
 
 MONAD_NAMESPACE_BEGIN
@@ -26,43 +24,10 @@ public:
         DECODE_ERROR
     };
 
-    static constexpr auto null{
-        0x0000000000000000000000000000000000000000000000000000000000000000_bytes32};
-
     [[nodiscard]] Status get(block_num_t, Block &);
-    // Support BLOCKHASH opcode, See YP Sec. 12.2
-    [[nodiscard]] bytes32_t get_block_hash(block_num_t);
-
-    void store_current_block_hash(block_num_t) noexcept;
-    [[nodiscard]] Status get_past_into_block_cache(block_num_t);
-    [[nodiscard]] bool should_be_in_cache(block_num_t) const noexcept;
-
-    [[nodiscard]] bool is_next_block(block_num_t n) const noexcept
-    {
-        return n == (current_block_.value() + 1);
-    }
-
-    [[nodiscard]] block_num_t earliest_block_in_cache() const noexcept
-    {
-        return ((current_block_.value() - number_of_hashes) >
-                current_block_.value())
-                   ? 0u
-                   : (current_block_.value() - number_of_hashes);
-    }
-
-    [[nodiscard]] size_t write_index(block_num_t n) const noexcept
-    {
-        return n % number_of_hashes;
-    }
-
-    // YP Appendix H.2, 40s BLOCKHASH
-    static constexpr auto number_of_hashes{256u};
 
 private:
     FileDb db_;
-    std::array<bytes32_t, number_of_hashes> recent_hashes_{};
-    std::optional<block_num_t> current_block_{};
-    byte_string current_block_decoded_;
 };
 
 MONAD_NAMESPACE_END
--- src/monad/db/block_db.cpp
+++ src/monad/db/block_db.cpp
@@ -30,92 +30,14 @@ BlockDb::Status BlockDb::get(block_num_t const num, Block &block)
     if (brotli_result != BROTLI_DECODER_RESULT_SUCCESS) {
         return Status::DECOMPRESS_ERROR;
     }
-    current_block_decoded_.assign(brotli_buffer, brotli_size);
 
     auto const decoding_result =
-        rlp::decode_block(block, current_block_decoded_);
+        rlp::decode_block(block, {brotli_buffer, brotli_size});
     if (decoding_result.size() != 0) {
         return Status::DECODE_ERROR;
     }
 
-    if (should_be_in_cache(num)) {
-        current_block_ =
-            current_block_
-                ? (num > current_block_.value()) ? num : current_block_.value()
-                : num;
-    }
-
     return Status::SUCCESS;
-}
-
-BlockDb::Status BlockDb::get_past_into_block_cache(block_num_t const num)
-{
-    // Read the child and use the parent hash from that block
-    MONAD_DEBUG_ASSERT(num < current_block_);
-    Block b{};
-    auto const key = std::to_string(num + 1);
-    auto const result = db_.get(key.c_str());
-    if (!result.has_value()) {
-        return Status::NO_BLOCK_FOUND;
-    }
-    byte_string_view view{
-        reinterpret_cast<uint8_t const *>(result->data()), result->length()};
-
-    size_t brotli_size = 1'048'576u; // 1M
-
-    uint8_t brotli_buffer[1'048'567u];
-
-    auto brotli_result = BrotliDecoderDecompress(
-        view.length(), view.data(), &brotli_size, brotli_buffer);
-
-    if (brotli_result != BROTLI_DECODER_RESULT_SUCCESS) {
-        return Status::DECOMPRESS_ERROR;
-    }
-    byte_string_view view2{brotli_buffer, brotli_size};
-
-    auto const decoding_result = rlp::decode_block(b, view2);
-    if (decoding_result.size() != 0) {
-        return Status::DECODE_ERROR;
-    }
-
-    recent_hashes_[write_index(num)] =
-        std::bit_cast<bytes32_t>(b.header.parent_hash);
-
-    return Status::SUCCESS;
-}
-
-bool BlockDb::should_be_in_cache(block_num_t n) const noexcept
-{
-    if (!current_block_) {
-        return true;
-    }
-    if (n > current_block_.value()) {
-        MONAD_DEBUG_ASSERT(is_next_block(n));
-    }
-
-    return is_next_block(n) ||
-           ((earliest_block_in_cache() <= n) && (n <= current_block_.value()));
-}
-
-void BlockDb::store_current_block_hash(block_num_t n) noexcept
-{
-    auto const h = rlp::get_rlp_header_from_block(current_block_decoded_);
-
-    recent_hashes_[write_index(n)] =
-        std::bit_cast<bytes32_t>(ethash::keccak256(h.data(), h.size()));
-}
-
-[[nodiscard]] bytes32_t BlockDb::get_block_hash(block_num_t number)
-{
-    if (should_be_in_cache(number)) {
-        if (recent_hashes_[write_index(number)] == null) {
-            auto const status = get_past_into_block_cache(number);
-            MONAD_DEBUG_ASSERT(status == Status::SUCCESS);
-        }
-        return recent_hashes_[write_index(number)];
-    }
-    // YP Appendix H.2, 40s BLOCKHASH, 1st condition
-    return bytes32_t{};
 }
 
 MONAD_NAMESPACE_END
