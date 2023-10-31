fc893bb80a88e3c24e9d6550a8cb36e5649c24d9
James Hunsaker <hunsaker@spacetime.org>
2023-10-30 21:45:47 -0400

    reserve 4GB of 2MB pages as well

--- core/scripts/root/reserve_hugepages.sh
+++ core/scripts/root/reserve_hugepages.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo 4 > /sys/devices/system/node/node${1}/hugepages/hugepages-1048576kB/nr_hugepages
+echo 2000 > /sys/devices/system/node/node${1}/hugepages/hugepages-2048kB/nr_hugepages
