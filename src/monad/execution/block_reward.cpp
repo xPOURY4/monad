1984f2f0e8f9318feabffa3d55d77942be7da443
James Hunsaker <hunsaker@spacetime.org>
2023-11-14 02:08:26 -0500

    clean up block reward

--- src/monad/execution/block_reward.cpp
+++ src/monad/execution/block_reward.cpp
@@ -11,24 +11,28 @@
 
 #include <intx/intx.hpp>
 
+#include <cstddef>
 #include <cstdint>
 #include <limits>
 
 MONAD_NAMESPACE_BEGIN
 
 constexpr uint256_t calculate_block_reward(
-    Block const &block, uint256_t const &reward, uint256_t const &ommer_reward)
+    uint256_t const &reward, uint256_t const &ommer_reward,
+    size_t const ommers_size)
 {
     MONAD_DEBUG_ASSERT(
-        reward + intx::umul(ommer_reward, uint256_t{block.ommers.size()}) <=
-        std::numeric_limits<uint256_t>::max());
-    return reward + ommer_reward * block.ommers.size();
+        intx::umul(ommer_reward, uint256_t{ommers_size}) <=
+        std::numeric_limits<uint256_t>::max() - reward);
+
+    return reward + ommer_reward * ommers_size;
 }
 
 constexpr uint256_t const calculate_ommer_reward(
-    Block const &block, uint256_t const &reward, uint64_t ommer_number)
+    uint256_t const &reward, uint64_t const header_number,
+    uint64_t const ommer_number)
 {
-    auto const subtrahend = ((block.header.number - ommer_number) * reward) / 8;
+    auto const subtrahend = ((header_number - ommer_number) * reward) / 8;
     return reward - subtrahend;
 }
 
@@ -38,7 +42,7 @@ void apply_block_reward(
 {
     State state{block_state, db};
     auto const miner_reward =
-        calculate_block_reward(block, block_reward, ommer_reward);
+        calculate_block_reward(block_reward, ommer_reward, block.ommers.size());
 
     // reward block beneficiary, YP Eqn. 172
     if (MONAD_LIKELY(miner_reward)) {
@@ -46,13 +50,14 @@ void apply_block_reward(
     }
 
     // reward ommers, YP Eqn. 175
-    for (auto const &header : block.ommers) {
-        auto const ommer_reward =
-            calculate_ommer_reward(block, block_reward, header.number);
+    for (auto const &ommer : block.ommers) {
+        auto const ommer_reward = calculate_ommer_reward(
+            block_reward, block.header.number, ommer.number);
         if (MONAD_LIKELY(ommer_reward)) {
-            state.add_to_balance(header.beneficiary, ommer_reward);
+            state.add_to_balance(ommer.beneficiary, ommer_reward);
         }
     }
+
     MONAD_DEBUG_ASSERT(can_merge(block_state.state, state.state_));
     merge(block_state.state, state.state_);
 }
