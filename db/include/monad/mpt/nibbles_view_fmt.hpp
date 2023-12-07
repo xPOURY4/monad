6d9386407a79b1fbf4573a463efbda373ebb481c
Alexander Lee <alex@monad.xyz>
2023-12-07 16:59:21 -0500

    add nibbles_view_fmt

---
+++ db/include/monad/mpt/nibbles_view_fmt.hpp
@@ -0,0 +1,24 @@
+#include <monad/core/basic_formatter.hpp>
+#include <monad/mpt/nibbles_view.hpp>
+
+#include <quill/Quill.h>
+#include <quill/bundled/fmt/format.h>
+
+template <>
+struct quill::copy_loggable<monad::mpt::NibblesView> : std::true_type
+{
+};
+
+template <>
+struct fmt::formatter<monad::mpt::NibblesView> : public monad::BasicFormatter
+{
+    template <typename FormatContext>
+    auto format(monad::mpt::NibblesView const &value, FormatContext &ctx) const
+    {
+        fmt::format_to(ctx.out(), "0x");
+        for (auto i = 0u; i < value.nibble_size(); ++i) {
+            fmt::format_to(ctx.out(), "{:01x}", value.get(i));
+        }
+        return ctx.out();
+    }
+};
