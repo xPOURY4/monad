e2cb5638ad733d8006b210cfc3845e533b6f00b6
James Hunsaker <hunsaker@spacetime.org>
2023-12-13 11:20:37 -0500

    ubuntu build deps

---
+++ core/scripts/ubuntu-build/install-boost.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+add-apt-repository -y ppa:mhier/libboost-latest
+
+apt-get update
+
+apt-get install -y \
+  libboost-fiber1.83.0 \
+  libboost-graph1.83.0 \
+  libboost-json1.83.0 \
+  libboost-stacktrace1.83.0
+
+apt-get install -y \
+  libboost-fiber1.83-dev \
+  libboost-graph1.83-dev \
+  libboost-json1.83-dev \
+  libboost-stacktrace1.83-dev \
+  libboost1.83-dev
---
+++ core/scripts/ubuntu-build/install-clang.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O /tmp/llvm.sh https://apt.llvm.org/llvm.sh
+
+bash /tmp/llvm.sh 18 all
