68a48b3215a6a10630d2d5b094030e4efc129cee
James Hunsaker <hunsaker@spacetime.org>
2024-01-16 04:56:34 -0500

    account state does not store deltas

--- include/monad/state3/account_state.hpp
+++ include/monad/state3/account_state.hpp
@@ -2,138 +2,146 @@
 
 #include <monad/config.hpp>
 #include <monad/core/account.hpp>
-#include <monad/core/address.hpp>
 #include <monad/core/assert.h>
-#include <monad/core/byte_string.hpp>
+#include <monad/core/bytes.hpp>
+#include <monad/core/int.hpp>
 #include <monad/core/likely.h>
-#include <monad/state2/block_state.hpp>
-#include <monad/state2/state_deltas.hpp>
 #include <monad/state3/account_substate.hpp>
 
-#include <ethash/keccak.hpp>
+#include <evmc/evmc.h>
 
 #include <intx/intx.hpp>
 
+#include <ankerl/unordered_dense.h>
+
 #include <optional>
+#include <utility>
 
 MONAD_NAMESPACE_BEGIN
 
 class AccountState final : public AccountSubstate
 {
-    Address const address_{};
-    BlockState &block_state_;
-    StateDelta state_delta_{};
+    template <class Key, class T>
+    using Map = ankerl::unordered_dense::segmented_map<Key, T>;
+
+    std::optional<Account> account_{};
+    Map<bytes32_t, bytes32_t> storage_{};
+
+    evmc_storage_status zero_out_key(
+        bytes32_t const &key, bytes32_t const &original_value,
+        bytes32_t const &current_value);
+
+    evmc_storage_status set_current_value(
+        bytes32_t const &key, bytes32_t const &value,
+        bytes32_t const &original_value, bytes32_t const &current_value);
 
 public:
-    explicit AccountState(Address const &, BlockState &);
+    explicit AccountState(std::optional<Account> &&account)
+        : account_{std::move(account)}
+    {
+    }
+
+    explicit AccountState(std::optional<Account> const &account)
+        : account_{account}
+    {
+    }
 
     AccountState(AccountState &&) = default;
     AccountState(AccountState const &) = default;
-    AccountState &operator=(AccountState &&) = delete;
-    AccountState &operator=(AccountState const &) = delete;
+    AccountState &operator=(AccountState &&) = default;
+    AccountState &operator=(AccountState const &) = default;
 
-protected:
-    Address const &get_address() const
+    constexpr bool account_exists() const noexcept
     {
-        return address_;
+        return account_.has_value();
     }
 
-    std::optional<Account> const &get_account() const
+    constexpr uint64_t get_nonce() const noexcept
     {
-        return state_delta_.account.second;
-    }
-
-    std::optional<Account> &get_account()
-    {
-        return state_delta_.account.second;
-    }
-
-    Delta<bytes32_t> &read_storage_delta(bytes32_t const &location)
-    {
-        {
-            auto const it = state_delta_.storage.find(location);
-            if (MONAD_LIKELY(it != state_delta_.storage.end())) {
-                return it->second;
-            }
-        }
-        auto const &account = get_account();
-        MONAD_ASSERT(account.has_value());
-        uint64_t const incarnation = account.value().incarnation;
-        auto const result =
-            block_state_.read_storage(address_, incarnation, location);
-        auto const it =
-            state_delta_.storage.try_emplace(location, result, result).first;
-        return it->second;
-    }
-
-public:
-    bytes32_t get_balance() const
-    {
-        auto const &account = get_account();
-        if (MONAD_LIKELY(account.has_value())) {
-            return intx::be::store<bytes32_t>(account.value().balance);
-        }
-        return {};
-    }
-
-    bytes32_t get_code_hash() const
-    {
-        auto const &account = get_account();
-        if (MONAD_LIKELY(account.has_value())) {
-            return account.value().code_hash;
-        }
-        return NULL_HASH;
-    }
-
-    uint64_t get_nonce() const
-    {
-        auto const &account = get_account();
-        if (MONAD_LIKELY(account.has_value())) {
-            return account.value().nonce;
+        if (MONAD_LIKELY(account_)) {
+            return account_->nonce;
         }
         return 0;
     }
 
+    constexpr bytes32_t get_balance() const noexcept
+    {
+        if (MONAD_LIKELY(account_)) {
+            return intx::be::store<bytes32_t>(account_->balance);
+        }
+        return {};
+    }
+
+    constexpr bytes32_t get_code_hash() const noexcept
+    {
+        if (MONAD_LIKELY(account_)) {
+            return account_->code_hash;
+        }
+        return NULL_HASH;
+    }
+
+    std::optional<bytes32_t> get_storage(bytes32_t const &key)
+    {
+        auto const it = storage_.find(key);
+        if (MONAD_LIKELY(it != storage_.end())) {
+            return it->second;
+        }
+        return {};
+    }
+
+    void set_nonce(uint64_t const nonce)
+    {
+        MONAD_ASSERT(account_);
+
+        account_->nonce = nonce;
+    }
+
     void add_to_balance(uint256_t const &delta)
     {
-        auto &account = get_account();
-        if (MONAD_UNLIKELY(!account.has_value())) {
-            account = Account{};
+        if (MONAD_UNLIKELY(!account_)) {
+            account_ = Account{};
         }
 
-        MONAD_ASSERT(
-            std::numeric_limits<uint256_t>::max() - delta >=
-            account.value().balance);
+        MONAD_ASSERT(UINT256_MAX - delta >= account_->balance);
 
-        account.value().balance += delta;
+        account_->balance += delta;
         touch();
     }
 
     void subtract_from_balance(uint256_t const &delta)
     {
-        auto &account = get_account();
-        if (MONAD_UNLIKELY(!account.has_value())) {
-            account = Account{};
+        if (MONAD_UNLIKELY(!account_)) {
+            account_ = Account{};
         }
 
-        MONAD_ASSERT(delta <= account.value().balance);
+        MONAD_ASSERT(delta <= account_->balance);
 
-        account.value().balance -= delta;
+        account_->balance -= delta;
         touch();
     }
 
-    void set_code(byte_string_view const &code)
+    void set_code_hash(bytes32_t const &code_hash)
     {
-        auto const code_hash = std::bit_cast<bytes32_t>(
-            ethash::keccak256(code.data(), code.size()));
+        MONAD_ASSERT(account_);
 
-        auto &account = get_account();
-        MONAD_ASSERT(account.has_value());
-        account.value().code_hash = code_hash;
-        if (!code.empty()) {
-            // TODO
-            // read_code(code_hash) = code;
+        account_->code_hash = code_hash;
+    }
+
+    evmc_storage_status set_storage(
+        bytes32_t const &key, bytes32_t const &value,
+        bytes32_t const &original_value)
+    {
+        bytes32_t current_value = original_value;
+        {
+            auto const it = storage_.find(key);
+            if (it != storage_.end()) {
+                current_value = it->second;
+            }
         }
+        if (value == bytes32_t{}) {
+            return zero_out_key(key, original_value, current_value);
+        }
+        return set_current_value(key, value, original_value, current_value);
     }
 };
 
--- src/monad/state3/account_state.cpp
+++ src/monad/state3/account_state.cpp
@@ -1,23 +1,60 @@
 #include <monad/state3/account_state.hpp>
 
 #include <monad/config.hpp>
-#include <monad/core/address.hpp>
-#include <monad/state2/block_state.hpp>
-#include <monad/state2/state_deltas.hpp>
+#include <monad/core/bytes.hpp>
+
+#include <evmc/evmc.h>
 
 MONAD_NAMESPACE_BEGIN
 
-AccountState::AccountState(Address const &address, BlockState &block_state)
-    : address_{address}
-    , block_state_{block_state}
-    , state_delta_{
-          .account =
-              [this] {
-                  auto const account = block_state_.read_account(address_);
-                  return AccountDelta{account, account};
-              }(),
-          .storage = {}}
+evmc_storage_status AccountState::zero_out_key(
+    bytes32_t const &key, bytes32_t const &original_value,
+    bytes32_t const &current_value)
 {
+    auto const status = [&] {
+        if (current_value == bytes32_t{}) {
+            return EVMC_STORAGE_ASSIGNED;
+        }
+        else if (original_value == current_value) {
+            return EVMC_STORAGE_DELETED;
+        }
+        else if (original_value == bytes32_t{}) {
+            return EVMC_STORAGE_ADDED_DELETED;
+        }
+        return EVMC_STORAGE_MODIFIED_DELETED;
+    }();
+
+    storage_[key] = bytes32_t{};
+
+    return status;
+}
+
+evmc_storage_status AccountState::set_current_value(
+    bytes32_t const &key, bytes32_t const &value,
+    bytes32_t const &original_value, bytes32_t const &current_value)
+{
+    auto const status = [&] {
+        if (current_value == bytes32_t{}) {
+            if (original_value == bytes32_t{}) {
+                return EVMC_STORAGE_ADDED;
+            }
+            else if (value == original_value) {
+                return EVMC_STORAGE_DELETED_RESTORED;
+            }
+            return EVMC_STORAGE_DELETED_ADDED;
+        }
+        else if (original_value == current_value && original_value != value) {
+            return EVMC_STORAGE_MODIFIED;
+        }
+        else if (original_value == value && original_value != current_value) {
+            return EVMC_STORAGE_MODIFIED_RESTORED;
+        }
+        return EVMC_STORAGE_ASSIGNED;
+    }();
+
+    storage_[key] = value;
+
+    return status;
 }
 
 MONAD_NAMESPACE_END
