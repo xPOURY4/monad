9a8ceb72e74edf09a636bdfdd2e96434270d14ce
Vicky Chen <yifanc3@illinois.edu>
2024-01-24 20:22:45 +0000

    TrieUpdateCollectedStats::reset() by reconstruct the object

--- db/include/monad/mpt/detail/collected_stats.hpp
+++ db/include/monad/mpt/detail/collected_stats.hpp
@@ -25,27 +25,14 @@ namespace detail
 
         void reset()
         {
-            num_nodes_created = 0;
-            num_nodes_copied = 0;
-            num_compaction_reads = 0;
-            nodes_copied_from_fast_to_slow = 0;
-            nodes_copied_from_fast_to_fast = 0;
-            nodes_copied_from_slow_to_slow = 0;
-            nreads_before_offset[0] = 0;
-            nreads_before_offset[1] = 0;
-            nreads_after_offset[0] = 0;
-            nreads_after_offset[1] = 0;
-            nodes_copied_for_compacting_slow = 0;
-            nodes_copied_for_compacting_fast = 0;
-            bytes_read_before_offset[0] = 0;
-            bytes_read_before_offset[1] = 0;
-            bytes_read_after_offset[0] = 0;
-            bytes_read_after_offset[1] = 0;
+            this->~TrieUpdateCollectedStats();
+            new (this) TrieUpdateCollectedStats();
         }
     };
 
     static_assert(sizeof(TrieUpdateCollectedStats) == 64);
     static_assert(alignof(TrieUpdateCollectedStats) == 4);
+    static_assert(std::is_trivially_copyable_v<TrieUpdateCollectedStats>);
 }
 
 MONAD_MPT_NAMESPACE_END
