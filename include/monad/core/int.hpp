03d0837cfe9b3305ef27ecbfd475dd55fcd17ca2
James Hunsaker <hunsaker@spacetime.org>
2024-02-04 19:23:05 -0500

    uint512

--- include/monad/core/int.hpp
+++ include/monad/core/int.hpp
@@ -19,14 +19,21 @@ using uint256_t = ::intx::uint256;
 static_assert(sizeof(uint256_t) == 32);
 static_assert(alignof(uint256_t) == 8);
 
+using uint512_t = ::intx::uint512;
+
+static_assert(sizeof(uint512_t) == 64);
+static_assert(alignof(uint512_t) == 8);
+
 template <class T>
 concept unsigned_integral =
-    std::unsigned_integral<T> || std::same_as<uint256_t, T> ||
-    std::same_as<uint128_t, T>;
+    std::unsigned_integral<T> || std::same_as<uint128_t, T> ||
+    std::same_as<uint256_t, T> || std::same_as<uint512_t, T>;
 
 inline constexpr uint128_t UINT128_MAX = std::numeric_limits<uint128_t>::max();
 
 inline constexpr uint256_t UINT256_MAX = std::numeric_limits<uint256_t>::max();
+
+inline constexpr uint512_t UINT512_MAX = std::numeric_limits<uint512_t>::max();
 
 using ::intx::to_big_endian;
 
