fb3f9da17de38bff6e01289ff355002ada8cc377
James Hunsaker <hunsaker@spacetime.org>
2024-02-16 23:26:33 -0500

    disable storage cache - does not handle account deletions

--- include/monad/db/db_cache.hpp
+++ include/monad/db/db_cache.hpp
@@ -11,10 +11,7 @@
 
 #include <thread-safe-lru/lru-cache.h>
 
-#include <boost/container_hash/hash.hpp>
-
 #include <optional>
-#include <utility>
 
 MONAD_NAMESPACE_BEGIN
 
@@ -22,26 +19,10 @@ class DbCache final : public Db
 {
     Db &db_;
 
-    struct HashCompare
-    {
-        size_t hash(std::pair<Address, bytes32_t> const &p) const
-        {
-            return boost::hash_value(p);
-        }
-
-        bool equal(auto const &p1, auto const &p2) const
-        {
-            return p1 == p2;
-        }
-    };
-
     using AccountCache =
         tstarling::ThreadSafeLRUCache<Address, std::optional<Account>>;
-    using StorageCache = tstarling::ThreadSafeLRUCache<
-        std::pair<Address, bytes32_t>, bytes32_t, HashCompare>;
 
     AccountCache accounts_{10000000};
-    StorageCache storage_{10000000};
 
 public:
     DbCache(Db &db)
@@ -69,15 +50,7 @@ public:
     virtual bytes32_t
     read_storage(Address const &address, bytes32_t const &key) override
     {
-        StorageCache::ConstAccessor it{};
-        if (storage_.find(it, {address, key})) {
-            return *it;
-        }
-        {
-            bytes32_t const result = db_.read_storage(address, key);
-            storage_.insert({address, key}, result);
-            return result;
-        }
+        return db_.read_storage(address, key);
     }
 
     virtual byte_string read_code(bytes32_t const &code_hash) override
@@ -94,14 +67,6 @@ public:
             auto const &account_delta = it->second.account;
             if (account_delta.second != account_delta.first) {
                 accounts_.insert(address, account_delta.second);
-            }
-            auto const &storage = it->second.storage;
-            for (auto it2 = storage.cbegin(); it2 != storage.cend(); ++it2) {
-                auto const &key = it2->first;
-                auto const &storage_delta = it2->second;
-                if (storage_delta.second != storage_delta.first) {
-                    storage_.insert({address, key}, storage_delta.second);
-                }
             }
         }
     }
