76005ec3c370704f6adf94b641c376179b3e1f30
Tong Zhi <tong@monad.xyz>
2024-06-20 14:45:53 -0400

    Remove outdated behave feature tests

--- test/features/tdb/steps/output.py
+++
@@ -1,7 +0,0 @@
-from behave import then  # type: ignore
-
-
-@then('the output should contain "{value}"')
-def then_output_contain(context, value):
-    print(value, context.output)
-    assert value in context.output
