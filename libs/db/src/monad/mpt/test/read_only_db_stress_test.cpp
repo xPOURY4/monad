f15e027662b326fbc14a536be5572334f78ec8d1
Kevin Kuehler <kkuehler@monad.xyz>
2024-07-25 15:43:23 -0400

    stress_test_read_only_db executable

---
+++ libs/db/src/monad/mpt/test/read_only_db_stress_test.cpp
@@ -0,0 +1,337 @@
+#include <monad/core/assert.h>
+#include <monad/core/byte_string.hpp>
+#include <monad/core/hex_literal.hpp>
+#include <monad/core/keccak.hpp>
+#include <monad/core/small_prng.hpp>
+#include <monad/mpt/db.hpp>
+#include <monad/mpt/ondisk_db_config.hpp>
+#include <monad/mpt/test/test_fixtures_base.hpp>
+#include <monad/mpt/traverse.hpp>
+#include <monad/mpt/update.hpp>
+#include <monad/mpt/util.hpp>
+
+#include <CLI/CLI.hpp>
+
+#include <atomic>
+#include <chrono>
+#include <filesystem>
+#include <limits>
+#include <thread>
+#include <utility>
+
+#include <signal.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace monad::mpt;
+using namespace monad::test;
+
+std::atomic<bool> g_done{false};
+static_assert(std::atomic<bool>::is_always_lock_free); // async signal safe
+
+static std::pair<ethash::hash256, monad::byte_string>
+key_value_from_version(uint64_t const version)
+{
+    auto const version_bytes = serialize_as_big_endian<6>(version);
+    return std::make_pair(monad::keccak256(version_bytes), version_bytes);
+}
+
+static uint64_t
+select_rand_version(Db const &db, monad::small_prng &rnd, double bias)
+{
+    auto const version_range_start =
+        static_cast<double>(db.get_earliest_block_id());
+    auto const version_range_end =
+        static_cast<double>(db.get_latest_block_id());
+    double r = rnd();
+    r = r / monad::small_prng::max();
+    if (r > 0.25) {
+        r = std::pow(1, bias);
+    }
+    return static_cast<uint64_t>(
+        version_range_start + r * (version_range_end - version_range_start));
+}
+
+static void on_signal(int)
+{
+    g_done.store(true, std::memory_order_release);
+}
+
+int main(int argc, char *const argv[])
+{
+    unsigned num_reader_threads = 4;
+    unsigned num_traverse_threads = 2;
+    double prng_bias = 1.66;
+    bool enable_compaction = true;
+    int64_t storage_size_gb = 32;
+    uint32_t timeout_seconds = std::numeric_limits<uint32_t>::max();
+    CLI::App cli(
+        "Tool for stress testing concurrent RO DB instances",
+        "read_only_db_stress_test");
+
+    try {
+        cli.add_option(
+            "--num-reader-threads",
+            num_reader_threads,
+            "Number of threads doing random reads");
+        cli.add_option(
+            "--num-traverse-threads",
+            num_traverse_threads,
+            "Number of threads traversing random version tries");
+        cli.add_option(
+            "--prng-bias",
+            prng_bias,
+            "After drawing R, raises r**bias. Can be used to choose versions "
+            "earlier or later in the history");
+        cli.add_option(
+            "--compaction",
+            enable_compaction,
+            "Enable compaction when writing new DB versions");
+        cli.add_option(
+            "--storage-size",
+            storage_size_gb,
+            "Size the database can grow to (GB)");
+        cli.add_option(
+            "--timeout",
+            timeout_seconds,
+            "Teardown the stress test after N seconds");
+        cli.parse(argc, argv);
+
+        struct sigaction sig;
+        sig.sa_handler = &on_signal;
+        sig.sa_flags = 0;
+        sigaction(SIGINT, &sig, nullptr);
+        sigaction(SIGALRM, &sig, nullptr);
+
+        auto tempdir = (MONAD_ASYNC_NAMESPACE::working_temporary_directory() /
+                        "read_only_db_stress_test_XXXXXX")
+                           .string();
+        if (::mkdtemp(tempdir.data()) == nullptr) {
+            std::cerr << "FATAL: Could not create temp directory: "
+                      << strerror(errno) << "\n\n";
+            return 1;
+        }
+        std::filesystem::path const dbname{
+            std::filesystem::path(tempdir) / "db"};
+        std::cout << "Database path is " << dbname << "\n\n";
+
+        auto const prefix = 0x00_hex;
+
+        auto upsert_new_version = [&](Db &db, uint64_t const version) {
+            UpdateList ul;
+            auto [key, value] = key_value_from_version(version);
+            auto u = make_update(key, value);
+            ul.push_front(u);
+
+            auto u_prefix = Update{
+                .key = prefix,
+                .value = monad::byte_string_view{},
+                .incarnation = false,
+                .next = std::move(ul)};
+            UpdateList ul_prefix;
+            ul_prefix.push_front(u_prefix);
+
+            db.upsert(std::move(ul_prefix), version);
+        };
+
+        auto random_read = [&]() {
+            ReadOnlyOnDiskDbConfig const ro_config{.dbname_paths = {dbname}};
+            Db ro_db{ro_config};
+
+            while (ro_db.get_latest_block_id() == INVALID_BLOCK_ID &&
+                   !g_done.load(std::memory_order_acquire)) {
+            }
+            // now the first version is written to db
+            MONAD_ASSERT(ro_db.get_latest_block_id() != INVALID_BLOCK_ID);
+            MONAD_ASSERT(ro_db.get_earliest_block_id() != INVALID_BLOCK_ID);
+
+            unsigned nsuccess = 0;
+            unsigned nfailed = 0;
+
+            auto rnd = monad::thread_local_prng();
+            while (!g_done.load(std::memory_order_acquire)) {
+                auto const read_version =
+                    select_rand_version(ro_db, rnd, prng_bias);
+                auto [key, value] = key_value_from_version(read_version);
+                auto const res = ro_db.get(
+                    concat(NibblesView{prefix}, NibblesView{key}),
+                    read_version);
+                if (res.has_value()) {
+                    MONAD_ASSERT(res.value() == value);
+                    ++nsuccess;
+                }
+                else {
+                    auto const min_version = ro_db.get_earliest_block_id();
+                    MONAD_ASSERT(read_version < min_version);
+                    ++nfailed;
+                }
+            }
+            std::ostringstream oss;
+            oss << "Reader thread (0x" << std::hex << std::this_thread::get_id()
+                << std::dec << ") finished"
+                << ". Did " << nsuccess << " successful and " << nfailed
+                << " failed reads" << std::endl;
+            std::cout << oss.str();
+        };
+
+        auto random_traverse = [&]() {
+            ReadOnlyOnDiskDbConfig const ro_config{.dbname_paths = {dbname}};
+            Db ro_db{ro_config};
+
+            unsigned nsuccess = 0;
+            unsigned nfailed = 0;
+
+            while (ro_db.get_latest_block_id() == INVALID_BLOCK_ID &&
+                   !g_done.load(std::memory_order_acquire)) {
+            }
+            // now the first version is written to db
+            MONAD_ASSERT(ro_db.get_latest_block_id() != INVALID_BLOCK_ID);
+            MONAD_ASSERT(ro_db.get_earliest_block_id() != INVALID_BLOCK_ID);
+
+            struct VersionValidatorMachine : public TraverseMachine
+            {
+                Nibbles path{};
+
+                virtual bool
+                down(unsigned char branch, Node const &node) override
+                {
+                    if (branch == INVALID_BRANCH) {
+                        return true;
+                    }
+                    path = concat(
+                        NibblesView{path}, branch, node.path_nibble_view());
+
+                    if (node.has_value()) {
+                        MONAD_ASSERT(path.nibble_size() == KECCAK256_SIZE * 2);
+                        MONAD_ASSERT(
+                            path ==
+                            NibblesView{monad::keccak256(node.value())});
+                    }
+                    return true;
+                }
+
+                virtual void up(unsigned char branch, Node const &node) override
+                {
+                    auto const path_view = NibblesView{path};
+                    auto const rem_size = [&] {
+                        if (branch == INVALID_BRANCH) {
+                            MONAD_ASSERT(path_view.nibble_size() == 0);
+                            return 0;
+                        }
+                        int const rem_size =
+                            path_view.nibble_size() - 1 -
+                            node.path_nibble_view().nibble_size();
+                        MONAD_ASSERT(rem_size >= 0);
+                        MONAD_ASSERT(
+                            path_view.substr(static_cast<unsigned>(rem_size)) ==
+                            concat(branch, node.path_nibble_view()));
+                        return rem_size;
+                    }();
+                    path = path_view.substr(0, static_cast<unsigned>(rem_size));
+                }
+
+                virtual std::unique_ptr<TraverseMachine> clone() const override
+                {
+                    return std::make_unique<VersionValidatorMachine>(*this);
+                }
+            };
+
+            auto rnd = monad::thread_local_prng();
+            while (!g_done.load(std::memory_order_acquire)) {
+                auto const version = select_rand_version(ro_db, rnd, prng_bias);
+                if (auto cursor = ro_db.find(prefix, version);
+                    cursor.has_value()) {
+                    VersionValidatorMachine machine;
+                    if (!ro_db.traverse(cursor.value(), machine, version)) {
+                        auto const min_version = ro_db.get_earliest_block_id();
+                        MONAD_ASSERT(version < min_version);
+                        ++nfailed;
+                    }
+                    else {
+                        ++nsuccess;
+                    }
+                }
+                else {
+                    auto const min_version = ro_db.get_earliest_block_id();
+                    MONAD_ASSERT(version < min_version);
+                    ++nfailed;
+                }
+            }
+            std::ostringstream oss;
+            oss << "Traverse thread (0x" << std::hex
+                << std::this_thread::get_id() << std::dec << ") finished"
+                << ". Did " << nsuccess << " successful and " << nfailed
+                << " failed traversals" << std::endl;
+            std::cout << oss.str();
+        };
+
+        auto open_close_read_only = [&]() {
+            unsigned nsuccess = 0;
+            unsigned nfailed = 0;
+            while (!g_done.load(std::memory_order_acquire)) {
+                ReadOnlyOnDiskDbConfig const ro_config{.dbname_paths = {dbname}};
+                Db ro_db{ro_config};
+                auto const version = ro_db.get_earliest_block_id() + 1;
+                auto [key, value] = key_value_from_version(version);
+                auto const res = ro_db.get(concat(NibblesView{prefix}, NibblesView{key}), version);
+                if (res.has_value()) {
+                    ++nsuccess;
+                    MONAD_ASSERT(res.value() == value);
+                } else {
+                    ++nfailed;
+                }
+            }
+            std::ostringstream oss;
+            oss << "Open close handle thread (0x" << std::hex
+                << std::this_thread::get_id() << std::dec << ") finished"
+                << ". Did " << nsuccess << " successful and " << nfailed
+                << " failed queries of earliest version root" << std::endl;
+            std::cout << oss.str();
+        };
+
+        // construct RWDb
+        uint64_t version = 0;
+        StateMachineAlwaysMerkle machine{};
+        OnDiskDbConfig const config{
+            .compaction = enable_compaction,
+            .dbname_paths = {dbname},
+            .file_size_db = storage_size_gb};
+        Db db{machine, config};
+
+        std::cout << "Running read only DB stress test..." << std::endl;
+
+        std::vector<std::thread> readers;
+        for (unsigned i = 0; i < num_reader_threads; ++i) {
+            readers.emplace_back(random_read);
+        }
+
+        for (unsigned i = 0; i < num_traverse_threads; ++i) {
+            readers.emplace_back(random_traverse);
+        }
+        readers.emplace_back(open_close_read_only);
+
+        alarm(timeout_seconds);
+        while (!g_done.load(std::memory_order_acquire)) {
+            upsert_new_version(db, version);
+            ++version;
+        }
+        for (auto &t : readers) {
+            t.join();
+        }
+
+        std::cout << "Writer finished. Max version in RWDb is "
+                  << db.get_latest_block_id() << ", min version in RWDb is "
+                  << db.get_earliest_block_id() << "\n\n";
+    }
+    catch (const CLI::CallForHelp &e) {
+        std::cout << cli.help() << std::flush;
+    }
+    catch (const CLI::RequiredError &e) {
+        std::cerr << "FATAL: " << e.what() << "\n\n";
+        std::cerr << cli.help() << std::flush;
+        return 1;
+    }
+
+    return 0;
+}
