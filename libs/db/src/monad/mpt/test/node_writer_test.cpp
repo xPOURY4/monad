2cb0fe946beb0136a1c3c0096ff173b165817ca7
Vicky Chen <yifanc3@illinois.edu>
2024-07-30 19:20:27 -0400

    add test replace fast writer at chunk boundary

--- libs/db/src/monad/mpt/test/node_writer_test.cpp
+++ libs/db/src/monad/mpt/test/node_writer_test.cpp
@@ -1,6 +1,8 @@
 #include "test_fixtures_base.hpp"
 #include "test_fixtures_gtest.hpp"
 
+#include <monad/core/assert.h>
+
 #include <monad/async/config.hpp>
 #include <monad/mpt/config.hpp>
 #include <monad/mpt/node.hpp>
@@ -39,7 +41,7 @@ private:
         state()->aux.advance_db_offsets_to(
             fast_offset_rewind_to, state()->aux.get_start_of_wip_slow_offset());
         state()->aux.append_root_offset(state()->aux.get_latest_root_offset());
-        this->state()->aux.rewind_to_match_offsets();
+        state()->aux.rewind_to_match_offsets();
 
         EXPECT_EQ(
             state()->aux.node_writer_fast->sender().offset(),
@@ -55,14 +57,24 @@ private:
         return curr_node_writer_chunk_id;
     }
 
+    Node::UniquePtr make_node_of_size(unsigned const node_disk_size)
+    {
+        MONAD_ASSERT(node_disk_size > sizeof(Node) + Node::disk_size_bytes);
+        auto const node_value_size =
+            node_disk_size - sizeof(Node) - Node::disk_size_bytes;
+        auto const value = monad::byte_string(node_value_size, 0xf);
+        auto node = make_node(0, {}, {}, value, {}, 0);
+        return node;
+    }
+
 public:
     void rewind_then_write_node(
-        unsigned const remaining_bytes_in_chunk, unsigned node_value_size)
+        unsigned const remaining_bytes_in_chunk, unsigned node_disk_size)
     {
         auto const curr_node_writer_chunk_id =
             rewind_fast_list_(remaining_bytes_in_chunk);
-        auto const value = monad::byte_string(node_value_size, 0xf);
-        auto node = make_node(0, {}, {}, value, {}, 0);
+        auto node = make_node_of_size(node_disk_size);
+        EXPECT_EQ(node->get_disk_size(), node_disk_size);
         auto const chunks_before = state()->fast_list_ids().size();
         // write node
         auto const node_offset =
@@ -76,7 +88,7 @@ public:
             EXPECT_EQ(node_offset.offset, 0);
             EXPECT_EQ(new_node_writer->sender().offset().offset, 0);
             // new node writer offset should start at the next chunk
-            EXPECT_TRUE(new_node_writer_chunk_id != curr_node_writer_chunk_id);
+            EXPECT_NE(new_node_writer_chunk_id, curr_node_writer_chunk_id);
             EXPECT_EQ(
                 (uint32_t)state()
                     ->aux.db_metadata()
@@ -104,4 +116,28 @@ TEST_F(NodeWriterTest, write_node)
 
     // reminaing bytes > node size: write node to existing chunk
     rewind_then_write_node(5 * 1024 * 1024, 2 * 1024 * 1024);
+}
+
+TEST_F(NodeWriterTest, replace_fast_writer_at_chunk_boundary)
+{
+    unsigned const node_disk_size = 1024;
+    // After this line, node writer is at the end of a chunk
+    rewind_then_write_node(node_disk_size, node_disk_size);
+
+    auto const new_writer_offset =
+        state()->aux.node_writer_fast->sender().offset();
+    EXPECT_EQ(
+        new_writer_offset.offset + node_disk_size,
+        state()->aux.io->chunk_capacity(new_writer_offset.id));
+
+    // Replace fast node writer, writer should start at a new chunk that belongs
+    // to fast list
+    auto new_node_writer =
+        replace_node_writer(state()->aux, state()->aux.node_writer_fast);
+    EXPECT_EQ(new_node_writer->sender().offset().offset, 0);
+    EXPECT_NE(new_writer_offset.id, new_node_writer->sender().offset().id);
+    EXPECT_TRUE(state()
+                    ->aux.db_metadata()
+                    ->at(new_node_writer->sender().offset().id)
+                    ->in_fast_list);
 }
