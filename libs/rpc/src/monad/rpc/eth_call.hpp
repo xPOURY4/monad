9773db13b8a3e50fa7100c98a996d69690000c9f
Tong Zhi <tong@monad.xyz>
2024-08-12 18:37:35 -0400

    move eth_call to execution side

---
+++ libs/rpc/src/monad/rpc/eth_call.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+struct monad_evmc_result
+{
+    int status_code;
+    std::vector<uint8_t> output_data;
+    std::string message;
+    int64_t gas_used;
+    int64_t gas_refund;
+
+    int get_status_code() const;
+    std::vector<uint8_t> get_output_data() const;
+    std::string get_message() const;
+    int64_t get_gas_used() const;
+    int64_t get_gas_refund() const;
+};
+
+monad_evmc_result eth_call(
+    std::vector<uint8_t> const &rlp_txn, std::vector<uint8_t> const &rlp_header,
+    std::vector<uint8_t> const &rlp_sender, uint64_t block_number,
+    std::string const &triedb_path, std::string const &blockdb_path);
