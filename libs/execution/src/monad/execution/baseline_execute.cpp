279b7bf195c741c037eed7409ed370eb773579c4
James Hunsaker <hunsaker@spacetime.org>
2024-08-13 15:52:53 -0400

    remove evmone patch which avoids one dynamic memory allocation

--- libs/execution/src/monad/execution/baseline_execute.cpp
+++ libs/execution/src/monad/execution/baseline_execute.cpp
@@ -14,6 +14,8 @@
     #pragma GCC diagnostic pop
 #endif
 
+#include <evmone/baseline.hpp>
+#include <evmone/evmone.h>
 #include <evmone/vm.hpp>
 
 #ifdef EVMONE_TRACING
@@ -35,15 +37,15 @@ evmc::Result baseline_execute(
     evmc_message const &msg, evmc_revision const rev, evmc::Host *const host,
     CodeAnalysis const &code_analysis)
 {
+    std::unique_ptr<evmc_vm> const vm{evmc_create_evmone()};
+
     if (code_analysis.executable_code.empty()) {
         return evmc::Result{EVMC_SUCCESS, msg.gas};
     }
 
-    evmone::VM vm{};
-
 #ifdef EVMONE_TRACING
     std::ostringstream trace_ostream;
-    vm.add_tracer(evmone::create_instruction_tracer(trace_ostream));
+    vm->add_tracer(evmone::create_instruction_tracer(trace_ostream));
 #endif
 
     auto const execution_state = std::make_unique<evmone::ExecutionState>(
@@ -54,12 +56,15 @@ evmc::Result baseline_execute(
         code_analysis.executable_code,
         byte_string_view{});
 
-    auto const result =
-        evmone::baseline::execute(vm, msg.gas, *execution_state, code_analysis);
+    auto const result = evmone::baseline::execute(
+        *static_cast<evmone::VM *>(vm.get()),
+        msg.gas,
+        *execution_state,
+        code_analysis);
 
-    #ifdef EVMONE_TRACING
-        LOG_TRACE_L1("{}", trace_ostream.str());
-    #endif
+#ifdef EVMONE_TRACING
+    LOG_TRACE_L1("{}", trace_ostream.str());
+#endif
 
     return evmc::Result{result};
 }
