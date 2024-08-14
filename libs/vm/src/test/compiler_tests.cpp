0650ce729208ac7083b6500f1d4fe62e643526a7
Brett Letner <brett@monad.xyz>
2024-08-14 12:21:55 -0400

    clang-format

--- libs/vm/src/test/compiler_tests.cpp
+++ libs/vm/src/test/compiler_tests.cpp
@@ -8,7 +8,8 @@
 
 using namespace monad::compiler;
 
-void tokens_eq(std::vector<uint8_t> const &in, std::vector<Token> const &expected)
+void tokens_eq(
+    std::vector<uint8_t> const &in, std::vector<Token> const &expected)
 {
     EXPECT_EQ(BytecodeIR(in).tokens, expected);
 }
@@ -31,10 +32,10 @@ TEST(BytecodeTest, ToTokens)
     tokens_eq({PUSH32, 0xff}, {{0, PUSH32, (uint256_t)0xff << 248}});
 }
 
-void blocks_eq(std::vector<uint8_t> const &in,
+void blocks_eq(
+    std::vector<uint8_t> const &in,
     std::unordered_map<byte_offset, block_id> const &expected_jumpdests,
-    std::vector<Block> const &expected_blocks
-               )
+    std::vector<Block> const &expected_blocks)
 {
     BytecodeIR const actual_bc(in);
     InstructionIR const actual(actual_bc);
@@ -43,57 +44,52 @@ void blocks_eq(std::vector<uint8_t> const &in,
     EXPECT_EQ(actual.blocks, expected_blocks);
 }
 
-
+using Terminator::Jump;
 using Terminator::JumpDest;
 using Terminator::JumpI;
-using Terminator::Jump;
 using Terminator::Return;
-using Terminator::Stop;
 using Terminator::Revert;
 using Terminator::SelfDestruct;
+using Terminator::Stop;
 
 TEST(InstructionTest, ToBlocks)
 {
     blocks_eq({}, {}, {{{}, Stop, INVALID_BLOCK_ID}});
     blocks_eq({STOP}, {}, {{{}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({0xEE}, {}, {
-        {{{0, 0xEE, 0}}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({PUSH1}, {}, {
-        {{{0, PUSH1, 0}}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({PUSH2,0xf}, {}, {
-        {{{0, PUSH2, 0xf00}}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({STOP,ADD}, {}, {
-        {{}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({JUMPDEST, STOP}, {{0,0}}, {
-        {{}, Stop, INVALID_BLOCK_ID}});
-    blocks_eq({ADD, REVERT}, {}, {
-        {{{0, ADD, 0}}, Revert, INVALID_BLOCK_ID}});
-    blocks_eq({ADD, ADD, RETURN}, {}, {
-        {{{0, ADD, 0},{1, ADD, 0}}, Return, INVALID_BLOCK_ID}});
-    blocks_eq({JUMPDEST, ADD, REVERT}, {{0,0}}, {
-        {{{1, ADD, 0}}, Revert, INVALID_BLOCK_ID}});
-    blocks_eq({JUMPI}, {}, {
-        {{}, JumpI, 1},
-        {{}, Stop, INVALID_BLOCK_ID}
-        });
-    blocks_eq({JUMPDEST,JUMPDEST}, {{0,0},{1,0}}, {
-        {{}, Stop, INVALID_BLOCK_ID}
-        });
-    blocks_eq({JUMPDEST,JUMPDEST,JUMPDEST}, {{0,0},{1,0},{2,0}}, {
-        {{}, Stop, INVALID_BLOCK_ID}
-        });
-    blocks_eq({JUMPDEST,ADD,JUMPDEST}, {{0,0},{2,1}}, {
-        {{{1, ADD, 0}}, JumpDest, 1},
-        {{}, Stop, INVALID_BLOCK_ID}
-        });
-    blocks_eq({ADD, ADD, JUMP, ADD, JUMPDEST, SELFDESTRUCT}, {{4,1}}, {
-        {{{0, ADD, 0},{1, ADD, 0}}, Jump, INVALID_BLOCK_ID},
-        {{}, SelfDestruct, INVALID_BLOCK_ID}
-        });
-    blocks_eq({ADD, ADD, JUMP, ADD, JUMPDEST, JUMPDEST, SELFDESTRUCT}, {{4,1},{5, 1}}, {
-        {{{0, ADD, 0},{1, ADD, 0}}, Jump, INVALID_BLOCK_ID},
-        {{}, SelfDestruct, INVALID_BLOCK_ID}
-        });
-
+    blocks_eq({0xEE}, {}, {{{{0, 0xEE, 0}}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq({PUSH1}, {}, {{{{0, PUSH1, 0}}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {PUSH2, 0xf}, {}, {{{{0, PUSH2, 0xf00}}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq({STOP, ADD}, {}, {{{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq({JUMPDEST, STOP}, {{0, 0}}, {{{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq({ADD, REVERT}, {}, {{{{0, ADD, 0}}, Revert, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {ADD, ADD, RETURN},
+        {},
+        {{{{0, ADD, 0}, {1, ADD, 0}}, Return, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {JUMPDEST, ADD, REVERT},
+        {{0, 0}},
+        {{{{1, ADD, 0}}, Revert, INVALID_BLOCK_ID}});
+    blocks_eq({JUMPI}, {}, {{{}, JumpI, 1}, {{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {JUMPDEST, JUMPDEST}, {{0, 0}, {1, 0}}, {{{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {JUMPDEST, JUMPDEST, JUMPDEST},
+        {{0, 0}, {1, 0}, {2, 0}},
+        {{{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {JUMPDEST, ADD, JUMPDEST},
+        {{0, 0}, {2, 1}},
+        {{{{1, ADD, 0}}, JumpDest, 1}, {{}, Stop, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {ADD, ADD, JUMP, ADD, JUMPDEST, SELFDESTRUCT},
+        {{4, 1}},
+        {{{{0, ADD, 0}, {1, ADD, 0}}, Jump, INVALID_BLOCK_ID},
+         {{}, SelfDestruct, INVALID_BLOCK_ID}});
+    blocks_eq(
+        {ADD, ADD, JUMP, ADD, JUMPDEST, JUMPDEST, SELFDESTRUCT},
+        {{4, 1}, {5, 1}},
+        {{{{0, ADD, 0}, {1, ADD, 0}}, Jump, INVALID_BLOCK_ID},
+         {{}, SelfDestruct, INVALID_BLOCK_ID}});
 }
-
