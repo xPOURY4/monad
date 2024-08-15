594c55101339049dc34ef36fa3ad340dca673778
Bruce Collie <brucecollie82@gmail.com>
2024-08-15 07:13:03 -0400

    Implement slow jump table

--- libs/vm/src/compiler/include/compiler/compiler.h
+++ libs/vm/src/compiler/include/compiler/compiler.h
@@ -11,6 +11,7 @@ namespace monad::compiler
     namespace constants
     {
         inline constexpr auto contract_entrypoint = "monad_evm_contract_main";
+        inline constexpr auto jump_table = "monad_evm_jump_table";
     }
 
     struct compile_result
--- libs/vm/src/compiler/ir/simple_llvm.cpp
+++ libs/vm/src/compiler/ir/simple_llvm.cpp
@@ -1,10 +1,13 @@
 #include <compiler/ir/simple_llvm.h>
 
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
 
 #include <cassert>
+#include <format>
 #include <iostream>
 #include <memory>
+#include <unordered_map>
 #include <utility>
 
 namespace
@@ -27,6 +30,68 @@ namespace
             constants::contract_entrypoint,
             mod);
     }
+
+    /**
+     * Compile a slow dynamic jump table function based on the program's
+     * JUMPDEST instructions.
+     *
+     * This function takes a single argument representing the byte offset into
+     * the original program that should be jumped to, and returns a block
+     * address constant that can be indirectly branched to. If the argument
+     * offset was not a JUMPDEST instruction in the original program, then the
+     * returned block address is null.
+     */
+    llvm::Function *build_slow_jump_table(
+        llvm::Module &mod,
+        std::unordered_map<byte_offset, block_id> const &jump_table,
+        std::vector<llvm::BasicBlock *> const &blocks)
+    {
+        auto *block_address_ty = llvm::PointerType::getUnqual(context());
+        auto *block_id_ty =
+            llvm::IntegerType::get(context(), sizeof(block_id) * CHAR_BIT);
+
+        auto *table_fn_ty =
+            llvm::FunctionType::get(block_address_ty, {block_id_ty}, false);
+
+        auto *table_fn = llvm::Function::Create(
+            table_fn_ty,
+            llvm::GlobalValue::LinkageTypes::InternalLinkage,
+            constants::jump_table,
+            mod);
+
+        auto *entry = llvm::BasicBlock::Create(context(), "entry", table_fn);
+        auto *error = llvm::BasicBlock::Create(context(), "error", table_fn);
+        auto *exit = llvm::BasicBlock::Create(context(), "exit", table_fn);
+
+        auto b = llvm::IRBuilder(error);
+        b.CreateRet(llvm::ConstantPointerNull::get(block_address_ty));
+
+        b.SetInsertPoint(exit);
+        auto *phi = b.CreatePHI(block_address_ty, 0);
+        b.CreateRet(phi);
+
+        b.SetInsertPoint(entry);
+
+        auto *offset_arg = table_fn->getArg(0);
+        auto *switch_table = b.CreateSwitch(offset_arg, error);
+
+        for (auto const &[offset, block_id] : jump_table) {
+            assert(block_id < blocks.size() && "Invalid block ID");
+
+            auto *case_block = llvm::BasicBlock::Create(
+                context(), std::format("jumpdest_{}", offset), table_fn);
+            b.SetInsertPoint(case_block);
+            b.CreateBr(exit);
+
+            auto *offset_const = llvm::ConstantInt::get(block_id_ty, offset);
+            switch_table->addCase(offset_const, case_block);
+
+            auto *block_addr = llvm::BlockAddress::get(blocks[block_id]);
+            phi->addIncoming(block_addr, case_block);
+        }
+
+        return table_fn;
+    }
 }
 
 namespace monad::compiler
@@ -43,6 +108,8 @@ namespace monad::compiler
         for (auto const &b : instrs.blocks) {
             evm_blocks.push_back(compile_block(b));
         }
+
+        build_slow_jump_table(*mod, instrs.jumpdests, evm_blocks);
     }
 
     compile_result SimpleLLVMIR::result() &&
