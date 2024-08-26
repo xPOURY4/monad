976e3ca2ab8e21883f6de6bcca629adf4e06a0fb
Alexander Lee <alex@monad.xyz>
2024-08-26 15:31:33 -0400

    log_tps only if block processed

--- cmd/monad.cpp
+++ cmd/monad.cpp
@@ -179,7 +179,10 @@ Result<std::pair<uint64_t, uint64_t>> run_monad(
         }
         ++block_num;
     }
-    log_tps(block_num, batch_num_blocks, batch_num_txs, batch_gas, batch_begin);
+    if (batch_num_blocks > 0) {
+        log_tps(
+            block_num, batch_num_blocks, batch_num_txs, batch_gas, batch_begin);
+    }
     return {ntxs, total_gas};
 }
 
