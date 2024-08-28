b5094c09c3192ddc13abadf3901a90ca7da07bf3
Alexander Lee <alex@monad.xyz>
2024-08-28 15:18:33 -0400

    move common test code out

--- libs/execution/src/monad/db/test/test_db.cpp
+++ libs/execution/src/monad/db/test/test_db.cpp
@@ -32,6 +32,7 @@
 #include <vector>
 
 using namespace monad;
+using namespace monad::test;
 
 namespace
 {
@@ -45,132 +46,8 @@ namespace
         0x0000000000000013370000000000000000000000000000000000000000000003_bytes32;
     constexpr auto value2 =
         0x0000000000000000000000000000000000000000000000000000000000000007_bytes32;
-    auto const code1 =
-        std::make_shared<CodeAnalysis>(analyze(byte_string{0xab, 0xcd, 0xef}));
-    auto const code2 =
-        std::make_shared<CodeAnalysis>(analyze(byte_string{0xbb, 0xbb, 0xbb}));
     constexpr auto code_hash1 =
         0x1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c_bytes32;
-    constexpr auto code_hash2 =
-        0x1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1bbbbbbbbb_bytes32;
-
-    auto const a_code =
-        evmc::from_hex("7ffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-                       "fffffffffff7fffffffffffffffffffffffffffffffffffffffffff"
-                       "ffffffffffffffffffffff0160005500")
-            .value();
-    auto const a_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(a_code.data(), a_code.size()));
-    auto const a_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(a_code));
-    auto const b_code =
-        evmc::from_hex("60047ffffffffffffffffffffffffffffffffffffffffffffffffff"
-                       "fffffffffffffff0160005500")
-            .value();
-    auto const b_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(b_code.data(), b_code.size()));
-    auto const b_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(b_code));
-    auto const c_code =
-        evmc::from_hex("60017ffffffffffffffffffffffffffffffffffffffffffffffffff"
-                       "fffffffffffffff0160005500")
-            .value();
-    auto const c_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(c_code.data(), c_code.size()));
-    auto const c_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(c_code));
-    auto const d_code = evmc::from_hex("600060000160005500").value();
-    auto const d_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(d_code.data(), d_code.size()));
-    auto const d_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(d_code));
-    auto const e_code =
-        evmc::from_hex("7ffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-                       "fffffffffff60010160005500")
-            .value();
-    auto const e_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(e_code.data(), e_code.size()));
-    auto const e_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(e_code));
-    auto const h_code =
-        evmc::from_hex("600060006000600060006004356101000162fffffff100")
-            .value();
-    auto const h_code_hash = std::bit_cast<bytes32_t>(
-        ethash::keccak256(h_code.data(), h_code.size()));
-    auto const h_code_analysis =
-        std::make_shared<CodeAnalysis>(analyze(h_code));
-
-    auto const json_str = R"(
-{
-  "0x03601462093b5945d1676df093446790fd31b20e7b12a2e8e5e09d068109616b": {
-    "balance": "838137708090664833",
-    "code": "0x",
-    "address": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
-    "nonce": "0x1",
-    "storage": {}
-  },
-  "0x227a737497210f7cc2f464e3bfffadefa9806193ccdf873203cd91c8d3eab518": {
-    "balance": "838137708091124174",
-    "code":
-    "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-    "address": "0x0000000000000000000000000000000000000100",
-    "nonce": "0x0",
-    "storage": {
-      "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563":
-      {
-        "slot": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        "value": "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe"
-      }
-    }
-  },
-  "0x4599828688a5c37132b6fc04e35760b4753ce68708a7b7d4d97b940047557fdb": {
-    "balance": "838137708091124174",
-    "code":
-    "0x60047fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-    "address": "0x0000000000000000000000000000000000000101",
-    "nonce": "0x0",
-    "storage": {}
-  },
-  "0x4c933a84259efbd4fb5d1522b5255e6118da186a2c71ec5efaa5c203067690b7": {
-    "balance": "838137708091124174",
-    "code":
-    "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60010160005500",
-    "address": "0x0000000000000000000000000000000000000104",
-    "nonce": "0x0",
-    "storage": {}
-  },
-  "0x9d860e7bb7e6b09b87ab7406933ef2980c19d7d0192d8939cf6dc6908a03305f": {
-    "balance": "459340",
-    "code": "0x",
-    "address": "0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
-    "nonce": "0x0",
-    "storage": {}
-  },
-  "0xa17eacbc25cda025e81db9c5c62868822c73ce097cee2a63e33a2e41268358a1": {
-    "balance": "838137708091124174",
-    "code":
-    "0x60017fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
-    "address": "0x0000000000000000000000000000000000000102",
-    "nonce": "0x0",
-    "storage": {}
-  },
-  "0xa5cc446814c4e9060f2ecb3be03085683a83230981ca8f19d35a4438f8c2d277": {
-    "balance": "838137708091124174",
-    "code": "0x600060000160005500",
-    "address": "0x0000000000000000000000000000000000000103",
-    "nonce": "0x0",
-    "storage": {}
-  },
-  "0xf057b39b049c7df5dfa86c4b0869abe798cef059571a5a1e5bbf5168cf6c097b": {
-    "balance": "838137708091124175",
-    "code": "0x600060006000600060006004356101000162fffffff100",
-    "address": "0xcccccccccccccccccccccccccccccccccccccccc",
-    "nonce": "0x0",
-    "storage": {}
-  }
-})";
-
-    auto const expected_payload = nlohmann::json::parse(json_str);
 
     struct InMemoryTrieDbFixture : public ::testing::Test
     {
@@ -280,6 +157,13 @@ TYPED_TEST(DBTest, read_storage)
 
 TYPED_TEST(DBTest, read_code)
 {
+    auto const code1 =
+        std::make_shared<CodeAnalysis>(analyze(byte_string{0xab, 0xcd, 0xef}));
+    auto const code2 =
+        std::make_shared<CodeAnalysis>(analyze(byte_string{0xbb, 0xbb, 0xbb}));
+    constexpr auto code_hash2 =
+        0x1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1c1bbbbbbbbb_bytes32;
+
     Account acct_a{.balance = 1, .code_hash = code_hash1, .nonce = 1};
     TrieDb tdb{this->db};
     tdb.commit(
@@ -439,15 +323,6 @@ TYPED_TEST(DBTest, commit_receipts)
 
 TYPED_TEST(DBTest, to_json)
 {
-    auto const a = 0x0000000000000000000000000000000000000100_address;
-    auto const b = 0x0000000000000000000000000000000000000101_address;
-    auto const c = 0x0000000000000000000000000000000000000102_address;
-    auto const d = 0x0000000000000000000000000000000000000103_address;
-    auto const e = 0x0000000000000000000000000000000000000104_address;
-    auto const f = 0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba_address;
-    auto const g = 0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b_address;
-    auto const h = 0xcccccccccccccccccccccccccccccccccccccccc_address;
-
     std::filesystem::path dbname{};
     if (this->on_disk) {
         dbname = {
@@ -462,69 +337,77 @@ TYPED_TEST(DBTest, to_json)
         return mpt::Db{this->machine};
     }();
     TrieDb tdb{db};
-    tdb.commit(
-        StateDeltas{
-            {a,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9ce,
-                          .code_hash = a_code_hash}},
-                 .storage =
-                     {{bytes32_t{},
-                       {bytes32_t{},
-                        0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe_bytes32}}}}},
-            {b,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9ce,
-                          .code_hash = b_code_hash}}}},
-            {c,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9ce,
-                          .code_hash = c_code_hash}}}},
-            {d,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9ce,
-                          .code_hash = d_code_hash}}}},
-            {e,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9ce,
-                          .code_hash = e_code_hash}}}},
-            {f,
-             StateDelta{
-                 .account = {std::nullopt, Account{.balance = 0x7024c}}}},
-            {g,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{.balance = 0xba1a9ce0b9aa781, .nonce = 1}}}},
-            {h,
-             StateDelta{
-                 .account =
-                     {std::nullopt,
-                      Account{
-                          .balance = 0xba1a9ce0ba1a9cf,
-                          .code_hash = h_code_hash}}}}},
-        Code{
-            {a_code_hash, a_code_analysis},
-            {b_code_hash, b_code_analysis},
-            {c_code_hash, c_code_analysis},
-            {d_code_hash, d_code_analysis},
-            {e_code_hash, e_code_analysis},
-            {h_code_hash, h_code_analysis}});
+    load_db(tdb, 0);
+
+    auto const expected_payload = nlohmann::json::parse(R"(
+{
+  "0x03601462093b5945d1676df093446790fd31b20e7b12a2e8e5e09d068109616b": {
+    "balance": "838137708090664833",
+    "code": "0x",
+    "address": "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b",
+    "nonce": "0x1",
+    "storage": {}
+  },
+  "0x227a737497210f7cc2f464e3bfffadefa9806193ccdf873203cd91c8d3eab518": {
+    "balance": "838137708091124174",
+    "code":
+    "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "address": "0x0000000000000000000000000000000000000100",
+    "nonce": "0x0",
+    "storage": {
+      "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563":
+      {
+        "slot": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "value": "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe"
+      }
+    }
+  },
+  "0x4599828688a5c37132b6fc04e35760b4753ce68708a7b7d4d97b940047557fdb": {
+    "balance": "838137708091124174",
+    "code":
+    "0x60047fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "address": "0x0000000000000000000000000000000000000101",
+    "nonce": "0x0",
+    "storage": {}
+  },
+  "0x4c933a84259efbd4fb5d1522b5255e6118da186a2c71ec5efaa5c203067690b7": {
+    "balance": "838137708091124174",
+    "code":
+    "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff60010160005500",
+    "address": "0x0000000000000000000000000000000000000104",
+    "nonce": "0x0",
+    "storage": {}
+  },
+  "0x9d860e7bb7e6b09b87ab7406933ef2980c19d7d0192d8939cf6dc6908a03305f": {
+    "balance": "459340",
+    "code": "0x",
+    "address": "0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
+    "nonce": "0x0",
+    "storage": {}
+  },
+  "0xa17eacbc25cda025e81db9c5c62868822c73ce097cee2a63e33a2e41268358a1": {
+    "balance": "838137708091124174",
+    "code":
+    "0x60017fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0160005500",
+    "address": "0x0000000000000000000000000000000000000102",
+    "nonce": "0x0",
+    "storage": {}
+  },
+  "0xa5cc446814c4e9060f2ecb3be03085683a83230981ca8f19d35a4438f8c2d277": {
+    "balance": "838137708091124174",
+    "code": "0x600060000160005500",
+    "address": "0x0000000000000000000000000000000000000103",
+    "nonce": "0x0",
+    "storage": {}
+  },
+  "0xf057b39b049c7df5dfa86c4b0869abe798cef059571a5a1e5bbf5168cf6c097b": {
+    "balance": "838137708091124175",
+    "code": "0x600060006000600060006004356101000162fffffff100",
+    "address": "0xcccccccccccccccccccccccccccccccccccccccc",
+    "nonce": "0x0",
+    "storage": {}
+  }
+})");
 
     if (this->on_disk) {
         // also test to_json from a read only db
