a56edba62d0c585b2776f332d9a5ec50ce74a8ca
Kevin Kuehler <kkuehler@category.xyz>
2024-09-03 17:43:20 -0400

    precompiles: implement identity

--- libs/execution/src/monad/execution/precompiles_impl.cpp
+++ libs/execution/src/monad/execution/precompiles_impl.cpp
@@ -5,6 +5,17 @@
 #include <evmc/evmc.h>
 #include <silkpre/precompile.h>
 
+#include <cstring>
+
+namespace
+{
+    constexpr size_t num_words(size_t const length)
+    {
+        constexpr size_t WORD_SIZE = 32;
+        return (length + WORD_SIZE - 1) / WORD_SIZE;
+    }
+}
+
 MONAD_NAMESPACE_BEGIN
 
 template <SilkpreRunFunction Func>
@@ -39,7 +50,8 @@ ripemd160_gas_cost(byte_string_view const input, evmc_revision const rev)
 
 uint64_t identity_gas_cost(byte_string_view const input, evmc_revision)
 {
-    return silkpre_id_gas(input.data(), input.size(), static_cast<int>(rev));
+    // YP eqn 232
+    return 15 + 3 * num_words(input.size());
 }
 
 uint64_t ecadd_gas_cost(byte_string_view const input, evmc_revision const rev)
@@ -100,7 +112,10 @@ PrecompileResult ecmul_execute(byte_string_view const input)
 
 PrecompileResult identity_execute(byte_string_view const input)
 {
-    return silkpre_execute<silkpre_id_run>(input);
+    auto output = static_cast<uint8_t *>(malloc(input.size()));
+    MONAD_ASSERT(output != nullptr);
+    memcpy(output, input.data(), input.size());
+    return {EVMC_SUCCESS, output, input.size()};
 }
 
 PrecompileResult expmod_execute(byte_string_view const input)
