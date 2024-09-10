eb41dda51bd4252cf72c4764d0e333cb910c4ead
Ken Camann <ken@monad.xyz>
2024-09-09 20:07:54 -0400

    [core] add a MONAD_ABORT macro, to prefer over MONAD_ASSERT(false)
    Occasionally we flow to a place in the code where we must die, where there
    is not obvious choice for the exact expression to assert is false. There are
    currently about 20 calls to `MONAD_ASSERT(false)` when this happens, which
    is not very informative.
    
    This adds a MONAD_ABORT macro. The difference over the abort() function
    in <stdlib.h> is that it must include a printf-style message that explains
    why we cannot continue. It includes the same source location and stack
    backtrace machinery as the assertion failure function.

--- libs/core/src/monad/core/assert.c
+++ libs/core/src/monad/core/assert.c
@@ -11,6 +11,40 @@ extern void monad_stack_backtrace_capture_and_print(
     char *buffer, size_t size, int fd, unsigned indent,
     _Bool print_async_unsafe_info);
 
+void monad_vabort(
+    char const *function, char const *file, long line, char const *format,
+    va_list ap)
+{
+    char buffer[16384];
+    monad_stack_backtrace_capture_and_print(buffer, sizeof(buffer), 2, 3, 1);
+    ssize_t written = snprintf(
+        buffer,
+        sizeof(buffer),
+        "%s: %s:%ld: %s: ABORT: ",
+        __progname,
+        file,
+        line,
+        function);
+    if ((size_t)written >= sizeof(buffer)) {
+        written = (int)(sizeof(buffer) - 1);
+    }
+    if (format != nullptr && written > 0 &&
+        written < (int)(sizeof(buffer) - 1)) {
+        written += vsnprintf(
+            buffer + written, sizeof(buffer) - (size_t)written, format, ap);
+    }
+    if ((size_t)written >= sizeof(buffer)) {
+        written = (int)(sizeof(buffer) - 1);
+    }
+    // abort() is async signal safe in glibc
+    written = write(2 /*stderr*/, buffer, (size_t)written);
+    if (written == -1) {
+        // This is to shut up the warning
+        abort();
+    }
+    abort();
+}
+
 void monad_assertion_failed(
     char const *const expr, char const *const function, char const *const file,
     long const line)
--- libs/core/src/monad/core/assert.h
+++ libs/core/src/monad/core/assert.h
@@ -1,14 +1,35 @@
 #pragma once
 
 #include <monad/core/likely.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-void __attribute__((noreturn)) monad_assertion_failed(
+[[noreturn]] void monad_vabort(
+    char const *function, char const *file, long line, char const *format,
+    va_list ap);
+
+[[noreturn]] static inline void monad_abort(
+    char const *function, char const *file, long line, char const *format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    monad_vabort(function, file, line, format, ap);
+    va_end(ap);
+}
+
+[[noreturn]] void monad_assertion_failed(
     char const *expr, char const *function, char const *file, long line);
+
+#define MONAD_ABORT(FORMAT, ...)                                               \
+    monad_abort(                                                               \
+        __extension__ __PRETTY_FUNCTION__,                                     \
+        __FILE__,                                                              \
+        __LINE__,                                                              \
+        FORMAT __VA_OPT__(, ) __VA_ARGS__)
 
 #define MONAD_ASSERT(expr)                                                     \
     if (MONAD_LIKELY(expr)) { /* likeliest */                                  \
