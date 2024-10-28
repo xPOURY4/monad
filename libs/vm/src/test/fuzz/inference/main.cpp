41130c7916cd57e999804df658f6f10bf81ab10e
goodlyrottenapple <goodlyrottenapple@gmail.com>
2024-10-28 09:56:27 -0400

    format

--- libs/vm/src/test/fuzz/inference/main.cpp
+++ libs/vm/src/test/fuzz/inference/main.cpp
@@ -9,7 +9,6 @@
 #include <compiler/ir/poly_typed/kind.h>
 #include <compiler/opcodes.h>
 
-
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
@@ -17,9 +16,7 @@
 #include <optional>
 #include <vector>
 
-
 __AFL_FUZZ_INIT();
-
 
 #pragma clang diagnostic ignored "-Weverything"
 #pragma clang optimize off
@@ -31,26 +28,25 @@ int main(int argc, char **argv)
     __AFL_INIT();
 
 #ifdef AFL_PERSISTENT_REPLAY_ARGPARSE
-    const char *filename = argv[1];
+    char const *filename = argv[1];
     auto in = std::ifstream(filename, std::ios::binary);
-    auto program = std::vector<uint8_t>(std::istreambuf_iterator<char>(in),
-                                        std::istreambuf_iterator<char>());
+    auto program = std::vector<uint8_t>(
+        std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
 #else
     unsigned char *buf = __AFL_FUZZ_TESTCASE_BUF;
-while (__AFL_LOOP(100000)) {
-    int len = __AFL_FUZZ_TESTCASE_LEN;
-    auto program = std::vector(buf, buf + len);
+    while (__AFL_LOOP(100000)) {
+        int len = __AFL_FUZZ_TESTCASE_LEN;
+        auto program = std::vector(buf, buf + len);
 #endif
 
     auto bytecode = monad::compiler::bytecode::BytecodeIR(program);
     auto ir = monad::compiler::local_stacks::LocalStacksIR(
-            monad::compiler::basic_blocks::BasicBlocksIR(bytecode)
-        );
+        monad::compiler::basic_blocks::BasicBlocksIR(bytecode));
     monad::compiler::poly_typed::infer_types(ir.jumpdests, ir.blocks);
 
 #ifndef AFL_PERSISTENT_REPLAY_ARGPARSE
 }
 #endif
 
-    return 0;
+return 0;
 }
