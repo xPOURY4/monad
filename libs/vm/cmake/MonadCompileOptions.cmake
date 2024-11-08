2a877fbca4959ac0685c3865933362afbc948532
Bruce Collie <brucecollie82@gmail.com>
2024-11-08 08:30:28 -0500

    Hide CMake collisions behind option
    This means that the client repo will be able to disable troublesome
    collisions that are produced by compatibility code.

--- libs/vm/cmake/MonadCompileOptions.cmake
+++ libs/vm/cmake/MonadCompileOptions.cmake
@@ -1,18 +1,20 @@
-# Excerpted directly from:
-#   https://github.com/monad-crypto/monad/blob/main/CMakeLists.txt#L18
-# in the interests of upstreaming this project into the core Monad monorepo.
-function(monad_compile_options target)
-  set_property(TARGET ${target} PROPERTY C_STANDARD 23)
-  set_property(TARGET ${target} PROPERTY C_STANDARD_REQUIRED ON)
-  set_property(TARGET ${target} PROPERTY CXX_STANDARD 23)
-  set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
+if(MONAD_COMPILER_STANDALONE)
+  # Excerpted directly from:
+  #   https://github.com/monad-crypto/monad/blob/main/CMakeLists.txt#L18
+  # in the interests of upstreaming this project into the core Monad monorepo.
+  function(monad_compile_options target)
+    set_property(TARGET ${target} PROPERTY C_STANDARD 23)
+    set_property(TARGET ${target} PROPERTY C_STANDARD_REQUIRED ON)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD 23)
+    set_property(TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON)
 
-  target_compile_options(${target} PRIVATE -Wall -Wextra -Wconversion -Werror)
+    target_compile_options(${target} PRIVATE -Wall -Wextra -Wconversion -Werror)
 
-  target_compile_options(
-    ${target} PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wno-missing-field-initializers>)
+    target_compile_options(
+      ${target} PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wno-missing-field-initializers>)
 
-  target_compile_options(
-    ${target}
-    PUBLIC $<$<CXX_COMPILER_ID:GNU>:-Wno-attributes=clang::no_sanitize>)
-endfunction()
+    target_compile_options(
+      ${target}
+      PUBLIC $<$<CXX_COMPILER_ID:GNU>:-Wno-attributes=clang::no_sanitize>)
+  endfunction()
+endif()
