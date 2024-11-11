869594f83da9db365ab1e3a2ca341cd1187f8a26
Andreas Lynge <andreaslyn@gmail.com>
2024-11-11 20:01:37 +0100

    Call type check in inference fuzz test

--- libs/vm/src/test/fuzz/inference/main.cpp
+++ libs/vm/src/test/fuzz/inference/main.cpp
@@ -4,6 +4,7 @@
 #include <compiler/ir/basic_blocks.h>
 #include <compiler/ir/bytecode.h>
 #include <compiler/ir/local_stacks.h>
+#include <compiler/ir/poly_typed.h>
 #include <compiler/ir/poly_typed/block.h>
 #include <compiler/ir/poly_typed/infer.h>
 #include <compiler/ir/poly_typed/kind.h>
@@ -11,6 +12,7 @@
 #include <cstdint>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <iterator>
 #include <optional>
 #include <vector>
@@ -37,15 +39,12 @@ int main(int argc, char **argv)
         int len = __AFL_FUZZ_TESTCASE_LEN;
         auto program = std::vector(buf, buf + len);
 #endif
-
     auto bytecode = monad::compiler::bytecode::BytecodeIR(program);
-    auto ir = monad::compiler::local_stacks::LocalStacksIR(
-        monad::compiler::basic_blocks::BasicBlocksIR(bytecode));
-    monad::compiler::poly_typed::infer_types(ir.jumpdests, ir.blocks);
-
+    auto ir = monad::compiler::poly_typed::PolyTypedIR(
+        monad::compiler::local_stacks::LocalStacksIR(
+            monad::compiler::basic_blocks::BasicBlocksIR(bytecode)));
+    ir.type_check_or_throw();
 #ifndef AFL_PERSISTENT_REPLAY_ARGPARSE
 }
 #endif
-
-return 0;
 }
