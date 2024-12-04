4d1d9278f83d5ce7d33cade6f9f7072116d1995f
Bruce Collie <brucecollie82@gmail.com>
2024-12-04 11:25:34 -0500

    Test SMOD

--- libs/vm/src/runtime/include/runtime/math.h
+++ libs/vm/src/runtime/include/runtime/math.h
@@ -52,8 +52,8 @@ namespace monad::runtime
 
     template <evmc_revision Rev>
     void smod(
-        Context *, utils::uint256_t *result_ptr, utils::uint256_t *const a_ptr,
-        utils::uint256_t *const b_ptr)
+        Context *, utils::uint256_t *result_ptr, utils::uint256_t const *a_ptr,
+        utils::uint256_t const *b_ptr)
     {
         if (*b_ptr == 0) {
             *result_ptr = 0;
