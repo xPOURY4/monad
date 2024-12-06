53c54be671a763729593c97a405e53c77d7a6486
Bruce Collie <brucecollie82@gmail.com>
2024-12-06 08:22:06 -0500

    Test keccak

--- libs/vm/src/runtime/include/runtime/keccak.h
+++ libs/vm/src/runtime/include/runtime/keccak.h
@@ -16,7 +16,7 @@ namespace monad::runtime
     template <evmc_revision Rev>
     void sha3(
         Context *ctx, utils::uint256_t *result_ptr,
-        utils::uint256_t *const offset_ptr, utils::uint256_t *const size_ptr)
+        utils::uint256_t const *offset_ptr, utils::uint256_t const *size_ptr)
     {
         auto [offset, size] =
             ctx->get_memory_offset_and_size(*offset_ptr, *size_ptr);
