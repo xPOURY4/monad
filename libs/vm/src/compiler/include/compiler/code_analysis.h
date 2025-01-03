635cd55fb2a3abdfd94bb575acbc3e201df9a00d
Bruce Collie <brucecollie82@gmail.com>
2025-01-03 12:25:09 -0500

    Add revision-aware variant

--- libs/vm/src/compiler/include/compiler/code_analysis.h
+++ libs/vm/src/compiler/include/compiler/code_analysis.h
@@ -8,8 +8,13 @@ namespace monad
 {
     using CodeAnalysis = ::evmone::baseline::CodeAnalysis;
 
+    inline CodeAnalysis analyze(evmc_revision rev, evmc::bytes_view const &code)
+    {
+        return evmone::baseline::analyze(rev, code);
+    }
+
     inline CodeAnalysis analyze(evmc::bytes_view const &code)
     {
-        return evmone::baseline::analyze(EVMC_FRONTIER, code); // TODO rev
+        return analyze(EVMC_FRONTIER, code);
     }
 }
