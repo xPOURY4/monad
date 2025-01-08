ba7886e829b08e12c025d3457737644fa2acdf5d
Bruce Collie <brucecollie82@gmail.com>
2025-01-08 06:34:47 -0500

    Implement benchmark runner

---
+++ libs/vm/src/test/blockchain/blockchain_benchmarks.cpp
@@ -0,0 +1,86 @@
+#include "blockchaintest.hpp"
+#include "test_vm.h"
+
+#include <test_resource_data.h>
+
+#include <evmc/evmc.hpp>
+
+#include <evmone/evmone.h>
+
+#include <benchmark/benchmark.h>
+
+#include <array>
+#include <filesystem>
+#include <format>
+#include <fstream>
+
+using namespace evmone::test;
+
+namespace fs = std::filesystem;
+
+namespace
+{
+    auto vm_performance_dir = test_resource::ethereum_tests_dir /
+                              "BlockchainTests" / "GeneralStateTests" /
+                              "VMTests" / "vmPerformance";
+
+    template <bool compile>
+    void run_benchmark(benchmark::State &state, fs::path const &json_file)
+    {
+        if (!fs::is_regular_file(json_file) ||
+            json_file.extension() != ".json") {
+            state.SkipWithError("Not a JSON test fixture");
+            return;
+        }
+
+        auto vm = [] {
+            if constexpr (compile) {
+                return evmc::VM{new BlockchainTestVM};
+            }
+            else {
+                return evmc::VM{evmc_create_evmone()};
+            }
+        }();
+
+        auto in_file = std::ifstream(json_file);
+        auto tests = evmone::test::load_blockchain_tests(in_file);
+
+        for (auto _ : state) {
+            evmone::test::run_blockchain_tests(tests, vm);
+        }
+    }
+
+    auto benchmark_tests()
+    {
+        return std::array{
+            vm_performance_dir / "loopExp.json",
+            vm_performance_dir / "loopMul.json",
+            vm_performance_dir / "performanceTester.json",
+        };
+    }
+
+    void register_benchmarks()
+    {
+        for (auto const &test : benchmark_tests()) {
+            auto stem = test.stem();
+
+            benchmark::RegisterBenchmark(
+                std::format("{}/compiled", stem.string()),
+                run_benchmark<true>,
+                test);
+
+            benchmark::RegisterBenchmark(
+                std::format("{}/evmone", stem.string()),
+                run_benchmark<false>,
+                test);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    register_benchmarks();
+
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+}
