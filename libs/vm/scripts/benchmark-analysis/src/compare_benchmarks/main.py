63632be9e6de9ff0b157ac5bc6d80123bf69ef46
Bruce Collie <brucecollie82@gmail.com>
2025-01-23 06:54:39 -0500

    Compare compile time results

--- libs/vm/scripts/benchmark-analysis/src/compare_benchmarks/main.py
+++ libs/vm/scripts/benchmark-analysis/src/compare_benchmarks/main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import pathlib
+from collections.abc import Callable
 
 import pandas as pd
 import scipy.stats
@@ -15,42 +16,64 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--before', type=pathlib.Path, required=True, help='Old benchmark results (before PR applied)')
     parser.add_argument('--after', type=pathlib.Path, required=True, help='New benchmark results (after PR applied)')
 
+    parser.add_argument(
+        '--type', choices=['run', 'compile'], required=True, help='Which set of benchmark data to compare'
+    )
+
     return parser.parse_args()
 
 
-def split_benchmark_impl(row: pd.Series) -> tuple[str, str]:
-    parts = row['name'].split('/')
-    return (parts[0], parts[1])
+def load_single_runtime_results(path: pathlib.Path, label: str) -> pd.DataFrame:
+    def _split_benchmark_impl(row: pd.Series) -> tuple[str, str]:
+        parts = row['name'].split('/')
+        return (parts[0], parts[1])
 
-
-def load_single_results(path: pathlib.Path, label: str) -> pd.DataFrame:
     with open(path, 'r') as f:
         json_data = json.load(f)
 
     df = pd.json_normalize(json_data['benchmarks'])
+
     df = df[df.name.str.endswith('_mean')]
     df.name = df.name.str.removesuffix('_mean')
-    df = df.drop(
-        [
-            'family_index',
-            'per_family_instance_index',
-            'run_name',
-            'run_type',
-            'cpu_time',
-            'repetitions',
-            'threads',
-        ],
-        axis='columns',
-    )
 
     df['version'] = label
-    df[['name', 'implementation']] = df.apply(split_benchmark_impl, axis='columns', result_type='expand')
+    df[['name', 'implementation']] = df.apply(_split_benchmark_impl, axis='columns', result_type='expand')
     df = df[df.implementation == 'compiled']
+
     return df
 
 
-def load_results(before: pathlib.Path, after: pathlib.Path) -> pd.DataFrame:
-    df = pd.concat([load_single_results(before, 'before'), load_single_results(after, 'after')]).reset_index(drop=True)
+def load_single_compile_results(path: pathlib.Path, label: str) -> pd.DataFrame:
+    def _compile_benchmark(row: pd.Series) -> str:
+        parts = row['name'].split('/')
+        return parts[1]
+
+    with open(path, 'r') as f:
+        json_data = json.load(f)
+
+    df = pd.json_normalize(json_data['benchmarks'])
+
+    df = df[~df.name.str.startswith('complexity')]
+    df = df[df.name.str.endswith('mean')]
+
+    df.name = df.name.str.removesuffix('_mean')
+    df['version'] = label
+    df['name'] = df.apply(_compile_benchmark, axis='columns', result_type='expand')
+
+    return df
+
+
+def load_json_results(
+    loader: Callable[[pathlib.Path, str], pd.DataFrame],
+    before: pathlib.Path,
+    after: pathlib.Path,
+) -> pd.DataFrame:
+    df = pd.concat(
+        [
+            loader(before, 'before'),
+            loader(after, 'after'),
+        ]
+    ).reset_index(drop=True)
     assert len(set(df.time_unit.tolist())) == 1
     return df
 
@@ -83,9 +106,20 @@ def to_markdown_with_stats(df: pd.DataFrame) -> str:
     return f'{main_table}\n{geomean_row}'
 
 
+def loader(type: str) -> Callable[[pathlib.Path, str], pd.DataFrame]:
+    if type == 'run':
+        return load_single_runtime_results
+    elif type == 'compile':
+        return load_single_compile_results
+    else:
+        raise ValueError
+
+
 def main() -> None:
     args = parse_args()
-    df = compute_ratio(load_results(args.before, args.after))
+
+    df = load_json_results(loader(args.type), args.before, args.after)
+    df = compute_ratio(df)
     df = relabel_for_printing(df)
     print(to_markdown_with_stats(df))
 
