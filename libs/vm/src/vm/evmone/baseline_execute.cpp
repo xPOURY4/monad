9ae767a20bcaefae3909f9d26bd8fdba27fa794c
Bruce Collie <brucecollie82@gmail.com>
2025-01-24 06:28:35 -0500

    Fix baseline execution error

--- libs/vm/src/vm/evmone/baseline_execute.cpp
+++ libs/vm/src/vm/evmone/baseline_execute.cpp
@@ -13,11 +13,19 @@
 
 namespace monad
 {
+    struct VMDeleter
+    {
+        static void operator()(evmc_vm *vm)
+        {
+            vm->destroy(vm);
+        }
+    };
+
     evmc::Result baseline_execute(
         evmc_message const &msg, evmc_revision const rev,
         evmc::Host *const host, CodeAnalysis const &code_analysis)
     {
-        std::unique_ptr<evmc_vm> const vm{evmc_create_evmone()};
+        std::unique_ptr<evmc_vm, VMDeleter> const vm{evmc_create_evmone()};
 
         if (code_analysis.executable_code.empty()) {
             return evmc::Result{EVMC_SUCCESS, msg.gas};
