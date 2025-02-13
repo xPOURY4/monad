b3d12e1690b3a5b69c49096e3201f4c41e429a8b
Kevin Kuehler <kkuehler@category.xyz>
2025-02-13 12:05:11 -0500

    Add monad refund test

---
+++ libs/execution/src/monad/execution/test/test_monad_refund.cpp
@@ -0,0 +1,22 @@
+#include <monad/chain/monad_testnet.hpp>
+#include <monad/core/block.hpp>
+#include <monad/core/transaction.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace monad;
+
+TEST(MonadRefund, testnet_fork)
+{
+    MonadTestnet monad_chain;
+    Transaction tx{.gas_limit = 21'000};
+
+    BlockHeader before_fork{.number = 0, .timestamp = 0};
+    BlockHeader after_fork{.number = 1, .timestamp = 1739559600};
+
+    auto const refund_before_fork = monad_chain.compute_gas_refund(
+        before_fork.number, before_fork.timestamp, tx, 20'000, 1000);
+    auto const refund_after_fork = monad_chain.compute_gas_refund(
+        after_fork.number, after_fork.timestamp, tx, 20'000, 1000);
+    EXPECT_EQ(20'200, refund_before_fork - refund_after_fork);
+}
