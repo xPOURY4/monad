c6f127a4768a4354e249f4fdc7a87b58c3cac9da
Alexander Lee <1354409+zander-xyz@users.noreply.github.com>
2025-03-04 11:15:43 -0500

    MonadChain.get_max_code_size

--- libs/execution/src/monad/execution/test/test_monad_chain.cpp
+++ libs/execution/src/monad/execution/test/test_monad_chain.cpp
@@ -1,3 +1,4 @@
+#include <monad/chain/ethereum_mainnet.hpp>
 #include <monad/chain/monad_testnet.hpp>
 #include <monad/core/block.hpp>
 #include <monad/core/transaction.hpp>
@@ -19,4 +20,11 @@ TEST(MonadChain, compute_gas_refund)
     auto const refund_after_fork = monad_chain.compute_gas_refund(
         after_fork.number, after_fork.timestamp, tx, 20'000, 1000);
     EXPECT_EQ(20'200, refund_before_fork - refund_after_fork);
+}
+
+TEST(MonadChain, get_max_code_size)
+{
+    MonadTestnet const chain;
+    EXPECT_EQ(chain.get_max_code_size(0, 1739559600), MAX_CODE_SIZE_EIP170);
+    EXPECT_EQ(chain.get_max_code_size(0, 1741978800), 128 * 1024);
 }
