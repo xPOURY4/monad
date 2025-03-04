d411dfa424816f54bd3ca8921542faa975b7376d
Brett Letner <brett@monad.xyz>
2025-03-04 11:16:23 -0500

    clang format

--- libs/vm/libs/utils/src/monad/utils/parser.hpp
+++ libs/vm/libs/utils/src/monad/utils/parser.hpp
@@ -9,24 +9,27 @@
 
 namespace monad::utils
 {
-char const *try_parse_line_comment(char const *input);
+    char const *try_parse_line_comment(char const *input);
 
     /**
-     * parse an evm opcode text file and return the resulting vector of evm bytecode
+     * parse an evm opcode text file and return the resulting vector of evm
+     * bytecode
      *
      * Notes:
      * case is ignored
      * data can be in hex or decimal form
-     * pushes can be statically sized (e.g. push3 0xabcdef) or computed size (e.g. push 0xabcdef)
-     * jumpdests can use named labels, e.g. push .mylabel jumpdest .mylabel
-     * end of line comments (// .. \n) and whitespace are ignored
+     * pushes can be statically sized (e.g. push3 0xabcdef) or computed size
+     * (e.g. push 0xabcdef) jumpdests can use named labels, e.g. push .mylabel
+     * jumpdest .mylabel end of line comments (// .. \n) and whitespace are
+     * ignored
      */
 
-std::vector<uint8_t> parse_opcodes(bool verbose, std::string filename, std::string str);
+    std::vector<uint8_t>
+    parse_opcodes(bool verbose, std::string filename, std::string str);
 
     /**
      * show text opcodes and data from binary evm bytecode
      */
-void show_opcodes(std::vector<uint8_t> &opcodes);
+    void show_opcodes(std::vector<uint8_t> &opcodes);
 
 }
