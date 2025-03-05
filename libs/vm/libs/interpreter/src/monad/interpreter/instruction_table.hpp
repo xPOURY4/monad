2a9ef2fc786068e05812660187c322b9cccf972c
Bruce Collie <brucecollie82@gmail.com>
2025-03-05 06:02:31 -0500

    Refactor instruction table

--- libs/vm/libs/interpreter/src/monad/interpreter/instruction_table.hpp
+++ libs/vm/libs/interpreter/src/monad/interpreter/instruction_table.hpp
@@ -16,13 +16,7 @@ namespace monad::interpreter
     using InstrTable = std::array<InstrEval, 256>;
 
     template <evmc_revision Rev>
-    consteval InstrTable make_instruction_table() = delete;
-
-    template <evmc_revision Rev>
-    constexpr InstrTable instruction_table = make_instruction_table<Rev>();
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_FRONTIER>()
+    consteval InstrTable make_instruction_table()
     {
         return {
             stop, // 0x00
@@ -35,8 +29,8 @@ namespace monad::interpreter
             smod, // 0x07,
             addmod, // 0x08,
             mulmod, // 0x09,
-            exp<EVMC_FRONTIER>, // 0x0A,
-            stop, // 0x0B,
+            exp<Rev>, // 0x0A,
+            signextend, // 0x0B,
             error, //
             error, //
             error, //
@@ -115,7 +109,7 @@ namespace monad::interpreter
             stop, // 0x52,
             stop, // 0x53,
             stop, // 0x54,
-            sstore<EVMC_FRONTIER>, // 0x55,
+            sstore<Rev>, // 0x55,
             stop, // 0x56,
             stop, // 0x57,
             stop, // 0x58,
@@ -281,7 +275,7 @@ namespace monad::interpreter
             error, //
 
             stop, // 0xF0,
-            call<EVMC_FRONTIER>, // 0xF1,
+            call<Rev>, // 0xF1,
             stop, // 0xF2,
             stop, // 0xF3,
             error, //
@@ -299,162 +293,6 @@ namespace monad::interpreter
         };
     }
 
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_HOMESTEAD>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_HOMESTEAD)>;
-        table[EXP] = exp<EVMC_HOMESTEAD>;
-        table[SSTORE] = sstore<EVMC_HOMESTEAD>;
-        table[CALL] = call<EVMC_HOMESTEAD>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_TANGERINE_WHISTLE>()
-    {
-        using namespace monad::compiler;
-
-        auto table =
-            instruction_table<previous_evm_revision(EVMC_TANGERINE_WHISTLE)>;
-        table[EXP] = exp<EVMC_TANGERINE_WHISTLE>;
-        table[SSTORE] = sstore<EVMC_TANGERINE_WHISTLE>;
-        table[CALL] = call<EVMC_TANGERINE_WHISTLE>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_SPURIOUS_DRAGON>()
-    {
-        using namespace monad::compiler;
-
-        auto table =
-            instruction_table<previous_evm_revision(EVMC_SPURIOUS_DRAGON)>;
-        table[EXP] = exp<EVMC_SPURIOUS_DRAGON>;
-        table[SSTORE] = sstore<EVMC_SPURIOUS_DRAGON>;
-        table[CALL] = call<EVMC_SPURIOUS_DRAGON>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_BYZANTIUM>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_BYZANTIUM)>;
-        table[EXP] = exp<EVMC_BYZANTIUM>;
-        table[SSTORE] = sstore<EVMC_BYZANTIUM>;
-        table[CALL] = call<EVMC_BYZANTIUM>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_CONSTANTINOPLE>()
-    {
-        using namespace monad::compiler;
-
-        auto table =
-            instruction_table<previous_evm_revision(EVMC_CONSTANTINOPLE)>;
-        table[EXP] = exp<EVMC_CONSTANTINOPLE>;
-        table[SSTORE] = sstore<EVMC_CONSTANTINOPLE>;
-        table[CALL] = call<EVMC_CONSTANTINOPLE>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_PETERSBURG>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_PETERSBURG)>;
-        table[EXP] = exp<EVMC_PETERSBURG>;
-        table[SSTORE] = sstore<EVMC_PETERSBURG>;
-        table[CALL] = call<EVMC_PETERSBURG>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_ISTANBUL>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_ISTANBUL)>;
-        table[EXP] = exp<EVMC_ISTANBUL>;
-        table[SSTORE] = sstore<EVMC_ISTANBUL>;
-        table[CALL] = call<EVMC_ISTANBUL>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_BERLIN>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_BERLIN)>;
-        table[EXP] = exp<EVMC_BERLIN>;
-        table[SSTORE] = sstore<EVMC_BERLIN>;
-        table[CALL] = call<EVMC_BERLIN>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_LONDON>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_LONDON)>;
-        table[EXP] = exp<EVMC_LONDON>;
-        table[SSTORE] = sstore<EVMC_LONDON>;
-        table[CALL] = call<EVMC_LONDON>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_PARIS>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_PARIS)>;
-        table[EXP] = exp<EVMC_PARIS>;
-        table[SSTORE] = sstore<EVMC_PARIS>;
-        table[CALL] = call<EVMC_PARIS>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_SHANGHAI>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_SHANGHAI)>;
-        table[EXP] = exp<EVMC_SHANGHAI>;
-        table[SSTORE] = sstore<EVMC_SHANGHAI>;
-        table[CALL] = call<EVMC_SHANGHAI>;
-
-        return table;
-    }
-
-    template <>
-    consteval InstrTable make_instruction_table<EVMC_CANCUN>()
-    {
-        using namespace monad::compiler;
-
-        auto table = instruction_table<previous_evm_revision(EVMC_CANCUN)>;
-        table[EXP] = exp<EVMC_CANCUN>;
-        table[SSTORE] = sstore<EVMC_CANCUN>;
-        table[CALL] = call<EVMC_CANCUN>;
-
-        return table;
-    }
+    template <evmc_revision Rev>
+    constexpr InstrTable instruction_table = make_instruction_table<Rev>();
 }
