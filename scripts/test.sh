fc4e9230f0489539bc549500df093de28d1cab4b
Ellis Clark <eclark0426@gmail.com>
2025-03-05 17:18:06 -0500

    scripts: bump test timeout

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 
 CTEST_PARALLEL_LEVEL=${CTEST_PARALLEL_LEVEL:-$(nproc)} \
-  ctest --output-on-failure --timeout 300 --test-dir build
+  ctest --output-on-failure --timeout 500 --test-dir build
 
 PYTEST_MATCH_ARGS=""
 if [ "${CC}" != "clang" ] || [ "${CMAKE_BUILD_TYPE}" != "RelWithDebInfo" ]; then
