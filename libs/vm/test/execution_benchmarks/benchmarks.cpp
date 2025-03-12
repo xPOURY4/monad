1c80a98add36d189e254fe12785379fdd13fe328
goodlyrottenapple <goodlyrottenapple@gmail.com>
2025-03-12 09:28:09 -0400

    Add benchmarks using the EVM tests JSON format

--- libs/vm/test/execution_benchmarks/benchmarks.cpp
+++ libs/vm/test/execution_benchmarks/benchmarks.cpp
@@ -2,13 +2,16 @@
 
 #include <test_resource_data.h>
 
+#include <test_resource_data.h>
 #include <test_vm.hpp>
 
 #include <evmc/evmc.h>
 #include <evmc/evmc.hpp>
 
+#include "benchmarktest.hpp"
 #include "host.hpp"
 #include "state.hpp"
+#include "test_state.hpp"
 
 #include <benchmark/benchmark.h>
 
@@ -20,6 +23,7 @@
 #include <ios>
 #include <iterator>
 #include <memory>
+#include <nlohmann/json.hpp>
 #include <span>
 #include <string>
 #include <string_view>
@@ -28,10 +32,14 @@
 
 namespace fs = std::filesystem;
 
+namespace json = nlohmann;
+
 using namespace evmone::state;
 using namespace test_resource;
 
 using enum BlockchainTestVM::Implementation;
+
+using namespace monad::test;
 
 struct free_message
 {
@@ -55,6 +63,10 @@ struct benchmark_case
 
 namespace
 {
+    auto vm_performance_dir = test_resource::ethereum_tests_dir /
+                              "BlockchainTests" / "GeneralStateTests" /
+                              "VMTests" / "vmPerformance";
+
     auto make_benchmark(
         std::string const &name, std::span<std::uint8_t const> code,
         std::span<std::uint8_t const> input)
@@ -108,6 +120,10 @@ namespace
             read_file(calldata_path));
     }
 
+    // This benchmark runner assumes that no state is modified during execution,
+    // as it re-uses the same stae between all the runs. For aynthing other that
+    // micro-benchmarks of e.g. specific opcodes, use the JSON format with
+    // `run_benchmark_json`
     void run_benchmark(
         benchmark::State &state, BlockchainTestVM::Implementation const impl,
         evmc_message const msg)
@@ -130,6 +146,36 @@ namespace
         for (auto _ : state) {
             auto const result = vm_ptr->execute(
                 interface, ctx, EVMC_CANCUN, &msg, code, code_size);
+
+            MONAD_COMPILER_DEBUG_ASSERT(result.status_code == EVMC_SUCCESS);
+        }
+    }
+
+    void run_benchmark_json(
+        benchmark::State &state, BlockchainTestVM::Implementation const impl,
+        evmone::test::TestState const &test_state, evmc_message const msg)
+    {
+        auto vm = evmc::VM(new BlockchainTestVM(impl));
+        auto *vm_ptr =
+            reinterpret_cast<BlockchainTestVM *>(vm.get_raw_pointer());
+
+        auto const *const code_acc =
+            test_state.to_intra_state().find(msg.code_address);
+        auto const code = code_acc != nullptr
+                              ? evmc::bytes_view{code_acc->code}
+                              : evmc::bytes_view{msg.code, msg.code_size};
+
+        for (auto _ : state) {
+            state.PauseTiming();
+            auto evm_state = test_state.to_intra_state();
+            auto host =
+                Host(EVMC_CANCUN, vm, evm_state, BlockInfo{}, Transaction{});
+            auto const *interface = &host.get_interface();
+            auto *ctx = host.to_context();
+            state.ResumeTiming();
+
+            auto const result = vm_ptr->execute(
+                interface, ctx, EVMC_CANCUN, &msg, code.data(), code.size());
 
             MONAD_COMPILER_DEBUG_ASSERT(result.status_code == EVMC_SUCCESS);
         }
@@ -171,6 +217,68 @@ namespace
 
         return ret;
     }
+
+    auto make_benchmark_json(std::filesystem::path const &json_test_file)
+    {
+        std::ifstream f{json_test_file};
+        return load_benchmark_tests(f);
+    }
+
+    auto benchmarks_json()
+    {
+        return std::array{
+            make_benchmark_json(vm_performance_dir / "loopExp.json"),
+            make_benchmark_json(vm_performance_dir / "loopMul.json"),
+            make_benchmark_json(vm_performance_dir / "performanceTester.json"),
+        };
+    }
+
+    void register_benchmark_json(std::vector<BenchmarkTest> const &tests)
+    {
+
+        for (auto &test : tests) {
+            for (size_t block_no = 0; block_no < test.test_blocks.size();
+                 ++block_no) {
+                auto const &block = test.test_blocks[block_no];
+                for (size_t i = 0; i < block.transactions.size(); ++i) {
+                    auto const &tx = block.transactions[i];
+
+                    auto const recipient =
+                        tx.to.has_value() ? *tx.to : evmc::address{};
+
+                    auto msg = evmc_message{
+                        .kind = tx.to.has_value() ? EVMC_CALL : EVMC_CREATE,
+                        .flags = 0,
+                        .depth = 0,
+                        .gas = 150'000'000,
+                        .recipient = recipient,
+                        .sender = tx.sender,
+                        .input_data = tx.data.data(),
+                        .input_size = tx.data.size(),
+                        .value = intx::be::store<evmc::uint256be>(tx.value),
+                        .create2_salt = {},
+                        .code_address = recipient,
+                        .code = nullptr,
+                        .code_size = 0,
+                    };
+
+                    for (auto const impl : {Interpreter, Compiler, Evmone}) {
+                        benchmark::RegisterBenchmark(
+                            std::format(
+                                "execute/{}/{}/{}/{}",
+                                test.name,
+                                block_no,
+                                i,
+                                impl_name(impl)),
+                            run_benchmark_json,
+                            impl,
+                            test.pre_state,
+                            msg);
+                    }
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -179,6 +287,12 @@ int main(int argc, char **argv)
 
     for (auto const &bm : all_bms) {
         register_benchmark(bm.name, *bm.msg);
+    }
+
+    auto const all_bms_json = benchmarks_json();
+
+    for (auto const &path : all_bms_json) {
+        register_benchmark_json(path);
     }
 
     benchmark::Initialize(&argc, argv);
