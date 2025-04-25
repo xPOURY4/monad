0c8bfc3770675a03b282b3c53fddddb1930144cd
Alexander Lee <1354409+zander-xyz@users.noreply.github.com>
2025-04-25 13:16:56 -0400

    remove extra config.hpp and move anonymous namespace macro

--- libs/core/src/monad/config.hpp
+++ libs/core/src/monad/config.hpp
@@ -11,6 +11,15 @@
 
 #define MONAD_NAMESPACE ::monad
 
+#define MONAD_ANONYMOUS_NAMESPACE_BEGIN                                        \
+    MONAD_NAMESPACE_BEGIN                                                      \
+    namespace                                                                  \
+    {
+
+#define MONAD_ANONYMOUS_NAMESPACE_END                                          \
+    }                                                                          \
+    MONAD_NAMESPACE_END
+
 static_assert(CHAR_BIT == 8);
 
 static_assert(
