fc63697a170a17322a7dcdad33e1ac94035039a3
James Hunsaker <hunsaker@spacetime.org>
2025-04-29 10:11:29 -0400

    replace impl with boost pool - previous impl used deprecated c++ features

--- libs/core/src/monad/mem/batch_mem_pool.hpp
+++ libs/core/src/monad/mem/batch_mem_pool.hpp
@@ -2,9 +2,13 @@
 
 #include <monad/synchronization/spin_lock.hpp>
 
+#include <boost/pool/pool.hpp>
+
+#include <concepts>
 #include <mutex>
 #include <new>
 #include <string>
+#include <utility>
 
 MONAD_NAMESPACE_BEGIN
 
@@ -18,105 +22,66 @@ class BatchMemPool
     /// TYPES
     using Mutex = SpinLock;
 
-    using Space = std::aligned_storage_t<
-        std::max(sizeof(T), sizeof(void *)),
-        std::max(alignof(T), alignof(void *))>;
-
-    struct FreeNode
-    {
-        FreeNode *next_;
-    };
-
-    static_assert(std::is_trivially_destructible<FreeNode>::value);
-
-    static_assert(sizeof(Space) >= sizeof(T));
-    static_assert(sizeof(Space) >= sizeof(FreeNode));
-    static_assert(alignof(Space) >= alignof(T));
-    static_assert(alignof(Space) >= alignof(FreeNode));
-
-    /// CONSTANTS
-    static constexpr size_t default_batch_size = 1000;
+    using Pool = boost::pool<>;
 
     /// DATA
-    Mutex mutex_;
-    FreeNode *free_{nullptr};
-    size_t batch_;
+    Mutex mutex_{};
+    Pool pool_;
 
 /// STATS MACROS
 #ifdef MONAD_BATCH_MEM_POOL_STATS
-    #define STATS_EVENT_ALLOC(x) stats_.event_alloc(x)
     #define STATS_EVENT_DELETE() stats_.event_delete()
     #define STATS_EVENT_NEW() stats_.event_new()
 #else
-    #define STATS_EVENT_ALLOC(x)
     #define STATS_EVENT_DELETE()
     #define STATS_EVENT_NEW()
 #endif
 
 public:
-    BatchMemPool(
-        size_t const initial = 0, size_t const batch = default_batch_size)
-        : batch_(batch)
+    BatchMemPool(size_t const initial, size_t const batch = 1024)
+        : pool_{sizeof(T), initial}
     {
-        alloc_batch(initial);
-    }
-
-    ~BatchMemPool()
-    {
-        free_all();
+        void *p = pool_.malloc();
+        pool_.free(p);
+        p = nullptr;
+        pool_.set_next_size(batch);
+        pool_.set_max_size(batch);
     }
 
     template <class... Args>
-    T *new_obj(Args... args)
+    T *new_obj(Args &&...args)
     {
-        std::unique_lock l(mutex_);
-        STATS_EVENT_NEW();
-        if (!free_) {
-            alloc_batch(batch_);
+        void *p = nullptr;
+        {
+            std::lock_guard const l{mutex_};
+            STATS_EVENT_NEW();
+            p = pool_.malloc();
         }
-        FreeNode *const ptr = free_;
-        free_ = ptr->next_;
-        ptr->~FreeNode();
-        return ::new (std::launder(ptr)) T(args...);
+        if (MONAD_LIKELY(p)) {
+            T *result = static_cast<T *>(p);
+            try {
+                new (result) T(std::forward<Args>(args)...);
+            }
+            catch (...) {
+                std::lock_guard const l{mutex_};
+                pool_.free(p);
+                throw;
+            }
+            return result;
+        }
+        throw std::bad_alloc{};
     }
 
-    void delete_obj(T *obj)
+    void delete_obj(T *const obj)
     {
         obj->~T();
-        FreeNode *const ptr = ::new (std::launder(obj)) FreeNode;
-        std::unique_lock l(mutex_);
+        std::lock_guard const l{mutex_};
         STATS_EVENT_DELETE();
-        ptr->next_ = free_;
-        free_ = ptr;
+        pool_.free(obj);
     }
 
 private:
-    void alloc_batch(size_t batch)
-    {
-        for (size_t i = 0; i < batch; ++i) {
-            Space *const sp = new Space;
-            sp->~Space();
-            FreeNode *const ptr = ::new (std::launder(sp)) FreeNode;
-            ptr->next_ = free_;
-            free_ = ptr;
-        }
-        STATS_EVENT_ALLOC(batch);
-    }
-
-    void free_all()
-    {
-        FreeNode *ptr = free_;
-        while (ptr) {
-            FreeNode *const next = ptr->next_;
-            ptr->~FreeNode();
-            Space *const sp = ::new (std::launder(ptr)) Space;
-            delete sp;
-            ptr = next;
-        }
-    }
-
 /// STATS
-#undef STATS_EVENT_ALLOC
 #undef STATS_EVENT_DELETE
 #undef STATS_EVENT_NEW
 
@@ -140,16 +105,10 @@ private:
     struct PoolStats
     {
         /// DATA
-        uint64_t n_alloc_{0};
         uint64_t n_new_{0};
         uint64_t n_delete_{0};
 
         /// MEMBER FUNCTIONS
-        void event_alloc(size_t batch)
-        {
-            n_alloc_ += batch;
-        }
-
         void event_new()
         {
             ++n_new_;
@@ -163,13 +122,12 @@ private:
         std::string print_stats()
         {
             char str[100];
-            sprintf(str, "%3ld %5ld %5ld", n_alloc_, n_new_, n_delete_);
+            sprintf(str, "%5ld %5ld", n_new_, n_delete_);
             return std::string(str);
         }
 
         void clear_stats()
         {
-            n_alloc_ = 0;
             n_new_ = 0;
             n_delete_ = 0;
         }
