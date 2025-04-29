191ab27422abd5d994d8e0f33f227f96cfa655a4
James Hunsaker <hunsaker@spacetime.org>
2025-04-29 13:41:53 -0400

    remove duplicate arch flags

--- libs/core/toolchains/temp-strip-release.cmake
+++ libs/core/toolchains/temp-strip-release.cmake
@@ -7,10 +7,6 @@ endfunction()
 check_debug_flags(CMAKE_C_FLAGS_INIT)
 check_debug_flags(CMAKE_CXX_FLAGS_INIT)
 
-set(CMAKE_ASM_FLAGS_INIT "-march=haswell")
-set(CMAKE_C_FLAGS_INIT "-march=haswell")
-set(CMAKE_CXX_FLAGS_INIT "-march=haswell")
-
 # eth_call.cpp and quill have issues with CMAKE_BUILD_TYPE=Release. Something to
 # do with -O3. Previously, we built with RelWithDebInfo, which is -O2.
 set(CMAKE_C_FLAGS_RELEASE "-DNDEBUG -Wall -Wextra -Wfatal-errors -O2 -g1")
