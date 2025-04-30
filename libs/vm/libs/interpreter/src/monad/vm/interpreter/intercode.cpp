896043afbbe0018bc84e8d185202b4731ac1cb97
Bruce Collie <brucecollie82@gmail.com>
2025-04-30 04:54:33 -0400

    Refactor intercode to pad at the front

--- libs/vm/libs/interpreter/src/monad/vm/interpreter/intercode.cpp
+++ libs/vm/libs/interpreter/src/monad/vm/interpreter/intercode.cpp
@@ -11,45 +11,6 @@ using namespace monad::vm::compiler;
 
 namespace monad::vm::interpreter
 {
-    namespace
-    {
-        std::unique_ptr<std::uint8_t[]>
-        pad(std::span<std::uint8_t const> const code)
-        {
-            // 32 for a truncated PUSH32, 1 for a STOP so that we don't have to
-            // worry about going off the end.
-            constexpr std::size_t padding_size = 32 + 1;
-
-            auto buffer = std::make_unique_for_overwrite<std::uint8_t[]>(
-                code.size() + padding_size);
-
-            std::copy(code.begin(), code.end(), &buffer[0]);
-            std::fill_n(&buffer[code.size()], padding_size, 0);
-
-            return buffer;
-        }
-
-        Intercode::JumpdestMap
-        find_jumpdests(std::span<std::uint8_t const> const code)
-        {
-            auto jumpdests = Intercode::JumpdestMap(code.size(), false);
-
-            for (auto i = 0u; i < code.size(); ++i) {
-                auto const op = code[i];
-
-                if (op == EvmOpCode::JUMPDEST) {
-                    jumpdests[i] = true;
-                }
-
-                if (is_push_opcode(op)) {
-                    i += get_push_opcode_index(op);
-                }
-            }
-
-            return jumpdests;
-        }
-    }
-
     Intercode::Intercode(std::span<std::uint8_t const> const code)
         : padded_code_(pad(code))
         , code_size_(code.size())
@@ -59,7 +20,7 @@ namespace monad::vm::interpreter
 
     std::uint8_t const *Intercode::code() const noexcept
     {
-        return padded_code_.get();
+        return padded_code_.get() + start_padding_size;
     }
 
     std::size_t Intercode::code_size() const noexcept
@@ -70,5 +31,39 @@ namespace monad::vm::interpreter
     bool Intercode::is_jumpdest(std::size_t const pc) const noexcept
     {
         return pc < code_size_ && jumpdest_map_[pc];
+    }
+
+    std::unique_ptr<std::uint8_t[]>
+    Intercode::pad(std::span<std::uint8_t const> const code)
+    {
+        auto buffer = std::make_unique_for_overwrite<std::uint8_t[]>(
+            start_padding_size + code.size() + end_padding_size);
+
+        std::fill_n(&buffer[0], start_padding_size, 0);
+        std::copy(code.begin(), code.end(), &buffer[start_padding_size]);
+        std::fill_n(
+            &buffer[code.size() + start_padding_size], end_padding_size, 0);
+
+        return buffer;
+    }
+
+    auto Intercode::find_jumpdests(std::span<std::uint8_t const> const code)
+        -> JumpdestMap
+    {
+        auto jumpdests = JumpdestMap(code.size(), false);
+
+        for (auto i = 0u; i < code.size(); ++i) {
+            auto const op = code[i];
+
+            if (op == EvmOpCode::JUMPDEST) {
+                jumpdests[i] = true;
+            }
+
+            if (is_push_opcode(op)) {
+                i += get_push_opcode_index(op);
+            }
+        }
+
+        return jumpdests;
     }
 }
--- libs/vm/libs/interpreter/src/monad/vm/interpreter/intercode.hpp
+++ libs/vm/libs/interpreter/src/monad/vm/interpreter/intercode.hpp
@@ -28,5 +28,21 @@ namespace monad::vm::interpreter
         std::unique_ptr<std::uint8_t[]> padded_code_;
         std::size_t code_size_;
         JumpdestMap jumpdest_map_;
+
+        // 30 bytes of initial padding ensures that we can implement all
+        // PUSHN opcodes by reading data from _before_ the instruction
+        // pointer with a single 32-byte read, then cleaning up any
+        // over-read in the result value.
+        static constexpr std::size_t start_padding_size = 30;
+
+        // 32 for a truncated PUSH32, 1 for a STOP so that we don't have to
+        // worry about going off the end.
+        static constexpr std::size_t end_padding_size = 32 + 1;
+
+        static std::unique_ptr<std::uint8_t[]>
+        pad(std::span<std::uint8_t const> const code);
+
+        static JumpdestMap
+        find_jumpdests(std::span<std::uint8_t const> const code);
     };
 }
