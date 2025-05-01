a29390cadc85ae908213ee8c371fb1e0481c7f77
James Hunsaker <hunsaker@spacetime.org>
2025-04-30 21:08:24 -0400

    include sanitizer on link line as well

--- libs/core/toolchains/gcc-asan.cmake
+++ libs/core/toolchains/gcc-asan.cmake
@@ -1,3 +1,4 @@
 set(CMAKE_ASM_FLAGS_INIT "-march=haswell")
-set(CMAKE_C_FLAGS_INIT "-march=haswell -fsanitize=address -fsanitize=undefined -fsanitize-address-use-after-scope -fno-omit-frame-pointer -g")
-set(CMAKE_CXX_FLAGS_INIT "-march=haswell -fsanitize=address -fsanitize=undefined -fsanitize-address-use-after-scope -fno-omit-frame-pointer -g")
+set(CMAKE_C_FLAGS_INIT "-march=haswell -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -g")
+set(CMAKE_CXX_FLAGS_INIT "-march=haswell -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -g")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fsanitize=address -fsanitize=undefined")
