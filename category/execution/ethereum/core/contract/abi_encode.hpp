c783cc0e8754b226736eaa2266271bcf1fe2dc7f
Kevin Kuehler <kkuehler@category.xyz>
2025-07-17 12:27:16 -0400

    [core/contract]: Add abi encode utility
    Supports solidity abi encoding of types to be used by the staking
    precompile getter functions and events.
    
    The ABI is defined here:
    
    https://docs.soliditylang.org/en/latest/abi-spec.html#formal-specification-of-the-encoding

---
+++ category/execution/ethereum/core/contract/abi_encode.hpp
@@ -0,0 +1,166 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <category/core/byte_string.hpp>
+#include <category/core/bytes.hpp>
+#include <category/core/config.hpp>
+#include <category/core/int.hpp>
+#include <category/core/math.hpp>
+#include <category/execution/ethereum/core/address.hpp>
+#include <category/execution/ethereum/core/contract/big_endian.hpp>
+
+#include <cstdint>
+#include <vector>
+
+MONAD_NAMESPACE_BEGIN
+
+// Helpers for encoding types into solidity encoding ABI. This is both for
+// events and so return values from contracts can be parsed by solidity
+// `abi.decode()`.
+
+//////////////////////////////////////////////////////////////
+// Standalone functions for encoding types.
+//
+// https://docs.soliditylang.org/en/latest/abi-spec.html#types
+//////////////////////////////////////////////////////////////
+inline bytes32_t abi_encode_address(Address const &address)
+{
+    bytes32_t output{};
+    std::memcpy(&output.bytes[12], address.bytes, sizeof(Address));
+    return output;
+}
+
+template <BigEndianType I>
+bytes32_t abi_encode_uint(I const &i)
+{
+    static_assert(sizeof(I) <= sizeof(bytes32_t));
+
+    constexpr size_t offset = sizeof(bytes32_t) - sizeof(I);
+    bytes32_t output{};
+    std::memcpy(&output.bytes[offset], &i, sizeof(I));
+    return output;
+}
+
+inline byte_string abi_encode_bytes(byte_string_view const input)
+{
+    byte_string output;
+    u256_be const size{input.size()};
+    size_t const padding =
+        round_up(input.size(), sizeof(bytes32_t)) - input.size();
+    output += abi_encode_uint(size);
+    output += input;
+    output = output.append(padding, 0);
+    return output;
+}
+
+template <BigEndianType I>
+inline byte_string abi_encode_uint_array(std::vector<I> const &arr)
+{
+    byte_string output{};
+    u64_be const len_be{arr.size()};
+    output += abi_encode_uint(len_be);
+    for (auto const &e : arr) {
+        output += abi_encode_uint(e);
+    }
+
+    return output;
+}
+
+inline byte_string abi_encode_address_array(std::vector<Address> const &arr)
+{
+    byte_string output{};
+    u64_be const len_be{arr.size()};
+    output += abi_encode_uint(len_be);
+    for (Address const &e : arr) {
+        output += abi_encode_address(e);
+    }
+
+    return output;
+}
+
+// Encodes a tuple
+//  * static types : Have size <= 32 are padded out and added to the "head".
+//  * dynamic types: size > 32. The "head" stores the offset in the tail, and
+//                   the actual data is stored in the tail.
+//
+// https://docs.soliditylang.org/en/latest/abi-spec.html#formal-specification-of-the-encoding
+class AbiEncoder
+{
+    byte_string head_;
+    byte_string tail_;
+    std::vector<std::pair<size_t, size_t>> unresolved_offsets_;
+
+    void add_static(bytes32_t data)
+    {
+        head_ += data;
+    }
+
+    void add_dynamic(byte_string data)
+    {
+        unresolved_offsets_.emplace_back(head_.size(), tail_.size());
+        head_ += bytes32_t{};
+        tail_ += data;
+    }
+
+public:
+    void add_address(Address const &address)
+    {
+        add_static(abi_encode_address(address));
+    }
+
+    template <BigEndianType I>
+    void add_uint(I const &i)
+    {
+        add_static(abi_encode_uint(i));
+    }
+
+    void add_bool(bool const b)
+    {
+        u64_be as_int = b ? 1 : 0;
+        add_static(abi_encode_uint(as_int));
+    }
+
+    template <BigEndianType I>
+    void add_uint_array(std::vector<I> const &arr)
+    {
+        add_dynamic(abi_encode_uint_array(arr));
+    }
+
+    void add_address_array(std::vector<Address> const &arr)
+    {
+        add_dynamic(abi_encode_address_array(arr));
+    }
+
+    void add_bytes(byte_string_view const data)
+    {
+        add_dynamic(abi_encode_bytes(data));
+    }
+
+    byte_string encode_final()
+    {
+        for (auto const [unresolved, tail_cumsum] : unresolved_offsets_) {
+            u256_be offset = static_cast<uint256_t>(head_.size()) + tail_cumsum;
+            uint8_t *const p = &head_[unresolved];
+            bytes32_t encoded = abi_encode_uint(offset);
+            std::memcpy(p, encoded.bytes, sizeof(bytes32_t));
+        }
+
+        return std::move(head_) + std::move(tail_);
+    }
+};
+
+MONAD_NAMESPACE_END
---
+++ category/execution/ethereum/core/contract/test_abi_encode.cpp
@@ -0,0 +1,173 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <category/core/byte_string.hpp>
+#include <category/core/int.hpp>
+#include <category/execution/ethereum/core/address.hpp>
+#include <category/execution/ethereum/core/contract/abi_encode.hpp>
+#include <category/execution/ethereum/core/contract/big_endian.hpp>
+
+#include <evmc/evmc.hpp>
+#include <gtest/gtest.h>
+#include <intx/intx.hpp>
+
+using namespace monad;
+using namespace intx::literals;
+
+TEST(AbiEncode, u16)
+{
+    constexpr uint16_t input{65535};
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000ffff")
+            .value();
+
+    u16_be encoded = input;
+    auto const output = byte_string{abi_encode_uint(encoded)};
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, u256)
+{
+    constexpr uint256_t input{15355346523654236542356453_u256};
+    byte_string const expected =
+        evmc::from_hex("0x0000000000000000000000000000000000000000000cb3"
+                       "9f00c54ee156444be5")
+            .value();
+
+    u256_be encoded = input;
+    auto const output = byte_string{abi_encode_uint(encoded)};
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, address)
+{
+    constexpr Address input{0xDEADBEEF000000000000000000F00D0000000100_address};
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000deadbeef000000000000000000f00d0000000100")
+            .value();
+
+    auto const output = byte_string{abi_encode_address(input)};
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, bytes)
+{
+    byte_string const bls_pubkey =
+        evmc::from_hex("0x85686279cefd8ce0d32338910d476ca090b67"
+                       "f97fc6f2fbc7d96b0cf3d7dca2fe9"
+                       "80de55a715702f2ad35ee5f9bd6f9b")
+            .value();
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000003085"
+            "686279cefd8ce0d32338910d476ca090b67f97fc6f2fbc7d96b0cf3d7dca2fe980"
+            "de55a715702f2ad35ee5f9bd6f9b00000000000000000000000000000000")
+            .value();
+
+    byte_string const output = abi_encode_bytes(bls_pubkey);
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, tuple)
+{
+    byte_string const input_bytes =
+        evmc::from_hex(
+            "0x85686279cefd8ce0d32338910d476ca090b67245034520354205420354203542"
+            "f97fc6f2fbc7d96b0cf3d7dca2f80de55a715702f2ad35ee5f9bd6f9bb")
+            .value();
+    u256_be const input_u256 = 15324315423000000_u256;
+
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000008000"
+            "0000000000000000000000000000000000000000000000003671623936c5c00000"
+            "0000000000000000000000000000000000000000000000000000000000e0000000"
+            "000000000000000000000000000000000000000000003671623936c5c000000000"
+            "0000000000000000000000000000000000000000000000000000003d85686279ce"
+            "fd8ce0d32338910d476ca090b67245034520354205420354203542f97fc6f2fbc7"
+            "d96b0cf3d7dca2f80de55a715702f2ad35ee5f9bd6f9bb00000000000000000000"
+            "0000000000000000000000000000000000000000000000003d85686279cefd8ce0"
+            "d32338910d476ca090b67245034520354205420354203542f97fc6f2fbc7d96b0c"
+            "f3d7dca2f80de55a715702f2ad35ee5f9bd6f9bb000000")
+            .value();
+    AbiEncoder encoder;
+    encoder.add_bytes(input_bytes);
+    encoder.add_uint(input_u256);
+    encoder.add_bytes(input_bytes);
+    encoder.add_uint(input_u256);
+    auto const output = encoder.encode_final();
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, empty_array)
+{
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000002000"
+            "00000000000000000000000000000000000000000000000000000000000000")
+            .value();
+    std::vector<u64_be> arr{};
+    AbiEncoder encoder;
+    encoder.add_uint_array(arr);
+    auto const output = encoder.encode_final();
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, array_tuple)
+{
+    std::vector<u64_be> arr{2, 4, 20'000, 40'000};
+
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000000100"
+            "000000000000000000000000000000000000000000000000000000000000400000"
+            "000000000000000000000000000000000000000000000000000000000004000000"
+            "000000000000000000000000000000000000000000000000000000000200000000"
+            "000000000000000000000000000000000000000000000000000000040000000000"
+            "000000000000000000000000000000000000000000000000004e20000000000000"
+            "0000000000000000000000000000000000000000000000009c40")
+            .value();
+    AbiEncoder encoder;
+    encoder.add_bool(true);
+    encoder.add_uint_array(arr);
+    auto const output = encoder.encode_final();
+    EXPECT_EQ(output, expected);
+}
+
+TEST(AbiEncode, array_address)
+{
+    std::vector<Address> arr{
+        0x1111111111111111111111111111111111111111_address,
+        0x2222222222222222222222222222222222222222_address,
+        0x3333333333333333333333333333333333333333_address,
+        0x4444444444444444444444444444444444444444_address,
+    };
+
+    byte_string const expected =
+        evmc::from_hex(
+            "000000000000000000000000000000000000000000000000000000000000002000"
+            "000000000000000000000000000000000000000000000000000000000000040000"
+            "000000000000000000001111111111111111111111111111111111111111000000"
+            "000000000000000000222222222222222222222222222222222222222200000000"
+            "000000000000000033333333333333333333333333333333333333330000000000"
+            "000000000000004444444444444444444444444444444444444444")
+            .value();
+    AbiEncoder encoder;
+    encoder.add_address_array(arr);
+    auto const output = encoder.encode_final();
+    EXPECT_EQ(output, expected);
+}
