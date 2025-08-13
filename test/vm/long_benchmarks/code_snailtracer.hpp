42a4626ca7f9ce0aaf100848dd334dbbd3950e16
Ken Camann <kcamann@category.xyz>
2025-08-13 16:31:02 -0400

    Add GPLv3 license comments to source code

--- test/vm/long_benchmarks/code_snailtracer.hpp
+++ test/vm/long_benchmarks/code_snailtracer.hpp
@@ -1,3 +1,18 @@
+// Copyright (C) 2025 Category Labs, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 unsigned char code_snailtracer[] = {
     0x60, 0x80, 0x60, 0x40, 0x52, 0x34, 0x80, 0x15, 0x61, 0x00, 0x10, 0x57,
     0x60, 0x00, 0x80, 0xfd, 0x5b, 0x50, 0x61, 0x54, 0x14, 0x80, 0x61, 0x00,
