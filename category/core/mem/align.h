edd82c2c55b8f736bfcfce01ce1453bae1f6ff10
Ken Camann <kcamann@category.xyz>
2025-09-03 15:38:09 -0400

    [core] remove bit_util.h
    This was only ever used for monad_round_size_to_align. Rounding size
    to alignment is commonly done via the expression
    
       (size + align - 1) & ~(align - 1)
    
    So just do that instead, after checking for power-of-2 alignment

--- category/core/mem/align.h
+++ category/core/mem/align.h
@@ -33,25 +33,19 @@
 #include <stddef.h>
 
 #include <category/core/assert.h>
-#include <category/core/bit_util.h>
-#include <category/core/likely.h>
 
 #ifdef __cplusplus
 [[gnu::always_inline]] static inline size_t
 monad_round_size_to_align(size_t const size, size_t const align)
 {
     MONAD_DEBUG_ASSERT(std::has_single_bit(align));
-    return bit_round_up(size, static_cast<unsigned>(std::countr_zero(align)));
+    return (size + align - 1) & ~(align - 1);
 }
 #else
 [[gnu::always_inline]] static inline size_t
 monad_round_size_to_align(size_t const size, size_t const align)
 {
-    // Round size up to the nearest multiple of align. bit_round_up does this,
-    // provided that align has the form 2^b and is expressed as `b`. `b` (which
-    // is `log_2 align`) is computed efficiently using stdc_trailing_zeros,
-    // an intrinsic operation on many platforms (e.g., TZCNT).
     MONAD_DEBUG_ASSERT(stdc_has_single_bit(align));
-    return bit_round_up(size, stdc_trailing_zeros(align));
+    return (size + align - 1) & ~(align - 1);
 }
 #endif
