17da9acbe54671c65822a411502d302198966bb1
Alexander Lee <alee@category.xyz>
2025-09-22 10:51:45 -0400

    enable eip-7825 test

--- test/ethereum_test/exclude/prague.cmake
+++ test/ethereum_test/exclude/prague.cmake
@@ -8,8 +8,6 @@ set(prague_excluded_tests
     "BlockchainTests.prague/eip7002_el_triggerable_withdrawals/*"
     "BlockchainTests.prague/eip7251_consolidations/*"
     "BlockchainTests.prague/eip7685_general_purpose_el_requests/*"
-    # Unimplemented tx types
-    "BlockchainTests.osaka/eip7825_transaction_gas_limit_cap/tx_gas_limit/transaction_gas_limit_cap.json"
     # Long-running tests
     "BlockchainTests.prague/eip2935_historical_block_hashes_from_state/block_hashes/block_hashes_history.json"
 )
